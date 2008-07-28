6300172f535d27adbc73f20ba568ca26485fc8e3
Theme Directory <noreply@wordpress.org>
2008-07-28 18:31:12 +0000

    New version of Baltimore Phototheme - 1.2

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://wordpress.org/">WordPress</a> and the <a href="http://blog.itsth.com/">Baltimore Fototheme</A>
+		<a href="http://wordpress.org/">WordPress</a> and the <a href="http://www.easy2sync.com/en/produkte/e2s4o.php">sync outlook</A> software.
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
 		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
