2fb4bf9c11ee2de723a821432ac61d7e3c7915fd
Theme Directory <noreply@wordpress.org>
2008-08-25 13:38:50 +0000

    New theme: Aurora - 2.0

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div class="clearboth"></div>
+
+</div><!-- #wrapper .hfeed -->
+
+<!-- This Wordpress theme is provided free of charge, much like Wordpress itself. Please help us by leaving in the Wordpress and MB Web Design links on the footer of your website. It costs you nothing, but this token of appreciation means a lot. -->
+<div id="footer">
+	<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e('WordPress', 'sandbox'); ?>" rel="generator"><?php _e('WordPress', 'sandbox'); ?></a></span>
+	<span class="meta-sep">|</span>
+	<span>This is a <a href="http://www.mbwebdesign.co.uk/templates/wordpress">free Wordpress template</a> by <a href="http://www.mbwebdesign.co.uk">MB Web Design</a></span>
+	</div><!-- #footer -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
