d2a08636226f38eba91a02b4e6c94da3dcec7a79
Theme Directory <noreply@wordpress.org>
2008-10-01 07:45:35 +0000

    New version of Aurora - 2.0.1

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 <div id="footer">
 	<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e('WordPress', 'sandbox'); ?>" rel="generator"><?php _e('WordPress', 'sandbox'); ?></a></span>
 	<span class="meta-sep">|</span>
-	<span>This is a <a href="http://www.mbwebdesign.co.uk/templates/wordpress">free Wordpress template</a> by <a href="http://www.mbwebdesign.co.uk">MB Web Design</a></span>
+	<span><a href="http://www.mbwebdesign.co.uk/templates/wordpress" title="Free Wordpress template">Free Wordpress template</a>: <a href="http://www.mbwebdesign.co.uk" title="MB Web Design is a UK Web Designer">MB Web Design</a></span>
 	</div><!-- #footer -->
 
 <?php wp_footer() ?>
