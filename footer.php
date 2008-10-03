380980d900d3d709456572fb4ad36063a6fb50a9
Theme Directory <noreply@wordpress.org>
2008-10-03 12:39:45 +0000

    New theme: Aapna - 1.2

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div><?php //closing for #main?>
+<div id="footer">
+
+ <?php // Please leave the footer credits intact ?>
+	<p><span><A HREF="http://www.saumendra.com/wordpress-themes/">Aapna Theme by</A> -<a href="http://www.saumendra.com" title="Saumendra">Saumendra</a></span><strong><?php bloginfo('name');?></strong> Copyright &copy; <?php echo date('Y');?> Powered by Wordpress</p>
+</div>
+<?php wp_footer();?>
+</body>  
+</html>
