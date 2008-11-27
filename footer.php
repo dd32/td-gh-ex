48d8d47f4d42526358e5bb67e0262751787f97bb
Theme Directory <noreply@wordpress.org>
2008-11-27 15:41:00 +0000

    New theme: QuickPic - 1.0

---
+++ footer.php
@@ -0,0 +1,142 @@
+  <!-- Bottom Box --> 
+<div id="bottombox">
+
+<div id="adbox">
+<!-- Author Info --> 
+<ul>
+<li><h4>Author</h4>
+			<p>Here you can provide some author or blog infomation.</p>
+			</li>
+</ul>
+</div>
+
+<table cellpadding="0" cellspacing="2" border="0"><tr><td valign="top" width="30%">
+ <div id="box1">
+
+<!-- Pages --> 
+<ul>
+<?php wp_list_pages('title_li=<h4>Pages</h4>' ); ?>
+</ul>
+
+<!-- Widgetized sidebar 1 --> 
+<ul>
+  <li>
+  <?php if ( !function_exists('dynamic_sidebar') 
+        || !dynamic_sidebar('sidebar1') ) : ?>
+<?php endif; ?> 
+  </li>
+</ul> 
+
+</div>
+</td>
+
+<td valign="top" width="30%">
+<div id="box2"> 
+
+<!-- Categories --> 
+<ul>
+     <?php wp_list_categories('show_count=1&title_li=<h4>Categories</h4>'); ?>
+</ul>
+
+
+<!-- Archives --> 
+<ul>
+  <li><h4>Archives</h4>
+      <ul>
+	 <?php wp_get_archives('type=monthly'); ?>
+     </ul>
+  </li>
+</ul> 
+
+
+<!-- Widgetized sidebar 2 --> 
+<ul>
+  <li>
+  <?php if ( !function_exists('dynamic_sidebar') 
+        || !dynamic_sidebar('sidebar2') ) : ?>
+<?php endif; ?> 
+  </li>
+</ul>
+ 
+
+</div>
+</td>
+
+<td valign="top" width="30%">
+<div id="box3">
+ 
+<!-- Search --> 
+<ul>
+<li><h4>Search</h4>
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+ <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="15" />
+<input type="submit" id="searchsubmit" value="&raquo;" />
+ </form>
+ </li>
+</ul>
+
+
+<!-- Blogroll --> 
+<ul>
+     <?php wp_list_bookmarks('categorize=0&title_before=<h4>&title_after=</h4>'); ?>
+ </ul>
+
+<ul>
+<li><h4>Meta</h4>
+   <ul>
+	<?php wp_register(); ?>
+	<li><?php wp_loginout(); ?></li>  
+	<?php wp_meta(); ?>
+  </ul>
+ </li>
+</ul>
+
+<!-- Widgetized sidebar 3 --> 
+<ul>
+  <li>
+  <?php if ( !function_exists('dynamic_sidebar') 
+        || !dynamic_sidebar('sidebar3') ) : ?>
+<?php endif; ?>
+  </li>
+</ul>
+
+ 
+ 
+</div>
+</td></tr></table>
+ 
+
+</div><!-- /Bottom Box --> 
+
+
+
+
+<div id="footer"> 
+	
+
+
+<p>
+
+		Copyright 2008. <?php bloginfo('name'); ?>. All Rights Reserved.
+ 
+<br />
+
+
+<!-- DO NOT REMOVE OR EDIT. ATTRIBUTION REQUIRED TO USE THIS THEME --> 
+<a href="http://www.quickonlinetips.com/archives/quickpic/" title="QuickPic">QuickPic</a> theme by <a href="http://www.quickonlinetips.com/" title="Quick Online Tips">Quick Online Tips</a>
+<!-- DO NOT REMOVE OR EDIT. ATTRIBUTION REQUIRED TO USE THIS THEME --> 
+
+
+
+</p>
+</div>
+</div>
+ 
+		<?php wp_footer(); ?>
+
+<!-- QuickPic Wordpress Theme by Quick Online Tips. http://www.quickonlinetips.com/ --> 
+
+</body>
+</html>
+
+
---
+++ search.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+
+	<?php if (have_posts()) : ?>
+
+		<h2 class="pagetitle">Search Results</h2>
+
+		<div class="navigation">
+			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+		</div>
+
+
+		<?php while (have_posts()) : the_post(); ?>
+
+			<div class="post">
+				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+				<small><?php the_time('F jS, Y') ?>  by <?php the_author() ?>   |  <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?> | Filed in <?php the_category(', ') ?> <?php edit_post_link('Edit'); ?>  </small>
+ 
+			</div>
+
+		<?php endwhile; ?>
+
+		<div class="navigation">
+			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+		</div>
+
+	<?php else : ?>
+
+		<h2 class="center">No posts found. Try a different search?</h2>
+		<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+<div><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
+<input type="submit" id="searchsubmit" value="Search" />
+</div>
+</form>
+
+	<?php endif; ?>
+
+	</div>
+  
+<?php get_footer(); ?>
