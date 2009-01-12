2710166ce299f865b70d1c63c839564c34101251
Theme Directory <noreply@wordpress.org>
2009-01-12 02:35:24 +0000

    New version of Azul - 1.2

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-
-	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
-
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-	<?php wp_get_archives('type=monthly&format=link'); ?>
-	<?php //comments_popup_script(); // off by default ?>
-	<?php wp_head(); ?>
-</head>
-<body>
-	
-	<div id="container">
-		
 		<?php get_header(); ?>
 
 		<div id="posts">
@@ -52,10 +27,10 @@
 							<?php comments_popup_link('0','1','%') ?>
 						</div>
 						
-						<p class="postmetadata">
+						<div class="postmetadata">
 						<?php _e('Filed under&#58;'); ?><br />
 						<?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?>
-						</p>
+						</div>
 
 					</div>
 
