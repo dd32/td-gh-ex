45c3fe064be6f128eb9f37de427e4a3fdfd34989
Theme Directory <noreply@wordpress.org>
2009-06-21 11:38:39 +0000

    New version of Abov - 1.1.2

--- functions.php
+++ functions.php
@@ -8,7 +8,13 @@ if ( function_exists('register_sidebar') ) {
 
 
 
-
+add_filter('comments_template', 'legacy_comments');
+function legacy_comments($file) {
+	if(!function_exists('wp_list_comments')) : // WP 2.7-only check
+		$file = TEMPLATEPATH . '/legacy.comments.php';
+	endif;
+	return $file;
+}
 
 
  
