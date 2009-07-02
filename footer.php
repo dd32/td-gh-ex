de8bdf8289257f7da6149b7f919b6cfed94057c2
Theme Directory <noreply@wordpress.org>
2009-07-02 02:33:14 +0000

    New version of Basal - 1.0.4

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
-<div id="copy"><p>Copyright &copy; Your Name <?php
-echo date("Y")?>, <span class="thin">All rights Reserved</span> | Theme by <a href="http://ryanmcnair.co.uk/">Ryan McNair</a></p>
+<div id="copy"><p>
+Copyright &copy; <!-- Insert Your Name --> 
+<?php echo date("Y")?>, |
+<?php bloginfo('name'); ?> is proudly powered by
+		<a href="http://wordpress.org/">WordPress</a>
+
+
+<span class="thin">All rights Reserved</span> | Theme by <a href="http://ryanmcnair.co.uk/">Ryan McNair</a></p>
 	<p><?php wp_footer() ?></p>
 </div>
 </div>
@@ -7,4 +13,4 @@ echo date("Y")?>, <span class="thin">All rights Reserved</span> | Theme by <a hr
 
 <!-- Can put web stats code here -->
 
-</html>
+</html> 
