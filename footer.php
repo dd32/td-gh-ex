ff4a482677c18e59766ed620ae3f9f0c780e100a
Theme Directory <noreply@wordpress.org>
2009-07-09 05:55:15 +0000

    New theme: Aurelia - 0.1

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+
+<hr /></div>
+<div id="footer" >
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<?php bloginfo('name'); ?> running
+		<a href="http://wordpress.org/">WordPress</a> using <a href="http://pretty.coolcomputerguy.com/">Aurelia.</a>
+
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+
+</div>
+</div>
+
+
+
+		<?php wp_footer(); ?>
+</body>
+</html>
