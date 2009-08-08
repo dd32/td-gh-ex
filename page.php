f2249bcff6ea742cf5fef9c0cb368402cea25a3a
Theme Directory <noreply@wordpress.org>
2009-08-08 22:21:00 +0000

    New version of Aurora - 4.0.1

--- page.php
+++ page.php
@@ -13,6 +13,8 @@
 <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 </div>
 
+<?php comments_template(); ?>
+
 </div>
 		<?php endwhile; endif; ?>
 	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
