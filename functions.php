fd2beda2c902f707c61da515cb9b1eed53ec38ff
Theme Directory <noreply@wordpress.org>
2009-08-23 14:26:54 +0000

    New version of RCG Forest - 1.3.0

--- functions.php
+++ functions.php
@@ -27,4 +27,45 @@ function widget_my_links($args) {
         
 if ( function_exists('register_sidebar_widget') )
   register_sidebar_widget(__('My Links'), 'widget_my_links');
+
+
+//Custom image header
+define('HEADER_TEXTCOLOR', '');
+define('HEADER_IMAGE_WIDTH', 774);
+define('HEADER_IMAGE', '%s/images/forest.png');
+define('HEADER_IMAGE_HEIGHT', 120);
+define('NO_HEADER_TEXT', true);
+
+function header_style() {
+?>
+<style type="text/css">
+#header {
+  background: url(<?php header_image() ?>) no-repeat;
+  height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
+  width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
+  border: #fff solid;
+}
+</style>
+<?php
+}
+
+function rcg_admin_header_style() {
+?>
+<style type="text/css">
+#headimg {
+  background: url(<?php header_image() ?>) no-repeat;
+  height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
+  width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
+  border: #fff solid;
+}
+
+#headimg * {
+  display:none;
+}
+</style>
+<?php
+}
+
+add_custom_image_header('header_style', 'rcg_admin_header_style');
+
 ?>
