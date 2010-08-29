2cbc7a429d52bdd26c26b1ee9d6414fcd38a7b96
Theme Directory <noreply@wordpress.org>
2010-08-29 12:44:51 +0000

    New version of RCG Forest - 1.6.0

--- functions.php
+++ functions.php
@@ -9,26 +9,6 @@ if ( function_exists('register_sidebar') )
         'after_title' => '</h2>',
     ));
 
-function widget_my_links($args) {
-  global $wp_db_version;
-  extract($args, EXTR_SKIP);
-  if ( $wp_db_version < 3582 ) {
-    // This ONLY works with li/h2 sidebars.
-    get_links_list();
-  } else {
-    $before_widget = preg_replace('/id="[^"]*"/','id="%id"', $before_widget);
-    wp_list_bookmarks(array(
-      'title_before' => $before_title, 'title_after' => $after_title,
-      'category_before' => $before_widget, 'category_after' => $after_widget,
-      'show_images' => true, 'class' => 'linkcat widget'
-    ));
-  }
-}
-        
-if ( function_exists('register_sidebar_widget') )
-  register_sidebar_widget(__('My Links'), 'widget_my_links');
-
-
 //Custom image header
 define('HEADER_TEXTCOLOR', '');
 define('HEADER_IMAGE_WIDTH', 774);
