36fed001fd82c690c6eac5c8a124a7808415df7a
Theme Directory <noreply@wordpress.org>
2010-09-25 13:21:36 +0000

    New version of Adventure - 1.3.0

--- functions.php
+++ functions.php
@@ -5,10 +5,10 @@ if ( function_exists( 'register_nav_menu' ) ) register_nav_menu( 'menu', 'Menu' 
 if ( function_exists('register_sidebar') ) register_sidebar( array(
     'name' => __( 'Widgets', 'simplest' ),
     'id' => 'widgets',
-    'before_widget' => '<div class="widget">',
-    'after_widget' => '</div><!-- widget -->',
-    'before_title' => '<h4>',
-    'after_title' => '</h4>') );
+    'before_widget' => '<li id="%1$s" class="widget %2$s">',
+    'after_widget' => '</li>',
+    'before_title' => '<h2 class="widgettitle">',
+    'after_title' => '</h2>',) );
    
 function print_comment($comment, $args, $depth) {
 	$GLOBALS['comment'] = $comment; ?>
