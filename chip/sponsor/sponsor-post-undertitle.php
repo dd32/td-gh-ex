c328294f5dc8931b668d8b67baabf245c1c5cd61
Theme Directory <noreply@wordpress.org>
2011-05-30 09:58:16 +0000

    New version of Chip Life - 1.3.5.1

--- chip/sponsor/sponsor-post-undertitle.php
+++ chip/sponsor/sponsor-post-undertitle.php
@@ -1,2 +1,2 @@
-<?php global $chip_zero_global; ?>
-<div class="margin10b"><?php echo htmlspecialchars_decode ( $chip_zero_global['theme_options']['chip_zero_sponsor_post_undertitle_code'] ); ?></div>
+<?php global $chip_life_global; ?>
+<div class="margin10b"><?php echo htmlspecialchars_decode ( $chip_life_global['theme_options']['chip_life_sponsor_post_undertitle_code'] ); ?></div>
