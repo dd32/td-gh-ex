287ebd5cde982b7e92b3dfc2644fa12753298bd5
Theme Directory <noreply@wordpress.org>
2011-07-29 19:42:32 +0000

    New version of Adsticle - 1.0.2

--- footer.php
+++ footer.php
@@ -8,14 +8,7 @@ Powered by <a href="http://wordpress.org">WordPress</a>.
 </div>
 
 <div class="fr">
-      Designed by 	  
-	  <?php 
-	  /*
-	  You can buy No Copyright License on http://wpmole.com/adsticle 
-	  (for remove this link)
-	  */
-	  ?>
-	  <a href="http://wpmole.com">WPMole</a>
+  Designed by <a href="http://wpmole.com">WPMole</a>.
 </div>
 
 </div>
