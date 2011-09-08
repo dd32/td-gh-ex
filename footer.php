4ca24332b7d68d5d6629caffcdc5174206aea7cf
Theme Directory <noreply@wordpress.org>
2011-09-08 02:25:52 +0000

    New version of Undedicated - 1.0.2

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<?php wp_nav_menu( array( 'menu' => 'Footer Navigation', 'container' => 'div','container_id' => 'footer-navi', 'depth' => '1', 'theme_location' => 'footer-menu',  'fallback_cb' => '' ) ); ?>
-	<p><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo ('name');?>"><?php bloginfo ('name');?></a> <?php _e('powered by','undedicated'); ?> <a href="http://www.wordpress.org"><?php _e('WordPress','undedicated'); ?></a> and <a href="http://www.speckygeek.com/wordpress-themes/" title="Free WordPress theme"><?php _e('Undedicated','undedicated'); ?></a>.</p>
+	<p><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo ('name');?>"><?php bloginfo ('name');?></a> <?php _e('powered by','undedicated'); ?> <a href="http://www.wordpress.org"><?php _e('WordPress','undedicated'); ?></a> and <a href="http://www.speckygeek.com/undedicated-minimal-wordpress-theme/" title="Free WordPress theme"><?php _e('Undedicated','undedicated'); ?></a>.</p>
 </div><!--#footer-->
 
 </div><!--#wrapper -->
