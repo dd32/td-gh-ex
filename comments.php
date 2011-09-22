773d2a9c4750f97939ed6bcf915679460b193ec3
Theme Directory <noreply@wordpress.org>
2011-09-22 10:57:07 +0000

    New version of Baza Noclegowa - 1.9

--- comments.php
+++ comments.php
@@ -1,6 +1,4 @@
-<?php // Do not delete these lines
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
+<?php
 
 		if ( post_password_required() ) : ?>
 			<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
