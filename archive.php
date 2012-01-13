5f8c4cb42f4a12e1323dc95fccf977054aa7c2ee
Theme Directory <noreply@wordpress.org>
2012-01-13 18:20:47 +0000

    New version of Admired - 1.0.6

--- archive.php
+++ archive.php
@@ -17,9 +17,9 @@ get_header(); ?>
 						<?php if ( is_day() ) : ?>
 							<?php printf( __( 'Daily Archives: %s', 'admired' ), '<span>' . get_the_date() . '</span>' ); ?>
 						<?php elseif ( is_month() ) : ?>
-							<?php printf( __( 'Monthly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php printf( __( 'Monthly Archives: %s', 'admired' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'admired' ) ) . '</span>' ); ?>
 						<?php elseif ( is_year() ) : ?>
-							<?php printf( __( 'Yearly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php printf( __( 'Yearly Archives: %s', 'admired' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'admired' ) ) . '</span>' ); ?>
 						<?php else : ?>
 							<?php _e( 'Blog Archives', 'admired' ); ?>
 						<?php endif; ?>
