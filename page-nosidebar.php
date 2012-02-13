6c8d2a35c4634836480d5db28b46c25dcb18afde
Theme Directory <noreply@wordpress.org>
2012-02-13 16:02:59 +0000

    New version of Babylog - 1.4.6

--- page-nosidebar.php
+++ page-nosidebar.php
@@ -18,21 +18,13 @@ get_header(); ?>
 			</div>
 			
 			<div class="entry">
-            	<?php if( wp_list_pages( 'child_of='.$post->ID.'&echo=0' ) ) { ?>
-                	<ul>
-                		<?php wp_list_pages( 'title_li=&child_of='.$post->ID.'&depth=1' ); ?></ul>
-				<?php } ?> 
 				
 				<?php the_content('<p>' . __( 'Read the rest of this entry' , 'babylog' ) . ' &raquo;</p>'); ?>
 				
 				<?php wp_link_pages( array( 'before' => '<p class="clear"><strong>' . __( 'Pages:' , 'babylog' ) . '</strong> ', 'after' => '</p>', 'next_or_number' => 'number' ) ); ?>
 			</div>
-			
-			<?php if( $post->post_parent ) { 
-				echo '<p class="clear"><a href="' . get_permalink( $post->post_parent ) . '">&laquo; ' . get_the_title( $post->post_parent ) . '</a></p>' ; 
-				} ?>
 				
-				<?php edit_post_link( __( 'Edit this entry', 'babylog' ), '<p class="clear">', '</p>'); ?>
+			<?php edit_post_link( __( 'Edit this entry', 'babylog' ), '<p class="clear">', '</p>'); ?>
 			
             <?php if ( comments_open() ) comments_template(); ?>
 		</div>
