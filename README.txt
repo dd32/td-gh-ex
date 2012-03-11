eabf7bac0873d6b39cfda85dc5e04f71e37ba482
Theme Directory <noreply@wordpress.org>
2012-03-11 18:56:53 +0000

    New theme: atheros - 1.0.1

---
+++ README.txt
@@ -0,0 +1,17 @@
+atheros
+
+================================================================================
+
+Version: 1.0.1
+Release date: March 11 2011
+
+Comments, Feedback and Bugs:
+http://www.themes-for-wordpress.net/atheros/
+
+The CSS, XHTML and design is released under GPL:
+http://www.opensource.org/licenses/gpl-license.php
+
+================================================================================
+
+Version: 1.0.1.  - March 11 2011
+First Release
---
+++ footer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage atheros
+ * @since atheros 1.0
+ */
+?>
+	
+	 
+	
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+		<div id="colophon">
+
+<?php
+	/* A sidebar in the footer? Yep. You can can customize
+	 * your footer with four columns of widgets.
+	 */
+	get_sidebar( 'footer' );
+?>
+
+			<div id="site-info">
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div><!-- #site-info -->
+
+		 
+
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+	<div id="leftstripe"></div>
+	<div id="rightstripe"></div>
+	
+	
+</div><!-- #wrapper -->
+
+<div id="footerbottom">
+
+
+	<?php 
+		/**
+		 * This is where the credit for the theme is placed. 
+		 * Credits are showed only at the first page. 
+		 * Please keep the link back to the author's website.
+		 * Seriously, developing this awesome theme took a lot
+		 * of effort and time, weeks and weeks of voluntary unpaid work. I only ask 
+		 * that you retain this link here, and you can use and/or modify the theme
+		 * however you like to.
+		*/
+		?>
+		<?php bloginfo('name'); ?><?php if(is_home() and !is_paged()) { ?> | Design by <a href="http://www.themes-for-wordpress.net/">Themes for Wordpress</a>.net<?php } ?>
+
+</div>
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
