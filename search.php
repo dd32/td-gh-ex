abca323cebd1afef1827fdd358c489eb4db855fd
Theme Directory <noreply@wordpress.org>
2012-06-03 06:56:35 +0000

    New version of Responsive - 1.5.7

--- search.php
+++ search.php
@@ -23,14 +23,13 @@ if ( !defined('ABSPATH')) exit;
         <div id="content" class="grid col-620">
 
 <?php if (have_posts()) : ?>
-            
-            <h6><?php printf(__('Search Results for: %s', 'responsive' ), '<span>' . get_search_query() . '</span>'); ?></h6>
 
 		<?php while (have_posts()) : the_post(); ?>
-          
+        
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                
                 <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'responsive'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h1>
-
+                
                 <div class="post-meta">
                 <?php 
                     printf( __( '<span class="%1$s">Posted on</span> %2$s by %3$s', 'responsive' ),'meta-prep meta-prep-author',
@@ -53,35 +52,18 @@ if ( !defined('ABSPATH')) exit;
                         </span>
                     <?php endif; ?> 
                 </div><!-- end of .post-meta -->
-                                
+                
                 <div class="post-entry">
-                    <?php the_content(__('Read more &#8250;', 'responsive')); ?>
-                    
-                    <?php if ( get_the_author_meta('description') != '' ) : ?>
-                    
-                    <div id="author-meta">
-                    <?php if (function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email'), '80' ); }?>
-                        <div class="about-author"><?php _e('About','responsive'); ?> <?php the_author_posts_link(); ?></div>
-                        <p><?php the_author_meta('description') ?></p>
-                    </div><!-- end of #author-meta -->
-                    
-                    <?php endif; // no description, no author's meta ?>
-                    
+                    <?php the_excerpt(); ?>
                     <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
                 </div><!-- end of .post-entry -->
-                
-                <div class="post-data">
-				    <?php the_tags(__('Tagged with:', 'responsive') . ' ', ', ', '<br />'); ?> 
-					<?php printf(__('Posted in %s', 'responsive'), get_the_category_list(', ')); ?> 
-                </div><!-- end of .post-data -->             
+                           
 
-            <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div>             
+            <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div>               
             </div><!-- end of #post-<?php the_ID(); ?> -->
             
-			<?php comments_template( '', true ); ?>
-            
         <?php endwhile; ?> 
-
+        
         <?php if (  $wp_query->max_num_pages > 1 ) : ?>
         <div class="navigation">
 			<div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ) ); ?></div>
