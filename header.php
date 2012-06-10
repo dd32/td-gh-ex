994edb1e577f1e0813293f191e180b2e921f7974
Theme Directory <noreply@wordpress.org>
2012-06-10 06:55:24 +0000

    New version of QuickPress - 1.6.6

--- header.php
+++ header.php
@@ -1,46 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
-<title><?php wp_title('|', true, right); ?></title> 
-
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" type="text/css" media="screen" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?> 
-
-<?php wp_head(); ?>
-
-</head>
-
-<body <?php body_class(); ?>>
- 
-<div id="page">
-
-<div id="header">
-
-<table width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
-<td width="60%" valign="top" align="left">
-		
-<?php if (is_home()) { ?><h1><?php } ?>
-<a class="blogtitle" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a><?php if (is_home()) { ?></h1><?php } ?><div class="description"><?php bloginfo('description'); ?></div>
- </td> 
-
-<td width="40%" valign="top" align="right">
-
-<?php get_search_form(); ?>
-
- </td>
-</tr></table>
-	 
-	</div>
-
-<div id="adsbar">
-<a href="<?php echo home_url(); ?>">Home</a>
-
-&nbsp; 
-&nbsp;
-
-<a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" type="application/rss+xml"><img src="<?php echo get_template_directory_uri(); ?>/images/rss16.png" title="RSS" alt="RSS" height="16" width="16" class="rssicon" />RSS</a>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<title><?php wp_title('|', true, 'right'); ?></title> 
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" type="text/css" media="screen" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?> 
+
+<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+ 
+<div id="page">
+
+<div id="header">
+
+<table width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
+<td width="60%" valign="top" align="left">
+		
+<?php if (is_home()) { ?><h1><?php } ?>
+<a class="blogtitle" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a><?php if (is_home()) { ?></h1><?php } ?><div class="description"><?php bloginfo('description'); ?></div>
+ </td> 
+
+<td width="40%" valign="top" align="right">
+
+<?php get_search_form(); ?>
+
+ </td>
+</tr></table>
+	 
+	</div>
+
+<div id="adsbar">
+<a href="<?php echo home_url(); ?>">Home</a>
+
+&nbsp; 
+&nbsp;
+
+<a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" type="application/rss+xml"><img src="<?php echo get_template_directory_uri(); ?>/images/rss16.png" title="RSS" class="rssicon" alt="RSS" height="16" width="16" />RSS</a>
 </div>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+QuickPress is a 2-3 columns, widget ready, SEO optimized, fast loading, easy to customize wordpress theme by QuickOnlineTips.com
+
+Navigation: Individual links can be custom added to create a navigational link structure in header.php
