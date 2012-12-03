d8529cebce804fa3933f8baee8825e0363af248a
Theme Directory <noreply@wordpress.org>
2012-12-03 06:07:20 +0000

    New version of Adventure - 1.8

--- comments.php
+++ comments.php
@@ -1,38 +1,55 @@
-<?php
-	
-	if ( post_password_required() ) { ?>
-		<div class="content" ><p class="nocomments">This post is password protected. Enter the password to view comments.</p></div>
-	<?php
-		return;
-	}
-?>
-<?php if ( comments_open() ) : ?>
-<div class="content" >				
-		<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-			<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
-		<?php else : ?>
-			
-		<?php comment_form(); ?>
+<?php if (post_password_required()) { ?>
+    <li><h4 class="nocomments"><?php _e('This post is password protected. Enter the password to view any comments.', 'responsive'); ?></h4></li>
+    
+	<?php return; } ?>
+   
+
+<?php if (have_comments()) : ?>
+<li>
+	<ul class="commentbox">
+        <h4 class="title">Comments</h4>
+    
+        <ol class="commentlist">
+            <?php wp_list_comments('avatar_size=100&type=comment'); ?>
+        </ol>
+	</ul>	
+    
+    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+        <ul class="starsbar"><h6>
+            <span class="left"><?php previous_comments_link(); ?></span>
+            &#9733; &#9733; &#9733; &#9733; &#9733;
+            <span class="right"><?php next_comments_link(); ?></span>
+        </h6></ul> 
+    <?php endif; ?>
+    
+
+
+	<?php if (!empty($comments_by_type['pings'])) : ?>
+        <ul class="commentbox">
+            <h4 class="title">Pings &amp; Trackbacks</h4>
         
-		<?php endif; // If registration required and not logged in ?>
-</div>
+            <ol class="commentlist">
+                <?php wp_list_comments('type=pings&max_depth=<em>'); ?>
+            </ol>
+        </ul>
+    <?php endif; ?>
+
+</li>
 <?php endif; ?>
 
-<?php if ( have_comments() ) : ?>
-<div class="content" >
-	<div class="label">COMMENTS</div>
-		<ol id="comments" class="comment">
-			<?php wp_list_comments('callback=adventure_print_comment'); ?>
-		</ol>
-		<div class="navigation">
-			<div class="alignleft"><?php previous_comments_link() ?></div>
-			<div class="alignright"><?php next_comments_link() ?></div>
-		</div>
-</div>
-<?php else : ?>
-<?php if ( !comments_open() && !is_page() ) : ?>
-<div class="content" ><p>Comments are closed.</p></div>
-<?php endif; ?>
-<?php endif; ?>
+<?php if (comments_open()) : ?>
+<li> 
+    <?php $fields = array(
+			'author' => '<p class="comment-form-author">' . '<label for="author">' . __('Name','responsive') . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+			'<input id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '" size="30" /></p>',
+			'email' => '<p class="comment-form-email"><label for="email">' . __('E-mail','responsive') . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+			'<input id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) . '" size="30" /></p>',
+			'url' => '<p class="comment-form-url"><label for="url">' . __('Website','responsive') . '</label>' .
+			'<input id="url" name="url" type="text" value="' . esc_attr($commenter['comment_author_url']) . '" size="30" /></p>', );
 
- <!-- if ('open' == $post->comment_status) : -->
+    $defaults = array('fields' => apply_filters('comment_form_default_fields', $fields));
+
+    comment_form($defaults); ?>
+
+</li>
+<?php endif; ?>
--- index.php
+++ index.php
@@ -1,91 +1,142 @@
-<?php get_header(); ?>
-<body <?php body_class(); ?> >
-<div id="center">
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php wp_title('&#124;', true, 'right'); ?><?php bloginfo('name'); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<?php if ( get_header_image() == '') : ?><?php else : ?>
-<div class="wpheader"><a href="<?php echo home_url(); ?>/"><img class="left title" title="<?php bloginfo('name'); ?>" src="<?php header_image(); ?>" alt="<?php bloginfo('description');?>" /></a></div>
+<!-- Begin WordPress Header -->
+<?php wp_head(); ?>
+<!-- End WordPress Header -->
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<!-- Setting up The Layout of the Webpage -->
+<ul id="spacing"></ul>
+<ul id="content">
+
+<!-- Searching Code -->
+
+<!-- End Search -->
+
+
+	
+<?php if (adventure_is_sidebar_active('widget')) : ?>
+<!-- The Sidebar -->
+<li id="sidebar"><?php if (!dynamic_sidebar('sidebar')) : ?><?php endif; ?></li>
+<!-- End Sidebar -->
+<?php else : ?>
+<!-- No Sidebar -->
 <?php endif; ?>
 
-<?php if (is_active_sidebar('sidebar') ) : ?>
-<div class="sidebar"><?php if ( !dynamic_sidebar('sidebar') ) : endif; ?></div>
-<?php else : get_sidebar(); ?>
-<?php endif; ?>
-
+    
+<?php $semperfi_404 = false;/* A 404 Error check */?>
+<?php if (is_search()) : ?>
+<!-- Searching Code -->
 <?php if (have_posts()) : ?>
+<li>
+<h4>Your Search "<?php echo get_search_query(); ?>" Returned <?php echo $wp_query->found_posts; ?> Results</h4>
+<p>Your search for "<?php echo get_search_query(); ?>" has returned exactly <?php echo $wp_query->found_posts; ?> results, no more and no less. Hopefully what you're looking for will be found just below on this page, but if you're unable find what you are looking for you may need to use the links "Older Search Results" or "Newer Search Results" to navigate through more pages of results for "<?php echo get_search_query(); ?>." Please keep in mind that if not enough results return, the links for "Older Search Results" and "Newer Search Results" may not appear becuase there is nothing more to show.</p>
+<ul>
 <?php while (have_posts()) : the_post(); ?>
-<div class="content">
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div class="imagez">
-<?php if ( has_post_thumbnail()) : ?><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('page-thumbnail'); ?></a><?php endif; ?>
-<div class="label"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php if ( get_the_title() ) { the_title();} else { echo '(No Title)';} ?></a></div></div>
-<?php the_content('Read the rest of this entry &raquo;'); ?>
-<div class="clearz">
-<?php wp_link_pages(); ?><div class="tags">
-<?php if ( is_home()) : ?>
-<?php if ('open' == $post->comment_status) : ?>
-<?php comments_popup_link('Leave A Comment', '1 Comment', '% Comments'); ?>,
-<?php endif; ?>
-<?php endif; ?>
-Written on <?php the_time('F jS, Y') ?> <?php if ( is_page()) : ?><?php else : ?>, <?php the_category(', ') ?> <?php the_tags('Tags: ', ', ', '<br />'); ?><?php endif; ?>
-</div></div></div></div>
-<?php comments_template(); ?>
+    <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+    <span><?php the_time('M') ?><br/><?php the_time('jS') ?></span><?php if ( get_the_title() ) { the_title();} else { echo '(No Title)';} ?>
+    </a></li>
 <?php endwhile; ?>
-
-
-<?php $max_page = $wp_query->max_num_pages; ?>
-<?php if ( $max_page >=  2 ) : ?>
-<div class="content" >
-<div class="label"><div class="left"><?php next_posts_link('&lt;&lt; Older Entries', 0); ?></div></div>
-<div class="label"><div class="right"><?php previous_posts_link('Newer Entries &gt;&gt;', '0') ?></div></div>
-</div>
-<?php endif; ?>
+</ul>
+</li>
 
 <?php else : ?>
-<div class="content">
-<div class="label">Not Found</div>
-<p>Sorry, but you are looking for something that isn't here.</p>
-<?php get_search_form(); ?>
-</div>
+<li>
+<h2>Your search resulted in nothing being found.</h2>
+</li>
+<?php endif; ?>
+<!-- End Search -->
 
+<?php if (have_posts() != true) : ?>
+<!-- 404 Error -->
+<li>
+	<h4>404 Error</h4>
+</li>
+<?php $semperfi_404 = true;?>
+<!-- End 404 Error -->
 <?php endif; ?>
 
-<?php if ( is_active_sidebar('widget') ) : ?>
-<div class="widgetbox">
-<div class="widget"><?php if ( !dynamic_sidebar('widget1') ) :  endif; ?></div>
-<div class="widget"><?php if ( !dynamic_sidebar('widget2') ) :  endif; ?></div>
-<div class="widget"><?php if ( !dynamic_sidebar('widget3') ) :  endif; ?></div>
-<div class="widget"><?php if ( !dynamic_sidebar('widget4') ) :  endif; ?></div>
-</div>
+<?php elseif (have_posts()) : ?>
+<!-- The Posts or Page -->   
+<?php while (have_posts()) : the_post(); ?>
+<li id="post-<?php the_ID(); ?>" <?php post_class('content'); ?>>
+	<h4><?php if ( is_page()) : else : ?><span><?php the_time('M') ?> <?php the_time('jS') ?></span><?php endif; ?><?php if (is_home() || is_404() || is_category() || is_day() || is_month() || is_year() || is_paged() || is_tag()) : ?><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php endif; ?><?php if ( get_the_title() ) { the_title();} else { echo '(No Title)';} ?><?php if (is_home() || is_404() || is_category() || is_day() || is_month() || is_year() || is_paged() || is_tag()) : ?></a><?php endif; ?></h4>
+	<?php the_content(); ?>
+	
+    <span class="tag"><?php wp_link_pages(); ?></br><?php if (is_single()) : ?>This entry was posted in <?php the_category(', '); the_tags(' and tagged ', ', ', ''); endif;?> by <?php the_author_posts_link(); ?><?php if ( ( is_page() && !comments_open() ) || is_single() ) : ?> and comments are closed.<?php endif; ?></span>
+</li>
+
+<!-- The Comments -->
+<?php if (is_search()) : ?>
+<?php elseif($semperfi_404) : ?>
+<?php elseif ( comments_open() ) : ?>
+<?php comments_template( '', true ); ?>
+<?php elseif ( ( is_page() && comments_open() ) || is_single() ) : ?>
+<?php comments_template( '', true ); ?>
 <?php endif; ?>
+<!-- End Comments -->
 
-<div id="notfooter">
-<p>
-<?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress</a> and the Theme <a href="http://schwarttzy.com/web-design/adventure/">Adventure by Eric Schwarz</a>
-<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-<!-- Theme design by Eric Schwarz - http://schwarttzy.com/?page_id=225 -->
-</p>
+<?php endwhile; ?>
+
+<!-- The Next Link -->
+<?php if (is_attachment()) : ?>
+<li>
+	<h3>
+	<span class="left"><?php previous_image_link( false, '&#8249; Previous Image'); ?></span>
+	<span class="right"><?php next_image_link( false, 'Next Image &#8250;'); ?></span>
+    </h3>
+</li>
+<?php elseif (is_home() || is_404() || is_category() || is_day() || is_month() || is_year() || is_paged() || is_tag()) : ?>
+<li>
+	<h3>
+	<span class="left"><?php next_posts_link('&#8249; Older Posts'); ?></span>
+	<span class="right"><?php previous_posts_link('Newer Posts &#8250;'); ?></span>
+    </h3>
+</li>
+<?php else : ?>
+<li>
+	<h3>
+    <span class="left"><?php previous_post_link('%link', '&#8249; Older Post'); ?></span>
+	<span class="right"><?php next_post_link('%link', 'Newer Post &#8250;'); ?></span>
+	</h3>
+</li>
+<?php endif; ?><!-- End Next Link -->
+
+<?php endif; ?><!-- End Posts or Page -->
+
+<ul id="finishing"></ul>
+</ul>
+
+<div id="footer">
+<p>Good Old Fashioned Hand Written Code by <a href="http://schwarttzy.com/about-2/">Eric J. Schwarz</a> <!-- <?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?> seconds --></p>
 </div>
+<!-- Closing the Layout of the Page with a Finishing Touch. -->
 
-</div>
 
-<div id="endspacer">
-</div>
+<!-- The Navigation Menu -->
+<ul id="navi">
+	<h1 id="fittext3"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a><i><?php bloginfo('description');?></i></h1>
+	<?php if ( has_nav_menu( 'bar' ) ) :  wp_nav_menu( array( 'theme_location' => 'bar', 'depth' => 2 ) ); else : ?>
+	<?php wp_list_pages( 'title_li=&depth=2' ); ?>
+	<?php endif; ?>
+</ul>
+<!-- End Navigation Menu -->
 
-<div id="bar">
-<?php 
-$options = adventure_get_theme_options();
-if ( $options['active'] == 'active' ) {	
-if ( has_nav_menu( 'bar' ) ) :  wp_nav_menu( array( 'theme_location' => 'bar', 'depth' => 2 ) ); else : ?>
-<ul><?php wp_list_pages('title_li=&depth=2'); ?></ul>
-<?php endif; } else {
-if ( has_nav_menu( 'bar' ) ) :  wp_nav_menu( array( 'theme_location' => 'bar', 'depth' => 1 ) ); else : ?>
-<ul><?php wp_list_pages('title_li=&depth=1'); ?></ul>
-<?php endif; }?>
-<div id="title"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></div>
-<div id="slogan"><h2><?php bloginfo('description');?></h2></div>  
-</div>
+<script> if (matchMedia('only screen and (max-width: 700px)').matches) { $('#sidebar').insertBefore('#finishing'); } </script>
 
+<!-- Start of WordPress Footer  -->
 <?php wp_footer(); ?>
+<!-- End of WordPress Footer -->
+
 </body>
 </html>
