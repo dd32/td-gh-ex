9b7df900c4a4360e6ad0654abf6decb33e2fb9f0
Theme Directory <noreply@wordpress.org>
2012-12-16 14:47:19 +0000

    New version of Baris - 1.1

--- search.php
+++ search.php
@@ -3,15 +3,15 @@
 <div id="content">
 
 <?php if ( have_posts() ) : ?>
-	<h1 class="archive"><?php echo $wp_query->found_posts; ?> <?php printf( __( 'Search Results for <strong>%s</strong>', 'baris' ), '' . get_search_query() . '' ); ?></h1>
+	<h1 class="archive"><?php echo $wp_query->found_posts; ?> <?php printf( __( 'Search Results for %s', 'baris' ), '' . get_search_query() . '' ); ?></h1>
 	
 	<?php get_template_part( 'content' ); ?>
 	
 	<?php else : ?>
-	<h1 class="archive"><strong><?php _e( 'No Search Result Found', 'ari' ); ?></strong></h1>
-		<div class="post">
+	<h1 class="archive"><?php _e( 'No Search Result Found', 'baris' ); ?></h1>
+		<article>
 			<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'baris' ); ?></p>
-		</div>
+		</article>
 	<?php endif; ?>
 
 </div>
