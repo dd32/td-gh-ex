37c518e307b9a4d0e0539c929c676862653827aa
Theme Directory <noreply@wordpress.org>
2013-02-10 22:13:40 +0000

    New version of raindrops - 1.008

--- functions.php
+++ functions.php
@@ -3030,7 +3030,7 @@ if(!function_exists("fallback_user_interface_view") ){
 
                     if( image_exists !== '' ){
         <?php
-            if( $url !== 'remove-header' ){
+            if( $raindrops_header_image_uri !== 'remove-header' ){
                     $ratio = $raindrops_header_image_height / $raindrops_header_image_width;
             ?>
                 var ratio = <?php echo $ratio;?>;
@@ -3919,6 +3919,7 @@ if( ! function_exists( 'raindrops_monthly_archive_prev_next_navigation' ) ){
             $thismonth  = mysql2date('m', $wp_query->posts[0]->post_date);
             $unixmonth  = mktime(0, 0 , 0, $thismonth, 1, $thisyear);
             $last_day   = date('t', $unixmonth);
+            $calendar_output = '';
 
             $previous   = $wpdb->get_row("SELECT MONTH(post_date) AS month, YEAR(post_date) AS year FROM $wpdb->posts
                 WHERE post_date < '$thisyear-$thismonth-01'
