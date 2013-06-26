ecefc84874ae3f337fc56f9809edd8b503aad2e4
Theme Directory <noreply@wordpress.org>
2013-06-26 06:25:28 +0000

    New version of activetab - 0.3.8

--- README.md
+++
@@ -1,7 +0,0 @@
-License URI: license.txt
-
-Font Family (Lora): https://www.google.com/fonts#UsePlace:use/Collection:Lora
-Subpattern (Inspiration Geometry): http://subtlepatterns.com/?s=geometry
-Image 1: http://www.gratisography.com/pictures/133_1.jpg
-Image 2: http://www.gratisography.com/pictures/133_1.jpg
-Image 3: http://www.gratisography.com/pictures/166_1.jpg
--- searchform.php
+++ searchform.php
@@ -3,8 +3,8 @@
 	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" class="form-inline" id="searchform" role="search">
 		<fieldset>
 			<input type="search" value="<?php echo get_search_query(); ?>" class="input-medium" id="search" name="s" />
-			<input type="submit" value="<?php esc_attr_e( 'Search', 'activetab' ); ?>" class="btn search-submit-button-real">
-			<span class="btn search-submit-button hide nowrap"><i class="icon-search"></i> <?php esc_attr_e( 'Search', 'activetab' ); ?></span>
+			<input type="submit" value="<?php echo esc_attr( __( 'Search', 'activetab' ) ); ?>" class="btn search-submit-button-real">
+			<span class="btn search-submit-button hide nowrap"><i class="icon-search"></i> <?php echo esc_attr( __( 'Search', 'activetab' ) ); ?></span>
 		</fieldset>
 	</form>
 </div>
