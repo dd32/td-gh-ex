07538e4c0246d7c5f8785cadbe774591a4d41c1a
Theme Directory <noreply@wordpress.org>
2013-07-05 16:26:36 +0000

    New version of QuickPic - 1.3.11

--- footer.php
+++ footer.php
@@ -1,61 +1,61 @@
-  <!-- Bottom Box --> 
-<div id="bottombox">
-
-<div id="adbox">
-
-<!-- Search --> 
-<?php get_search_form(); ?>
-  
-</div>
-
-<div style="clear:both;"></div>
-
-<div id="boxes">
-
- <div id="box1">
- 
-<!-- Widgetized sidebar 1 --> 
-<ul>
-  <li>
- <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
- <?php dynamic_sidebar( 'sidebar-1' ); ?>
-<?php endif; ?> 
-  </li>
-</ul> 
-
-</div>
-
-<div id="box2"> 
- 
-<!-- Widgetized sidebar 2 --> 
-<ul>
-  <li>
- <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
- <?php dynamic_sidebar( 'sidebar-2' ); ?>
-<?php endif; ?> 
-  </li>
-</ul>
- 
-</div>
-
-
-<div style="clear:both;"></div>
-
-</div><!-- /Boxes --> 
-
-
- 
-</div><!-- /Bottom Box --> 
-
-<div id="footer"> 
-<p>
-
-&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All Rights Reserved.
-<br /> 
-<a href="http://wordpress.org/extend/themes/quickpic" title="QuickPic">QuickPic</a> Design.
- </p>
-</div>
-</div>
-		<?php wp_footer(); ?> 
-</body>
+  <!-- Bottom Box --> 
+<div id="bottombox">
+
+<div id="adbox">
+
+<!-- Search --> 
+<?php get_search_form(); ?>
+  
+</div>
+
+<div style="clear:both;"></div>
+
+<div id="boxes">
+
+ <div id="box1">
+ 
+<!-- Widgetized sidebar 1 --> 
+<ul>
+  <li>
+ <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+ <?php dynamic_sidebar( 'sidebar-1' ); ?>
+<?php endif; ?> 
+  </li>
+</ul> 
+
+</div>
+
+<div id="box2"> 
+ 
+<!-- Widgetized sidebar 2 --> 
+<ul>
+  <li>
+ <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+ <?php dynamic_sidebar( 'sidebar-2' ); ?>
+<?php endif; ?> 
+  </li>
+</ul>
+ 
+</div>
+
+
+<div style="clear:both;"></div>
+
+</div><!-- /Boxes --> 
+
+
+ 
+</div><!-- /Bottom Box --> 
+
+<div id="footer"> 
+<p>
+
+&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All Rights Reserved.
+<br /> 
+<a href="http://wordpress.org/extend/themes/quickpic" title="QuickPic">QuickPic</a> Design.
+ </p>
+</div>
+</div>
+		<?php wp_footer(); ?> 
+</body>
 </html>
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
+
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title(); ?></title> 
 <?php load_theme_textdomain( 'quickpic', get_template_directory() . '/languages' ); ?>  
@@ -12,21 +13,17 @@
 </head>
 
 <body <?php body_class(); ?>>
- 
+
 <div id="page">
 <div id="header">
-		
+
 <?php if (is_home()) { ?><h1><?php } ?>
 <a class="blogtitle" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a><?php if (is_home()) { ?></h1><?php } ?><div class="description"><?php bloginfo('description'); ?></div>
-  
 	</div>
 
-<div id="navbar">
-<a href="<?php echo home_url(); ?>">Home</a>
-&nbsp; 
-&nbsp;
-<!-- RSS Links --> 
-<a   href="<?php bloginfo('rss2_url'); ?>" rel="alternate" type="application/rss+xml"><img src="<?php echo get_template_directory_uri(); ?>/images/rss16.png" title="RSS" alt="RSS" class="rssicon"  />RSS</a>
-&nbsp;
-&nbsp;
- </div>
+<div id="menu-header">
+  <!-- Widgetized Nav -->  
+ <?php if ( is_active_sidebar( 'nav-menu' ) ) : ?>
+ <?php dynamic_sidebar( 'nav-menu' ); ?>
+<?php endif; ?> 
+</div>
