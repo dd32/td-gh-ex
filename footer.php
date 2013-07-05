1198f5c70ae384d794acc5e9e56c3a1b33c9ea62
Theme Directory <noreply@wordpress.org>
2013-07-05 16:25:50 +0000

    New version of QuickChic - 1.3.6

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<div id="footer"> 
-	<p>		
-&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All Rights Reserved
-|
-<a href="http://wordpress.org/extend/themes/quickchic" title="QuickChic" >QuickChic</a> Design.
-</p>
-</div>
-</div>
-		<?php wp_footer(); ?>
-</body>
+<div id="footer"> 
+	<p>		
+&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All Rights Reserved
+|
+<a href="http://wordpress.org/extend/themes/quickchic" title="QuickChic" >QuickChic</a> Design.
+</p>
+</div>
+</div>
+		<?php wp_footer(); ?>
+</body>
 </html>
--- functions.php
+++ functions.php
@@ -1,40 +1,52 @@
-<?php
-/* Register sidebar widgets */
-function quickchic_widgets_init() {
-register_sidebar(array(
-        'name' => __( 'Sidebar 1', 'quickchic' ),
-        'id' => 'sidebar-1',
-        'before_widget' => '',
-        'after_widget' => '',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>',
-    ));
-register_sidebar(array(
-        'name' => __( 'Sidebar 2', 'quickchic' ),
-        'id' => 'sidebar-2',
-        'before_widget' => '',
-        'after_widget' => '',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>',
-    ));  
-}
-add_action( 'init', 'quickchic_widgets_init' );
-
-/* Add feeds */
-add_theme_support('automatic-feed-links'); 
- 
-/* Title filter */
-function quick_title( $title ) {
-    // Get the Site Name
-    $site_name = get_bloginfo( 'name' );
-    // Prepend it to the default output
-    $filtered_title = $site_name . $title;
-    // Return the modified title
-    return $filtered_title;
-}
-add_filter( 'wp_title', 'quick_title');
-
-/* Content width */
-if ( ! isset( $content_width ) )
-	$content_width = 540;
+<?php
+/* Register sidebar widgets */
+function quickchic_widgets_init() {
+register_sidebar(array(
+        'name' => __( 'Sidebar 1', 'quickchic' ),
+        'id' => 'sidebar-1',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+register_sidebar(array(
+        'name' => __( 'Sidebar 2', 'quickchic' ),
+        'id' => 'sidebar-2',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));  
+}
+add_action( 'init', 'quickchic_widgets_init' );
+
+/* Add feeds */
+add_theme_support('automatic-feed-links'); 
+ 
+
+/* Custom Background */
+$args = array(
+	'default-color' => 'FFFFFF',
+	'default-image' => get_template_directory_uri() . '/images/background.jpg',
+);
+add_theme_support( 'custom-background', $args );
+
+// post thumbnails
+add_theme_support('post-thumbnails');
+
+
+/* Title filter */
+function quick_title( $title ) {
+    // Get the Site Name
+    $site_name = get_bloginfo( 'name' );
+    // Prepend it to the default output
+    $filtered_title = $site_name . $title;
+    // Return the modified title
+    return $filtered_title;
+}
+add_filter( 'wp_title', 'quick_title');
+
+/* Content width */
+if ( ! isset( $content_width ) )
+	$content_width = 540;
 ?>
