41aaf68b4eaa3c032742cc501dfea93f063450f9
Theme Directory <noreply@wordpress.org>
2013-09-02 20:25:11 +0000

    New version of discover - 1.7.12

--- functions.php
+++ functions.php
@@ -214,7 +214,8 @@ add_filter('get_comments_number', 'discover_comment_count', 0);
 function discover_comment_count( $count ) {
 	if ( ! is_admin() ) {
 	global $id;
-	$comments_by_type = &separate_comments(get_comments('status=approve&post_id=' . $id));
+	$get_comments_status= get_comments('status=approve&post_id=' . $id);
+	$comments_by_type = separate_comments($get_comments_status);
 	return count($comments_by_type['comment']);
 } else {
 return $count;
@@ -319,21 +320,20 @@ function discover_widgets_init() {
 }
 if ( ! function_exists( 'discover_posted_on' ) ) :
 /**
- * Prints HTML with meta information for the current post—date/time and author.
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own discover_posted_on to override in a child theme
+ *
+ * @since Twenty Eleven 1.0
  */
 function discover_posted_on() {
-	printf( __( '%2$s <span class="meta-sep">by</span> %3$s', 'discover' ),
-		'meta-prep meta-prep-author',
-		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
-			get_permalink(),
-			esc_attr( get_the_time() ),
-			get_the_date()
-		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
-			get_author_posts_url( get_the_author_meta( 'ID' ) ),
-			sprintf( esc_attr__( 'View all posts by %s', 'discover' ), get_the_author() ),
-			get_the_author()
-		)
+	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'discover' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		esc_attr( sprintf( __( 'View all posts by %s', 'discover' ), get_the_author() ) ),
+		get_the_author()
 	);
 }
 endif;
