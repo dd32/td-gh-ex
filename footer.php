7cb8c0b9a09e1d8ab145b769f852c614d35b0700
Theme Directory <noreply@wordpress.org>
2013-09-16 16:55:09 +0000

    New version of Intuition - 1.0.2

--- footer.php
+++ footer.php
@@ -12,10 +12,7 @@
 					<div id="footermenu" class="footermenu">
 						<?php wp_nav_menu(array('menu_class' => 'menu-footer', 'theme_location' => 'footer_menu', 'depth' => '1', 'fallback_cb' => false)); ?>
 					</div>
-					&copy; <?php bloginfo('name'); ?> 
-					Intuition WordPress Theme <?php if(cpotheme_get_option('cpo_general_credit') == 1) echo ', Copyright &copy; '.date('Y').' <a href="http://www.cpothemes.com">CPOThemes</a>.'; ?>
-					<br/>
-					Intuition WordPress Theme is licensed under the GPL.
+					<?php echo 'Copyright &copy; '.date('Y'); ?> <?php bloginfo('name'); if(cpotheme_get_option('cpo_general_credit') == 1) echo ', Intuition WordPress Theme by <a href="http://www.cpothemes.com">CPOThemes</a>.'; ?>
 				</div>
 			</footer>
 			<div class="clear"></div>
