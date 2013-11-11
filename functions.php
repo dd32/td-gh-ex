ae15e458a3e4ae2f61b514dfd2200830da421328
Theme Directory <noreply@wordpress.org>
2013-11-11 11:02:26 +0000

    New version of Appointway - 1.0.4

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once get_template_directory() . '/functions/inkthemes-functions.php';
 $functions_path = get_template_directory() . '/functions/';
 /* These files build out the options interface.  Likely won't need to edit these. */
@@ -6,27 +7,33 @@ require_once ($functions_path . 'admin-functions.php');  // Custom functions and
 require_once ($functions_path . 'admin-interface.php');  // Admin Interfaces 
 require_once ($functions_path . 'theme-options.php');   // Options panel settings and custom settings
 require_once ($functions_path . 'themes-page.php');  // InkThmes Theme Page 
- ?>
+?>
 <?php
+
 /* ----------------------------------------------------------------------------------- */
 /* jQuery Enqueue */
 /* ----------------------------------------------------------------------------------- */
+
 function appointway_wp_enqueue_scripts() {
     if (!is_admin()) {
-        wp_enqueue_script('jquery');		
+        wp_enqueue_script('jquery');
         wp_enqueue_script('appointway-ddsmoothmenu', get_template_directory_uri() . '/js/ddsmoothmenu.js', array('jquery'));
         wp_enqueue_script('appointway-custom', get_template_directory_uri() . '/js/custom.js', array('jquery'));
     } elseif (is_admin()) {
+        
     }
 }
+
 add_action('wp_enqueue_scripts', 'appointway_wp_enqueue_scripts');
 /* ----------------------------------------------------------------------------------- */
 /* Custom Jqueries Enqueue */
 /* ----------------------------------------------------------------------------------- */
-function appointway_custom_jquery(){
+
+function appointway_custom_jquery() {
     wp_enqueue_script('mobile-menu', get_template_directory_uri() . "/js/mobile-menu.js", array('jquery'));
 }
-add_action('wp_footer','appointway_custom_jquery');
+
+add_action('wp_footer', 'appointway_custom_jquery');
 //Front Page Rename
 $get_status = appointway_get_option('re_nm');
 $get_file_ac = get_template_directory() . '/front-page.php';
@@ -39,28 +46,37 @@ if ($get_status === 'off' && file_exists($get_file_ac)) {
 if ($get_status === 'on' && file_exists($get_file_dl)) {
     rename("$get_file_dl", "$get_file_ac");
 }
+
 //
 function appointway_get_option($name) {
     $options = get_option('appointway_options');
     if (isset($options[$name]))
         return $options[$name];
 }
+
 //
 function appointway_update_option($name, $value) {
     $options = get_option('appointway_options');
     $options[$name] = $value;
     return update_option('appointway_options', $options);
 }
+
 //
 function appointway_delete_option($name) {
     $options = get_option('appointway_options');
     unset($options[$name]);
     return update_option('appointway_options', $options);
 }
+
 //Enqueue comment thread js
 function appointway_enqueue_scripts() {
     if (is_singular() and get_site_option('thread_comments')) {
         wp_print_scripts('comment-reply');
     }
 }
+
+//Add plugin notification 
+require_once(get_template_directory() . '/functions/plugin-activation.php');
+require_once(get_template_directory() . '/functions/inkthemes-plugin-notify.php');
+add_action('tgmpa_register', 'inkthemes_plugins_notify');
 ?>
