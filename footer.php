1767dea4c38060d2b3fbe11eb66e342939428927
Theme Directory <noreply@wordpress.org>
2013-12-09 20:31:28 +0000

    New version of application - 1.3.9

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
 				
 				<span id="copyright"><?php _e( 'Copyright ', 'application' ); ?><?php echo date( 'Y' ); ?>
  <?php echo get_option('footer_cr') ?> | <?php _e( 'Powered by', 'application' ); ?> <a href="http://www.wordpress.org">WordPress</a> | application theme by <a href="http://www.themeszen.com">themeszen</a></span>
-				<span id="follow-us"><?php _e( 'Follow us: ', 'application' ); ?><a href="<?php echo get_option('footer_facebook') ?>">Facebook</a> | <a href="<?php echo get_option('footer_twitter') ?> ">Twitter</a></span>
+				<span id="follow-us"><?php _e( 'Follow us: ', 'application' ); ?><a href="<?php echo of_get_option('footer_facebook') ?>">Facebook</a> | <a href="<?php echo of_get_option('footer_twitter') ?> ">Twitter</a></span>
 			
 			</div>
 	
