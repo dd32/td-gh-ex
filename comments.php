4d0840b47c81f6ee4780ed0ca57bfb4bb2a174c2
Theme Directory <noreply@wordpress.org>
2013-12-14 20:19:36 +0000

    New version of Customizr - 3.1.3

--- comments.php
+++ comments.php
@@ -15,14 +15,28 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( have_comments() )
-	echo apply_filters( 'tc_comment_separator', '<hr class="featurette-divider '. current_filter() .'">' );
 ?>
+<?php if ( have_comments() ) : ?>
 
-<div id="comments" class="<?php echo apply_filters( 'tc_comments_wrapper_class' , 'comments-area' ) ?>" >
+	<?php echo apply_filters( 'comment_separator', '<hr class="featurette-divider '.current_filter().'">' ); ?>
+
+<?php endif; ?>
+
+<div id="comments" class="comments-area">
+	
 	<?php 
-		comment_form( array( 'title_reply' => __( 'Leave a Comment' , 'customizr' ) ) );
-		if ( have_comments() )
-			do_action ( '__comment' );
+	$args = array(
+	  'title_reply'       => __( 'Leave a Comment' , 'customizr' ),
+	);
+
+	comment_form($args); 
+
 	?>
+
+	<?php if ( have_comments() ) : ?>
+
+		<?php do_action ( '__comment' );?>
+
+	<?php endif; // have_comments() ?>
+
 </div><!-- #comments .comments-area -->
--- sidebar-right.php
+++ sidebar-right.php
@@ -10,3 +10,4 @@
 dynamic_sidebar( 'right' );
 
 ?>
+
