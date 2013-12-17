a5a3c0f5f39b57f837ff75634f8c2c65352e2b07
Theme Directory <noreply@wordpress.org>
2013-12-17 21:37:56 +0000

    New version of Nova Lite - 1.0.3

--- footer.php
+++ footer.php
@@ -10,16 +10,19 @@
 		<div class="row" >
              
 			<div class="span12 copyright" >
-    
-				<?php if (novalite_setting('wip_copyright_text')): ?>
-                   <?php echo stripslashes(novalite_setting('wip_copyright_text')); ?>
-                <?php else: ?>
-                  <?php _e('Copyright','wip'); ?> <?php echo get_bloginfo("name"); ?> <?php echo date("Y"); ?> 
-                <?php endif; ?> 
-                | <?php _e('Theme by','wip'); ?> <a href="http://www.themeinprogress.com/" target="_blank">Theme in Progress</a> |
-                <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wip' ) ); ?>" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wip' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wip' ), 'WordPress' ); ?></a>
-    
-			</div>
+            
+                <p>
+                    <?php if (novalite_setting('wip_copyright_text')): ?>
+                       <?php echo stripslashes(novalite_setting('wip_copyright_text')); ?>
+                    <?php else: ?>
+                      <?php _e('Copyright','wip'); ?> <?php echo get_bloginfo("name"); ?> <?php echo date("Y"); ?> 
+                    <?php endif; ?> 
+                    | <?php _e('Theme by','wip'); ?> <a href="http://www.themeinprogress.com/" target="_blank">Theme in Progress</a> |
+                    <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wip' ) ); ?>" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wip' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wip' ), 'WordPress' ); ?></a>
+                
+                </p>
+			
+            </div>
                 
 		</div>
 	</div>
