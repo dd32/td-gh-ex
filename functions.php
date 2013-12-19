6a054676d43221a5a5f19d08ae26d6e9efd9e69d
Theme Directory <noreply@wordpress.org>
2013-12-19 07:58:39 +0000

    New version of Rockers - 2.1

--- functions.php
+++ functions.php
@@ -34,114 +34,129 @@ add_action( 'after_setup_theme', 'rockers_setup' );
 
 require( get_template_directory() . '/inc/custom-header.php' );
 
-function rockers_scripts_styles() {
-    if (is_singular() && comments_open() && get_option( 'thread_comments' ) )
-        wp_enqueue_script( 'comment-reply' );
-    wp_enqueue_script( 'rockers-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
-    if ( 'off' !== _x( 'on', 'Source Sans Pro font: on or off', 'rockers' ) ) {
-        $subsets = 'latin,latin-ext';
-        $subset = _x( 'no-subset', 'Source Sans Pro font: add new subset (greek, cyrillic, vietnamese)', 'rockers' );
-
-        if ( 'cyrillic' == $subset )
-            $subsets .= ',cyrillic,cyrillic-ext';
-        elseif ( 'greek' == $subset )
-            $subsets .= ',greek,greek-ext';
-        elseif ( 'vietnamese' == $subset )
-            $subsets .= ',vietnamese';
-        $protocol = is_ssl() ? 'https' : 'http';
-        $query_args = array(
-            'family' => 'Source+Sans+Pro:400italic,700italic,400,700',
-            'subset' => $subsets,
-        );
-        wp_enqueue_style( 'rockers-fonts', add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" ), array(), null );
+if ( ! function_exists( 'rockers_scripts_styles' ) ) : 
+    function rockers_scripts_styles() {
+        if (is_singular() && comments_open() && get_option( 'thread_comments' ) )
+            wp_enqueue_script( 'comment-reply' );
+        wp_enqueue_script( 'rockers-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
+        if ( 'off' !== _x( 'on', 'Source Sans Pro font: on or off', 'rockers' ) ) {
+            $subsets = 'latin,latin-ext';
+            $subset = _x( 'no-subset', 'Source Sans Pro font: add new subset (greek, cyrillic, vietnamese)', 'rockers' );
+            if ( 'cyrillic' == $subset )
+                $subsets .= ',cyrillic,cyrillic-ext';
+            elseif ( 'greek' == $subset )
+                $subsets .= ',greek,greek-ext';
+            elseif ( 'vietnamese' == $subset )
+                $subsets .= ',vietnamese';
+            $protocol = is_ssl() ? 'https' : 'http';
+            $query_args = array(
+                'family' => 'Source+Sans+Pro:400italic,700italic,400,700',
+                'subset' => $subsets,
+            );
+            wp_enqueue_style( 'rockers-fonts', add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" ), array(), null );
+        }
+        wp_enqueue_style( 'rockers-style', get_stylesheet_uri() );
     }
-    wp_enqueue_style( 'rockers-style', get_stylesheet_uri() );
-}
-add_action( 'wp_enqueue_scripts', 'rockers_scripts_styles' );
+    add_action( 'wp_enqueue_scripts', 'rockers_scripts_styles' );
+endif;
 
-function rockers_wp_title( $title, $sep ) {
-    global $paged, $page;
-    if( is_feed() )
+if ( ! function_exists( 'rockers_wp_title' ) ) : 
+    function rockers_wp_title( $title, $sep ) {
+        global $paged, $page;
+        if( is_feed() )
+            return $title;
+        $title .= get_bloginfo( 'name' );
+        $site_description = get_bloginfo( 'description', 'display' );
+        if( $site_description && ( is_home() || is_front_page() ) )
+            $title = "$title $sep $site_description";
+        if( $paged >= 2 || $page >= 2 )
+            $title = "$title $sep " . sprintf( __( 'Page %s', 'rockers' ), max( $paged, $page ) );
         return $title;
-    $title .= get_bloginfo( 'name' );
-    $site_description = get_bloginfo( 'description', 'display' );
-    if( $site_description && ( is_home() || is_front_page() ) )
-        $title = "$title $sep $site_description";
-    if( $paged >= 2 || $page >= 2 )
-        $title = "$title $sep " . sprintf( __( 'Page %s', 'rockers' ), max( $paged, $page ) );
-    return $title;
-}
-add_filter( 'wp_title', 'rockers_wp_title', 10, 2 );
-
-function rockers_page_menu_args( $args ) {
-    $args['show_home'] = true;
-    return $args;
-}
-add_filter( 'wp_page_menu_args', 'rockers_page_menu_args' );
-
-function rockers_excerpt_length( $length ) {
-    return 35;
-}
-add_filter( 'excerpt_length', 'rockers_excerpt_length' );
-
-function rockers_continue_reading_link() {
-    global $post;
-    return ' <p>' . __( '<a href="'. get_permalink( $post->ID ) . '" title="' . __( 'Continue reading', 'rockers' ) . '">Continue reading <span class="meta-nav">&rarr;</span></a>', 'rockers' ) . '</p>';
-}
-
-function rockers_auto_excerpt_more( $more ) {
-    return ' &hellip;' . rockers_continue_reading_link();
-}
-add_filter( 'excerpt_more', 'rockers_auto_excerpt_more' );
-
-function rockers_custom_excerpt_more( $output ) {
-    if ( has_excerpt() && ! is_attachment() ) {
-        $output .= rockers_continue_reading_link();
     }
-    return $output;
-}
-add_filter( 'get_the_excerpt', 'rockers_custom_excerpt_more' );
+    add_filter( 'wp_title', 'rockers_wp_title', 10, 2 );
+endif;
 
-function rockers_widgets_init() {
-    register_sidebar( 
-        array(
-            'name' => __( 'Main Sidebar', 'rockers' ),
-            'id' => 'sidebar-1',
-            'description' => __( 'This widget will appears on posts and pages except on our optional Front page template.', 'rockers' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>',
-        ) 
-    );
+if ( ! function_exists( 'rockers_page_menu_args' ) ) : 
+    function rockers_page_menu_args( $args ) {
+        $args['show_home'] = true;
+        return $args;
+    }
+    add_filter( 'wp_page_menu_args', 'rockers_page_menu_args' );
+endif;
 
-    register_sidebar( 
-        array(
-            'name' => __( 'First Front Page Widget Area', 'rockers' ),
-            'id' => 'sidebar-2',
-            'description' => __( 'This widget will appears when using the optional Front page template with a page set as Static Front page.', 'rockers' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>',
-        ) 
-    );
+if ( ! function_exists( 'rockers_excerpt_length' ) ) : 
+    function rockers_excerpt_length( $length ) {
+        return 35;
+    }
+    add_filter( 'excerpt_length', 'rockers_excerpt_length' );
+endif;
 
-    register_sidebar( 
-        array(
-            'name' => __( 'Second Front Page Widget Area', 'rockers' ),
-            'id' => 'sidebar-3',
-            'description' => __( 'This widget will appears when using the optional Front Page template with a page set as Static Front Page.', 'rockers' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>',
-        ) 
-    );
-}
-add_action( 'widgets_init', 'rockers_widgets_init' );
+if ( ! function_exists( 'rockers_continue_reading_link' ) ) : 
+    function rockers_continue_reading_link() {
+        global $post;
+        return '<p><a href="'. get_permalink( $post->ID ) .'" title="' . __('continue to read', 'rockers') . '">' . __('Continue to read...', 'rockers') . '</a></p>';
+    }
+endif;
 
-if ( ! function_exists( 'rockers_content_nav' ) ) :
+if ( ! function_exists( 'rockers_auto_excerpt_more' ) ) : 
+    function rockers_auto_excerpt_more( $more ) {
+        return ' &hellip;' . rockers_continue_reading_link();
+    }
+    add_filter( 'excerpt_more', 'rockers_auto_excerpt_more' );
+endif;
+
+if ( ! function_exists( 'rockers_custom_excerpt_more' ) ) : 
+    function rockers_custom_excerpt_more( $output ) {
+        if ( has_excerpt() && ! is_attachment() ) {
+            $output .= rockers_continue_reading_link();
+        }
+        return $output;
+    }
+    add_filter( 'get_the_excerpt', 'rockers_custom_excerpt_more' );
+endif;
+
+if ( ! function_exists( 'rockers_widgets_init' ) ) : 
+    function rockers_widgets_init() {
+        register_sidebar( 
+            array(
+                'name' => __( 'Main Sidebar', 'rockers' ),
+                'id' => 'sidebar-1',
+                'description' => __( 'This widget will appears on posts and pages except on our optional Front page template.', 'rockers' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget' => '</aside>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title' => '</h3>',
+            ) 
+        );
+
+        register_sidebar( 
+            array(
+                'name' => __( 'First Front Page Widget Area', 'rockers' ),
+                'id' => 'sidebar-2',
+                'description' => __( 'This widget will appears when using the optional Front page template with a page set as Static Front page.', 'rockers' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget' => '</aside>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title' => '</h3>',
+            ) 
+        );
+
+        register_sidebar( 
+            array(
+                'name' => __( 'Second Front Page Widget Area', 'rockers' ),
+                'id' => 'sidebar-3',
+                'description' => __( 'This widget will appears when using the optional Front Page template with a page set as Static Front Page.', 'rockers' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget' => '</aside>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title' => '</h3>',
+            ) 
+        );
+    }
+    add_action( 'widgets_init', 'rockers_widgets_init' );
+endif;
+
+if ( ! function_exists( 'rockers_content_nav' ) ) : 
     function rockers_content_nav($nav_id) {
         global $wp_query;
         if ( $wp_query->max_num_pages > 1 ) : ?>
