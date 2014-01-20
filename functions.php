d8ab411e3ecac113fc7c0ae6d7266ffe6c1cfdaa
Theme Directory <noreply@wordpress.org>
2014-01-20 18:48:27 +0000

    New theme: Unite - 1.0

---
+++ functions.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * _s functions and definitions
+ *
+ * @package unite
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 730; /* pixels */
+}
+
+if ( ! function_exists( 'unite_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function unite_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on _s, use a find and replace
+	 * to change 'unite' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'unite', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'unite-featured', 730, 410, true );
+	add_image_size( 'tab-small', 60, 60 , true); // Small Thumbnail
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'unite' ),
+		'footer-links' => __( 'Footer Links', 'unite' ) // secondary nav in footer
+	) );
+
+	// Enable support for Post Formats.
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+
+	// Setup the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'unite_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif; // unite_setup
+add_action( 'after_setup_theme', 'unite_setup' );
+
+/**
+ * Register widgetized area and update sidebar with default widgets.
+ */
+function unite_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'unite' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar(array(
+    	'id' => 'home1',
+    	'name' => 'Homepage Widget 1',
+    	'description' => 'Used only on the homepage page template.',
+    	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    	'after_widget' => '</div>',
+    	'before_title' => '<h3 class="widgettitle">',
+    	'after_title' => '</h3>',
+    ));
+
+    register_sidebar(array(
+      'id' => 'home2',
+      'name' => 'Homepage Widget 2',
+      'description' => 'Used only on the homepage page template.',
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3 class="widgettitle">',
+      'after_title' => '</h3>',
+    ));
+
+    register_sidebar(array(
+      'id' => 'home3',
+      'name' => 'Homepage Widget 3',
+      'description' => 'Used only on the homepage page template.',
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3 class="widgettitle">',
+      'after_title' => '</h3>',
+    ));	
+
+    register_widget( 'unite_popular_posts_widget' );
+}
+add_action( 'widgets_init', 'unite_widgets_init' );
+
+include(get_template_directory() . "/inc/popular-posts-widget.php");
+
+/**
+ * adding the unite search form (created in extra.php)
+ */
+
+add_filter( 'get_search_form', 'unite_wpsearch' );
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+function unite_scripts() {
+
+    wp_enqueue_style( 'unite-bootstrap', get_template_directory_uri() . '/inc/css/bootstrap.css' );
+
+    wp_enqueue_style( 'unite-icons', get_template_directory_uri().'/inc/css/font-awesome.min.css' );
+
+	wp_enqueue_style( 'unite-style', get_stylesheet_uri() );
+
+	wp_enqueue_script('unite-bootstrapjs', get_template_directory_uri().'/inc/js/bootstrap.js', array('jquery') );
+
+	wp_enqueue_script( 'unite-bootstrapwp', get_template_directory_uri() . '/inc/js/bootstrap-wp.js', array('jquery') );
+
+	wp_enqueue_script( 'unite-skip-link-focus-fix', get_template_directory_uri() . '/inc/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'unite_scripts' );
+
+/*
+ * Helper function to return the theme option value. If no value has been saved, it returns $default.
+ * Needed because options are saved as serialized strings.
+ *
+ * This code allows the theme to work without errors if the Options Framework plugin has been disabled.
+ */
+if ( !function_exists( 'of_get_option' ) ) {
+function of_get_option($name, $default = false) {
+	$optionsframework_settings = get_option('optionsframework');
+	// Gets the unique option id
+	$option_name = $optionsframework_settings['id'];
+	if ( get_option($option_name) ) {
+		$options = get_option($option_name);
+	}
+	if ( isset($options[$name]) ) {
+		return $options[$name];
+	} else {
+		return $default;
+	}
+}
+}
+
+/**
+ * Implement the Custom Header feature.
+ */
+// require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load custom nav walker
+ */
+
+require get_template_directory() . '/inc/navwalker.php';
+
---
+++ page-homepage.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area col-sm-12 col-md-12">
+		<main id="main" class="site-main" role="main">
+			
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'unite' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+					<?php edit_post_link( __( 'Edit', 'unite' ), '<footer class="entry-meta"><i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span></footer>' ); ?>
+				</article><!-- #post-## -->
+
+					<div class="home-widget-area">
+
+						<div class="col-sm-6 col-md-4 home-widget">
+							<?php dynamic_sidebar('home1'); ?>
+						</div>
+
+						<div class="col-sm-6 col-md-4 home-widget">	
+							<?php dynamic_sidebar('home2'); ?>
+						</div>	
+						
+						<div class="col-sm-6 col-md-4 home-widget">
+							<?php dynamic_sidebar('home3');  ?>
+						</div>	
+						
+					</div>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
