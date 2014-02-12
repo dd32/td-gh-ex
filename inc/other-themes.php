c4900fd14430079549360669e59d5e90b5a2eeb2
Theme Directory <noreply@wordpress.org>
2014-02-12 09:42:25 +0000

    New version of Aura - 1.0.9

--- inc/other-themes.php
+++ inc/other-themes.php
@@ -4,7 +4,7 @@
 
 <div id="theme">
 <img class="screenshot" src="<?php echo get_template_directory_uri() . '/img/tautira_banner@2x.png'; ?>" width="584">
-<div id="name"><b>[NEW]</b> Tautira</div>
+<div id="name">Tautira Premium Theme</div>
 <div id="info">Tautira is a Premium Business Theme - it's very easy to create a professional website with it. We integrated Jetpack by WordPress.com for the first time in one of our themes, so it gets even more powerful. Tautira looks great on every device because of its Responsive Design.
 
 <p><a href="http://fimply.de/tautira" target="_blank">Go to the Fimply Store</a></p></div>
