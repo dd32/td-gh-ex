bb2d2dce0d35ad0fe6e869bd1384e480b480bdae
Theme Directory <noreply@wordpress.org>
2014-02-19 19:59:02 +0000

    New version of agency - 1.2.4

--- home.php
+++ home.php
@@ -1,8 +1,34 @@
-<?php if ( 'posts' == get_option( 'show_on_front' ) ) {
-    include( get_home_template() );
-} else { ?>
-
 <?php get_header(); ?>
+
+<?php if(!is_front_page()) { ?>
+
+	<div id="subhead_container">
+		
+		<div class="row">
+
+		<div class="large-12 columns">
+	
+			
+<h1><?php if ( is_category() ) {
+		single_cat_title();
+		} elseif (is_tag() ) {
+		echo (__( 'Archives for ', 'agency' )); single_tag_title();
+		} elseif (is_author() ) {
+    $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));		
+		echo (__( 'Archives for ', 'agency' )); echo $curauth->nickname;		
+	} elseif (is_archive() ) {
+		echo (__( 'Archives for ', 'agency' )); single_month_title(' ', true);
+	} else {
+		wp_title('',true);
+	} ?></h1>
+			
+			</div>	
+			
+	</div></div>
+	
+<?php } ?>
+
+<?php if(is_front_page()) { ?>
 
 	<!--welcome-->
 	<div id="welcome_container">
@@ -40,14 +66,31 @@
 		
 	<div class="row">		
 				
-		<?php get_template_part( 'element-boxes', 'index' ); ?>
+		<?php get_template_part( 'element-boxes', 'home' ); ?>
 		
 </div><!--row end-->			
 		
 		</div><!--box-container end-->
 			
 			<div class="clear"></div>
+	<?php } ?> 
+<!--content-->
+
+		<div class="row" id="content_container">
+				
+	<!--left col--><div class="large-8 columns">
+	
+		<div id="left-col">
+			
+			<?php get_template_part( 'loop', 'home' ); ?>
+
+	</div> <!--left-col end-->
+</div> <!--column end-->
+
+<?php get_sidebar(); ?>
+
+</div>
+<!--content end-->			
+		
 
 <?php get_footer(); ?>
-
-<?php } ?>
