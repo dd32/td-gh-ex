167cca5f816ff586946bb40774a2a7aa5bb422eb
Theme Directory <noreply@wordpress.org>
2014-02-21 16:51:40 +0000

    New version of Base WP - 1.03

--- functions.php
+++ functions.php
@@ -332,8 +332,8 @@ function BaseBreadcrumb() {
  */
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'base_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'base_wrapper_end', 10);
 
 function base_wrapper_start() {
   echo '<section id="main" class="twelve columns">';
