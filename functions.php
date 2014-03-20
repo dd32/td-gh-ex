d2026bb97bbbac0fcb77d2870259cdd97f809d80
Theme Directory <noreply@wordpress.org>
2014-03-20 12:52:36 +0000

    New version of Hemingway - 1.35

--- functions.php
+++ functions.php
@@ -339,4 +339,164 @@ function cd_meta_box_save( $post_id ) {
 
 }
 
+
+// Hemingway theme options
+
+class Hemingway_Customize {
+
+   public static function register ( $wp_customize ) {
+   
+      //1. Define a new section (if desired) to the Theme Customizer
+      $wp_customize->add_section( 'hemingway_options', 
+         array(
+            'title' => __( 'Hemingway Options', 'hemingway' ), //Visible title of section
+            'priority' => 35, //Determines what order this appears in
+            'capability' => 'edit_theme_options', //Capability needed to tweak
+            'description' => __('Allows you to customize some settings for Hemingway.', 'hemingway'), //Descriptive tooltip
+         ) 
+      );
+      
+      //2. Register new settings to the WP database...
+      $wp_customize->add_setting( 'accent_color', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+         array(
+            'default' => '#1abc9c', //Default setting/value to save
+            'type' => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
+            'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
+            'transport' => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
+         ) 
+      );
+                  
+      //3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
+      $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
+         $wp_customize, //Pass the $wp_customize object (required)
+         'hemingway_accent_color', //Set a unique ID for the control
+         array(
+            'label' => __( 'Accent Color', 'hemingway' ), //Admin-visible name of the control
+            'section' => 'colors', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+            'settings' => 'accent_color', //Which setting to load and manipulate (serialized is okay)
+            'priority' => 10, //Determines the order this control appears in for the specified section
+         ) 
+      ) );
+      
+      //4. We can also change built-in settings by modifying properties. For instance, let's make some stuff use live preview JS...
+      $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+      $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+      $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+      $wp_customize->get_setting( 'background_color' )->transport = 'postMessage';
+   }
+
+   public static function header_output() {
+      ?>
+      
+	      <!--Customizer CSS--> 
+	      
+	      <style type="text/css">
+	           <?php self::generate_css('.blog-title a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog-menu a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog-search #searchsubmit', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog-search #searchsubmit', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog-search #searchsubmit:hover', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog-search #searchsubmit:hover', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('.featured-media .sticky-post', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-title a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-meta a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.blog .format-quote blockquote cite a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content a.more-link:hover', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.content .button:hover', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content .searchform #searchsubmit', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content .searchform #searchsubmit', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content .searchform #searchsubmit:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.post-content .searchform #searchsubmit:hover', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-categories a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-categories a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-tags a:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.post-tags a:hover:after', 'border-right-color', 'accent_color'); ?>
+	           <?php self::generate_css('.post-nav a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.archive-nav a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.logged-in-as a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.logged-in-as a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.content #respond input[type="submit"]:hover', 'background-color', 'accent_color'); ?>
+	           <?php self::generate_css('.comment-meta-content cite a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.comment-meta-content p a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.comment-actions a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('#cancel-comment-reply-link', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('#cancel-comment-reply-link:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.comment-nav-below a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget-title a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget-title a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_text a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_text a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_rss a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_rss a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_archive a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_archive a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_meta a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_meta a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_recent_comments a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_recent_comments a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_pages a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_pages a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_recent_entries a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_recent_entries a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_categories a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_categories a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_search #searchsubmit', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_search #searchsubmit', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_search #searchsubmit:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.widget_search #searchsubmit:hover', 'border-color', 'accent_color'); ?>
+	           <?php self::generate_css('#wp-calendar a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('#wp-calendar a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('#wp-calendar tfoot a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.dribbble-shot:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.widgetmore a', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.widgetmore a:hover', 'color', 'accent_color'); ?>
+	           <?php self::generate_css('.flickr_badge_image a:hover img', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.sidebar .tagcloud a:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.footer .tagcloud a:hover', 'background', 'accent_color'); ?>
+	           <?php self::generate_css('.credits a:hover', 'color', 'accent_color'); ?>
+	      </style> 
+	      
+	      <!--/Customizer CSS-->
+	      
+      <?php
+   }
+   
+   public static function live_preview() {
+      wp_enqueue_script( 
+           'hemingway-themecustomizer', // Give the script a unique ID
+           get_template_directory_uri() . '/js/theme-customizer.js', // Define the path to the JS file
+           array(  'jquery', 'customize-preview' ), // Define dependencies
+           '', // Define a version (optional) 
+           true // Specify whether to put in footer (leave this true)
+      );
+   }
+
+   public static function generate_css( $selector, $style, $mod_name, $prefix='', $postfix='', $echo=true ) {
+      $return = '';
+      $mod = get_theme_mod($mod_name);
+      if ( ! empty( $mod ) ) {
+         $return = sprintf('%s { %s:%s; }',
+            $selector,
+            $style,
+            $prefix.$mod.$postfix
+         );
+         if ( $echo ) {
+            echo $return;
+         }
+      }
+      return $return;
+    }
+}
+
+// Setup the Theme Customizer settings and controls...
+add_action( 'customize_register' , array( 'Hemingway_Customize' , 'register' ) );
+
+// Output custom CSS to live site
+add_action( 'wp_head' , array( 'Hemingway_Customize' , 'header_output' ) );
+
+// Enqueue live preview javascript in Theme Customizer admin screen
+add_action( 'customize_preview_init' , array( 'Hemingway_Customize' , 'live_preview' ) );
+
 ?>
