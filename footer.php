9b0603b469c38964b1d01a90b4d8e5b0a18caa83
Theme Directory <noreply@wordpress.org>
2014-03-27 10:12:28 +0000

    New version of QuickChic - 1.4.2

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-<div id="footer"> 
-	<p>		
-&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?> 
-&nbsp;
-&bull;
-&nbsp;
-<a href="http://www.quickonlinethemes.com/wordpress/quickchic/" title="QuickChic Theme">QuickChic Theme</a> powered by <a href="http://wordpress.org">WordPress</a> 
-</p>
-</div>
-</div>
-		<?php wp_footer(); ?>
-</body>
+<div id="footer"> 
+	<p>		
+&copy; Copyright <?php echo date('Y'); ?> - <?php bloginfo('name'); ?> 
+&nbsp;
+&bull;
+&nbsp;
+<a href="http://www.quickonlinethemes.com/wordpress/quickchic/" rel="nofollow" title="QuickChic Theme">QuickChic Theme</a> powered by <a href="http://wordpress.org">WordPress</a> 
+</p>
+</div>
+</div>
+		<?php wp_footer(); ?>
+</body>
 </html>
