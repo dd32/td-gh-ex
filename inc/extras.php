7a4e0c0731d0f69e910c076abf2bb69da287e016
Theme Directory <noreply@wordpress.org>
2014-04-23 20:39:41 +0000

    New version of Unite - 1.4

--- inc/extras.php
+++ inc/extras.php
@@ -240,7 +240,7 @@ add_action( 'unite_footer', 'unite_footer_info', 30 );
 /**
  * function to show the footer info, copyright information
  */
-function unite_footer_info() {         
+function unite_footer_info() {
    $output = '<a href="http://colorlib.com/wp/unite" title="Unite Theme" target="_blank">Unite Theme</a> powered by <a href="http://wordpress.org" title="WordPress" target="_blank">WordPress</a>.';
    echo $output;
 }
@@ -268,7 +268,7 @@ if (!function_exists('get_unite_theme_options'))  {
         echo '.btn-primary:hover, .label-primary[href]:hover, .label-primary[href]:focus, #infinite-handle span:hover, .btn.btn-primary.read-more:hover, .btn-primary:hover, .btn-primary:focus, .btn-primary:active, .btn-primary.active, .site-main [class*="navigation"] a:hover, .more-link:hover, #image-navigation .nav-previous a:hover, #image-navigation .nav-next a:hover  { background-color: '.of_get_option('element_color_hover', '#000').'; border-color: '.of_get_option('element_color_hover', '#000').'; }';
       }
       if ( of_get_option('heading_color')) {
-        echo 'h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6, .entry-title {color: '.of_get_option('heading_color', '#000').';}';   
+        echo 'h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6, .entry-title {color: '.of_get_option('heading_color', '#000').';}';
       }
       if ( of_get_option('top_nav_bg_color')) {
         echo '.navbar.navbar-default {background-color: '.of_get_option('top_nav_bg_color', '#000').';}';
@@ -296,16 +296,16 @@ if (!function_exists('get_unite_theme_options'))  {
       }
       if ( of_get_option('social_hover_color')) {
         echo '.social-profile:hover {color: '.of_get_option('social_hover_color', '#000').';}';
-      }  
+      }
       $typography = of_get_option('main_body_typography');
       if ( $typography ) {
         echo '.entry-content {font-family: ' . $typography['face'] . '; font-size:' . $typography['size'] . '; font-weight: ' . $typography['style'] . '; color:'.$typography['color'] . ';}';
-      }   
+      }
       if ( of_get_option('custom_css')) {
         echo of_get_option( 'custom_css', 'no entry' );
-      }    
+      }
         echo '</style>';
-    }   
+    }
 }
 add_action('wp_head','get_unite_theme_options',10);
 
@@ -313,10 +313,10 @@ add_action('wp_head','get_unite_theme_options',10);
 add_action( 'optionsframework_after','unite_options_display_sidebar' );
 
 function unite_options_display_sidebar() { ?>
-  <!-- Twitter --> 
+  <!-- Twitter -->
   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-  <!-- Facebook --> 
+  <!-- Facebook -->
     <div id="fb-root"></div>
   <div id="fb-root"></div>
   <script>(function(d, s, id) {
@@ -417,3 +417,60 @@ function unite_register_required_plugins() {
   tgmpa( $plugins, $config );
 
 }
+
+/*
+ * Basic WooCommerce setup.
+ */
+
+add_action('woocommerce_before_main_content', 'unite_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'unite_wrapper_end', 10);
+
+function unite_wrapper_start() {
+  echo '<div id="primary" class="col-md-8">';
+}
+
+function unite_wrapper_end() {
+  echo '</div>';
+}
+
+/**
+ * Place a cart icon with number of items and total cost in the menu bar.
+ *
+ * https://gist.github.com/srikat/8264387#file-functions-php
+ */
+
+function unite_woocommerce_menucart($menu, $args) {
+
+  // Check if WooCommerce is active and add a new item to a menu assigned to Primary Navigation Menu location
+  if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) || 'primary' !== $args->theme_location )
+    return $menu;
+
+  ob_start();
+    global $woocommerce;
+    $viewing_cart = __('View your shopping cart', 'unite');
+    $start_shopping = __('Start shopping', 'unite');
+    $cart_url = $woocommerce->cart->get_cart_url();
+    $shop_page_url = get_permalink( woocommerce_get_page_id( 'shop' ) );
+    $cart_contents_count = $woocommerce->cart->cart_contents_count;
+    $cart_contents = sprintf(_n('%d item', '%d items', $cart_contents_count, 'unite'), $cart_contents_count);
+    $cart_total = $woocommerce->cart->get_cart_total();
+    // Uncomment the line below to hide nav menu cart item when there are no items in the cart
+    // if ( $cart_contents_count > 0 ) {
+      if ($cart_contents_count == 0) {
+        $menu_item = '</ul><ul class="nav navbar-nav navbar-right"><li><a class="woomenucart-menu-item" href="'. $shop_page_url .'" title="'. $start_shopping .'">';
+      } else {
+        $menu_item = '</ul><ul class="nav navbar-nav navbar-right"><li><a class="woomenucart-menu-item" href="'. $cart_url .'" title="'. $viewing_cart .'">';
+      }
+
+      $menu_item .= '<i class="fa fa-shopping-cart"></i> ';
+
+      $menu_item .= $cart_contents.' - '. $cart_total;
+      $menu_item .= '</a></li></ul>';
+    // Uncomment the line below to hide nav menu cart item when there are no items in the cart
+    // }
+    echo $menu_item;
+  $social = ob_get_clean();
+  return $menu . $social;
+
+}
+add_filter('wp_nav_menu_items','unite_woocommerce_menucart', 10, 2);
