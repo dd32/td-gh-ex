a7654be62692477c3a0624ad7e8312d9e6effaf1
Theme Directory <noreply@wordpress.org>
2014-06-01 22:11:04 +0000

    New theme: Solon - 1.0

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+//Menu dropdown animation
+jQuery(function($) {
+	$('.main-navigation .children').hide();
+	$('.main-navigation li').hover( 
+		function() {
+			$(this).children('.main-navigation .children').slideDown();
+		}, 
+		function() {
+			$(this).children('.main-navigation .children').hide();
+		}
+	);
+});
+
+// Nice scroll
+jQuery(function($) {
+	$("html").niceScroll({
+		cursorborder:"", 
+		cursorcolor:"#2A363B", 
+		boxzoom:true, 
+		cursoropacitymin: 0.5,
+		cursoropacitymax: 1,
+		cursorwidth: 10,
+		mousescrollstep: 50
+	});
+});
+
+//Fit Vids
+jQuery(function($) {
+  
+  $(document).ready(function(){
+    $("body").fitVids();
+  });
+  
+});
+
+//Comments clearfix
+jQuery(function($) {
+	$(".comment-body").addClass('clearfix');
+});
