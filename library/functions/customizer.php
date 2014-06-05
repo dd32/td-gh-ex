ee6e8a9c92fe9f1c65a2f6aaa4e10e16c9d51b4c
Theme Directory <noreply@wordpress.org>
2014-06-05 13:30:16 +0000

    New version of Travelify - 2.1

--- library/functions/customizer.php
+++ library/functions/customizer.php
@@ -205,7 +205,7 @@ function travelify_options_register_theme_customizer( $wp_customize ) {
             $wp_customize,
             'travelify_content_bg_color',
             array(
-                'label'      => __( 'Contnet Background Color', 'travelify' ),
+                'label'      => __( 'Content Background Color', 'travelify' ),
                 'section'    => 'travelify_element_options',
                 'settings'   => 'travelify_content_bg_color',
                 'priority' => 7
@@ -314,7 +314,7 @@ function travelify_options_register_theme_customizer( $wp_customize ) {
     $wp_customize->add_control(
         'travelify_footer_textbox',
         array(
-            'label' => 'Copyright text',
+            'label' => __('Copyright text','travelify'),
             'section' => 'travelify_footer_options',
             'type' => 'text',
         )
