cc63dbb33cf4eb3ee94c691f29dac0b725878ee5
Theme Directory <noreply@wordpress.org>
2014-07-24 17:12:19 +0000

    New version of Artikler - 1.3

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 <div class="clear"></div>
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-<p style="float:left;margin:10px 0 0 0;">Copyright &copy; 2014 <a href="<?php echo home_url(); ?>"><?php bloginfo('title'); ?></a>, All Rights Reserved</p>
-			<p style="float:right;">Theme designed by <a href="http://jathemes.com" target="_blank" title="JATheme"><img src="http://jathemes.com/wp-content/uploads/2014/07/icon.png" />JATheme</a></p>		</div><!-- .site-info -->
+<p style="float:left;">Copyright &copy; 2014 <a href="<?php echo home_url(); ?>"><?php bloginfo('title'); ?></a>, All Rights Reserved</p>
+			<p style="float:right;">Theme designed by <a href="http://jathemes.com" target="_blank" title="JATheme"><img src="http://jathemes.com/wp-content/uploads/2014/07/icon.png" />JATheme</a> and powered by <a href="http://wordpress.org/" target="_blank">Wordpress</a></p>		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
     </div><!--main-->
 <?php wp_footer(); ?>
