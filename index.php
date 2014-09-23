c1e732e15ad701d8138d54bfe200cc87a9cfd998
Theme Directory <noreply@wordpress.org>
2014-09-23 14:08:55 +0000

    New version of Badeyes - 1.1.4

--- index.php
+++ index.php
@@ -1,244 +1,118 @@
 <?php
 
-
-
 /**
-
-
 
  * The main template file
 
-
-
  *
-
-
 
  * This is the most generic template file in a WordPress theme and one
 
-
-
  * of the two required files for a theme (the other being style.css).
-
-
 
  * It is used to display a page when nothing more specific matches a query,
 
-
-
  * e.g., it puts together the home page when no home.php file exists.
 
-
-
  *
-
-
 
  * @link http://codex.wordpress.org/Template_Hierarchy
 
-
-
  *
-
-
 
  * @package WordPress
 
-
-
  * @subpackage Twenty_Fourteen
-
-
 
  * @since Twenty Fourteen 1.0
 
-
-
  */
-
 get_header(); ?>
-
-
 
 <div id="main-content" class="main-content">
 
-
-
 <?php
-
 if (is_home() && twentyfourteen_has_featured_posts() ) {
-
-
 
 		// Include the featured content template.
 
-
-
 get_template_part( 'featured-content' );
-
-
 
 	}
 
-
-
 ?>
 
-
-
 <div id="primary" class="content-area">
-
-<?php
-
-global $badeyes_options;
-
-
+		<div id="content" class="site-content" role="main">
+<?php global $badeyes_options;
 
 					$badeyes_settings = get_option( 'badeyes_options', $badeyes_options );
 
-
-
 				?>
-
-
 
 <?php if( $badeyes_settings['intro_text'] != '' ) : ?>
 
-
-
 <?php 
-
 echo $badeyes_settings['intro_text']; 
-
 ?>
 
-
-
 <?php endif; ?>
-
-		<div id="content" class="site-content" role="main">
-
-<?php
-
-global $badeyes_options;
-
-
+<?php global $badeyes_options;
 
 					$badeyes_settings = get_option( 'badeyes_options', $badeyes_options );
-
 				?>
-
 <?php if( $badeyes_settings['blog_title'] != '' ) : ?>
-
 <?php 
-
 echo "<h1>";
-
 echo $badeyes_settings['blog_title']; 
-
 echo "</h1>";
-
 ?>
-
 <?php endif; ?>
-
-
-
-
-
 <?php
 
-$week = date('W');
+			if ( have_posts() ) :
 
-$year = date('Y');
+				// Start the Loop.
 
-$args=array(
+				while ( have_posts() ) : the_post();
+		/*
 
-  'w'=> $week,
+					 * Include the post format-specific template for the content. If you want to
 
-  'year'=> $year,
+					 * use this in a child theme, then include a file called called content-___.php
 
-  'post_type' => 'post',
+					 * (where ___ is the post format) and that will be used instead.
 
-  'post_status' => 'publish',
+					 */
 
-  'posts_per_page' => -1,
+					get_template_part( 'content', get_post_format() );
 
-  'ignore_sticky_posts'=> 1
+			endwhile;
 
-);
+//Previous/next post navigation.
 
-$my_query = null;
+				twentyfourteen_paging_nav();
+else :
 
-$my_query = new WP_Query($args);
+				// If no content, include the "No posts found" template.
 
-if( $my_query->have_posts() ) {
+				get_template_part( 'content', 'none' );
+endif;
 
-while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		?>
 
-<h2><?php the_title(); ?></h2>
-
-
-
-	<?php the_content ("" . the_title('', '- Full Article', false)); ?>
-
-
-
-<?php endwhile; 
-
-
-
-} else { ?> <h2> Oops ... No Content</h2> <?php } 
-
-
-
-wp_reset_query(); // Restore global post data stomped by the_post(). 
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		</div><!-- #content -->
-
-
-
-	</div><!-- #primary -->
-
-
+</div><!-- #content -->
+</div><!-- #primary -->
 
 	<?php get_sidebar( 'content' ); ?>
-
-
 
 </div><!-- #main-content -->
 
 
 
-
-
-
-
 <?php
-
-
 
 get_sidebar();
 
-
-
 get_footer();
-
-
 
