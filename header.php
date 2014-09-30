73e2c216594d83c082ddb0c008ffb4c7bdda2624
Theme Directory <noreply@wordpress.org>
2014-09-30 07:08:36 +0000

    New version of Quill - 1.01

--- header.php
+++ header.php
@@ -14,6 +14,21 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php if ( get_theme_mod('site_favicon') ) : ?>
+	<link rel="shortcut icon" href="<?php echo esc_url(get_theme_mod('site_favicon')); ?>" />
+<?php endif; ?>
+<?php if ( get_theme_mod('apple_touch_144') ) : ?>
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url(get_theme_mod('apple_touch_144')); ?>" />
+<?php endif; ?>
+<?php if ( get_theme_mod('apple_touch_114') ) : ?>
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url(get_theme_mod('apple_touch_114')); ?>" />
+<?php endif; ?>
+<?php if ( get_theme_mod('apple_touch_72') ) : ?>
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url(get_theme_mod('apple_touch_72')); ?>" />
+<?php endif; ?>
+<?php if ( get_theme_mod('apple_touch_57') ) : ?>
+	<link rel="apple-touch-icon" href="<?php echo esc_url(get_theme_mod('apple_touch_57')); ?>" />
+<?php endif; ?>
 
 <?php wp_head(); ?>
 </head>
@@ -44,8 +59,12 @@
 		</nav><!-- #site-navigation -->
 
 		<div class="site-branding">
-			<h1 class="site-title wow fadeInRight"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description wow fadeInLeft"><?php bloginfo( 'description' ); ?></h2>
+			<?php if ( get_theme_mod('site_logo') ) : ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('name'); ?>"><img class="logo-image" src="<?php echo esc_url(get_theme_mod('site_logo')); ?>" alt="<?php bloginfo('name'); ?>" /></a>
+			<?php else : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php endif; ?>
 		</div>
 	</header><!-- #masthead -->
 
