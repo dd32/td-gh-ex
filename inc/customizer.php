3cac43ef10010a5050d1b72d01dbcae54d917232
Theme Directory <noreply@wordpress.org>
2014-10-10 14:19:09 +0000

    New version of Bharat - 1.2

--- inc/customizer.php
+++ inc/customizer.php
@@ -37,7 +37,7 @@ add_action( 'customize_register', 'bharat_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function bharat_customize_preview_js() {
-	wp_enqueue_script( 'bharat_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'bharat_customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'bharat_customize_preview_js' );
 
