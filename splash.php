bbcda4cb537cf3899322e3e9ba46c59174ea0c05
Theme Directory <noreply@wordpress.org>
2014-10-30 18:16:24 +0000

    New version of Badeyes - 1.2.1

---
+++ splash.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: 2 column 
+ * @package WordPress
+ * @subpackage badeyes
+ * @since badeyes 1.0
+ */
+
+get_header(masthead);
+
+<div id="content" class="contents_full" role="main">
+
+<?php get_template_part( 'featured-content' ); ?>
+
+</div>
+
+<?php get_footer(splash); ?>
+
