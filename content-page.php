75b0c60a058236e690596941ae81841100641313
Theme Directory <noreply@wordpress.org>
2014-11-03 09:12:15 +0000

    New version of Artikler - 1.3.4.2

--- content-page.php
+++ content-page.php
@@ -11,7 +11,7 @@
 
 		<div class="entry-content">
 			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'jatheme' ), 'after' => '</div>' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'artikler' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
 		<footer class="entry-meta">
 			<?php edit_post_link( __( 'Edit', 'artikler' ), '<span class="edit-link">', '</span>' ); ?>
