ff69d4105c028c55b0232580afbc7cd321de7d6c
Theme Directory <noreply@wordpress.org>
2014-11-05 13:22:35 +0000

    New version of Albar - 1.5.6

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Albar
  */
 
-define( 'KAIRA_THEME_VERSION' , '1.5.5' );
+define( 'KAIRA_THEME_VERSION' , '1.5.6' );
 
 if ( file_exists( get_stylesheet_directory() . '/settings/class.kaira-theme-settings.php' ) ) {
     require_once( get_stylesheet_directory() . '/settings/class.kaira-theme-settings.php' );
