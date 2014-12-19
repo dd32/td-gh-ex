16f7fa078e9df4d930cc6fc32b767362c1deb9b7
Theme Directory <noreply@wordpress.org>
2014-12-19 15:49:21 +0000

    New version of DarkElements - 2.1

--- functions.php
+++ functions.php
@@ -19,6 +19,9 @@
 		array( 'primary' => __( 'Primary Navigation', 'darkelements' ), 
 	 	) ); 
 
+	// Add document title
+		add_theme_support( 'title-tag' );
+
 	// Add editor styles
 		add_editor_style( array( 'custom-editor-style.css' ) );
 
@@ -74,18 +77,6 @@
 		echo '<![endif]-->'. "\n"; 
 		}
 	add_action( 'wp_head', 'darkelements_html5' ); 
-
-
-// Add blogname to wp_title
-	function darkelements_wp_title( $title ) { 
-		global $page, $paged; 
-		if ( is_feed() ) 
-		return $title; 
-	
-		$filtered_title = $title . get_bloginfo( 'name' ); 
-			return $filtered_title; 
-	}
-	add_filter( 'wp_title', 'darkelements_wp_title' ); 
 
 
 // Enqueues scripts and styles for front-end
