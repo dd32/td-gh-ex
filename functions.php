9ffea0a47fbe895e2b5826e139315051925821e7
Theme Directory <noreply@wordpress.org>
2014-12-19 15:50:08 +0000

    New version of MultiColors - 2.1

--- functions.php
+++ functions.php
@@ -19,6 +19,9 @@
 		array( 'primary' => __( 'Primary Navigation', 'multicolors' ), 
 	 	) ); 
 
+	// Add document title
+		add_theme_support( 'title-tag' );
+
 	// Add editor styles
 		add_editor_style( array( 'custom-editor-style.css' ) );
 
@@ -72,18 +75,6 @@
 		echo '<![endif]-->'. "\n"; 
 		} 
 	add_action( 'wp_head', 'multicolors_html5' ); 
-
-
-// Add blogname to wp_title
-	function multicolors_wp_title( $title ) { 
-		global $page, $paged; 
-		if ( is_feed() ) 
-		return $title; 
-	
-		$filtered_title = $title . get_bloginfo( 'name' ); 
-			return $filtered_title; 
-	}
-	add_filter( 'wp_title', 'multicolors_wp_title' ); 
 
 
 // Enqueues scripts and styles for front-end
