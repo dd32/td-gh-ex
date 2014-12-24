1823508a71c8db180172c09ec558c08eebd76e2f
Theme Directory <noreply@wordpress.org>
2014-12-24 16:04:17 +0000

    New version of PrivateBusiness - 3.0

--- js/nav-secondary.js
+++ js/nav-secondary.js
@@ -1,4 +1,5 @@
 /*
+ * MOBILE NAVIGATION WITHOUT SUBPAGE LISTING
  * Source: http://maxfoundry.com/blog/responsive-mobile-navigation-in-wordpress/
  * Copyright author: John Hartley
  * Author URI: http://maxfoundry.com/blog/author/johnbhartley 
@@ -6,33 +7,34 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-jQuery(document).ready(function() {
-            
-            // build <select> dropdown
-            jQuery("<select />").appendTo("div.nav-head-secondary");
+jQuery(document).ready(function() { 
+	// build <select> dropdown
+	jQuery("<select />").appendTo("div.nav-head-secondary");
 
-            //Create deafult option "Go to..."
-            jQuery("<option />", {
-                "selected": "selected",
-                "value": "",
-                "text": "Menu Top..."  
-            }).appendTo(".nav-head-secondary select");
+	// create deafult option "Menu..."
+	jQuery("<option />", {
+		"selected": "selected",
+		"value": "",
+		"text": "Menu Top..."  
+	}).appendTo(".nav-head-secondary select");
 
-            //Populate
-            jQuery(".nav-head-secondary ul li a").each(function() {
-                var el = jQuery(this);
-                    if(el.parents('.nav-head-secondary ul ul').length) {
-                        // if there are ul in li
-                    } else { // if no ul in li
-                        jQuery('<option />', {
-                        'value': el.attr('href'),
-                        'text': el.text()
-                    }).appendTo('.nav-head-secondary select');
-                }
-            });
+	// populate
+	jQuery(".nav-head-secondary ul li a").each(function() {
+		var el = jQuery(this);
 
-            //make links work 
-            jQuery(".nav-head-secondary select").change(function() {
-                window.location = jQuery(this).find("option:selected").val();
-            });
-    });
+		if(el.parents('.nav-head-secondary ul ul').length) {
+			// if there are ul in li
+		} else { 
+			// if no ul in li
+			jQuery('<option />', {
+				'value': el.attr('href'),
+				'text': el.text()
+			}).appendTo('.nav-head-secondary select');
+		}
+	});
+
+	// make links work 
+	jQuery(".nav-head-secondary select").change(function() { 
+		window.location = jQuery(this).find("option:selected").val();
+	});
+});
