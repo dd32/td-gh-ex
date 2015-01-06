892485dc732cc3701c8f1e31305e293714cfcafb
Theme Directory <noreply@wordpress.org>
2015-01-06 08:04:55 +0000

    New version of Aileron - 1.0.1

--- inc/extras.php
+++ inc/extras.php
@@ -162,37 +162,52 @@ function aileron_site_info() {
 }
 add_action( 'aileron_footer', 'aileron_site_info' );
 
-/**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function aileron_wp_title( $title, $sep ) {
-	global $page, $paged;
+if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
+	/**
+	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
+	 *
+	 * @param string $title Default title text for current view.
+	 * @param string $sep Optional separator.
+	 * @return string The filtered title.
+	 */
+	function aileron_wp_title( $title, $sep ) {
+		if ( is_feed() ) {
+			return $title;
+		}
 
-	if ( is_feed() ) {
+		global $page, $paged;
+
+		// Add the blog name
+		$title .= get_bloginfo( 'name', 'display' );
+
+		// Add the blog description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) ) {
+			$title .= " $sep $site_description";
+		}
+
+		// Add a page number if necessary:
+		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+			$title .= " $sep " . sprintf( __( 'Page %s', 'aileron' ), max( $paged, $page ) );
+		}
+
 		return $title;
 	}
+	add_filter( 'wp_title', 'aileron_wp_title', 10, 2 );
 
-	// Add the blog name
-	$title .= get_bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title .= " $sep $site_description";
+	/**
+	 * Title shim for sites older than WordPress 4.1.
+	 *
+	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
+	 * @todo Remove this function when WordPress 4.3 is released.
+	 */
+	function aileron_render_title() {
+		?>
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php
 	}
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 ) {
-		$title .= " $sep " . sprintf( __( 'Page %s', 'aileron' ), max( $paged, $page ) );
-	}
-
-	return $title;
-}
-add_filter( 'wp_title', 'aileron_wp_title', 10, 2 );
+	add_action( 'wp_head', 'aileron_render_title' );
+endif;
 
 /**
  * Sets the authordata global when viewing an author archive.
--- readme.txt
+++ readme.txt
@@ -1,6 +1,29 @@
-= AILERON =
+=== Aileron ===
+Contributors: ThemeCot.com
+Tags: black, blue, white, light, two-columns, right-sidebar, responsive-layout, custom-background, custom-colors, custom-header, custom-menu, editor-style, featured-images, flexible-header, post-formats, theme-options, microformats, sticky-post, threaded-comments, translation-ready
+Requires at least: 3.9
+Tested up to: 4.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-* by ThemeCot, http://themecot.com/
+== Description ==
+Aileron is a magazine and blog WordPress theme with a clean design and modern look. It is well suited for magazine, news, editorial, blog, photography or review site.
+
+* Responsive Layout
+* Custom Colors
+* Custom Header
+* Post Formats
+* The GPL v2.0 or later license. :) Use it to make something cool.
+
+== Installation ==
+
+1. In your admin panel, go to Appearance -> Themes and click the Add New button.
+2. Click Upload and Choose File, then select the theme's ZIP file. Click Install Now.
+3. Click Activate to use your new theme right away.
+
+== How can I setup Aileron WordPress theme? ==
+
+http://themecot.com/aileron-theme-documentation/
 
 == License ==
 
@@ -10,7 +33,3 @@ Please see the file headers for more information.
 Fonts are licensed - http://fontawesome.io/license (Font: SIL OFL 1.1, CSS: MIT License)
 
 aileron/fonts
-
-== Tested At ==
-
-WordPress 4.0.1
