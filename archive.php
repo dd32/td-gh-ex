ebdc682e87bcadf30d1a722af2148e0ff16f5218
Theme Directory <noreply@wordpress.org>
2015-01-21 19:11:50 +0000

    New version of MW Small - 1.0.5

--- archive.php
+++ archive.php
@@ -25,10 +25,10 @@ get_header(); ?>
 							printf( __( 'Day: %s', 'mwsmall' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'mwsmall' ), '<span>' . get_the_date( _e( 'F Y', 'monthly archives date format', 'mwsmall' ) ) . '</span>' );
+							printf( __( 'Month: %s', 'mwsmall' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'mwsmall' ) ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'mwsmall' ), '<span>' . get_the_date( _e( 'Y', 'yearly archives date format', 'mwsmall' ) ) . '</span>' );
+							printf( __( 'Year: %s', 'mwsmall' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'mwsmall' ) ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
 							_e( 'Galleries', 'mwsmall');
