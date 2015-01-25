7308daf8260514a60cee7abd46981f6b4bda1bad
Theme Directory <noreply@wordpress.org>
2015-01-25 19:48:35 +0000

    New version of Swell Lite - 1.0.3

--- loop-blog.php
+++ loop-blog.php
@@ -8,7 +8,7 @@
 			<div class="feature-vid radius-top"><?php echo get_post_meta($post->ID, 'featurevid', true); ?></div>
 		<?php } else { ?>
 			<?php if ( has_post_thumbnail() ) { ?>
-				<a class="feature-img radius-top" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'swelllite' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php the_post_thumbnail( 'swell-featured-large' ); ?></a>
+				<a class="feature-img radius-top" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'swelltheme' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php the_post_thumbnail( 'swell-featured-large' ); ?></a>
 			<?php } ?>
 		<?php } ?>
 		
@@ -17,8 +17,8 @@
 		
 			<?php if (get_theme_mod('display_date_blog') == '1') { ?>
 			<div class="post-date">
-				<p><i class="fa fa-comment"></i> <a href="<?php the_permalink(); ?>#comments"><?php comments_number(__("Leave a Comment", 'swelllite'), __("1 Comment", 'swelllite'), '% Comments'); ?></a></p>
-				<p><i class="fa fa-clock-o"></i> <?php _e("Posted on", 'swelllite'); ?> <?php the_time(__("F j, Y", 'swelllite')); ?></p>
+				<p><i class="fa fa-comment"></i> <a href="<?php the_permalink(); ?>#comments"><?php comments_number(__("Leave a Comment", 'swelltheme'), __("1 Comment", 'swelltheme'), '% Comments'); ?></a></p>
+				<p><i class="fa fa-clock-o"></i> <?php _e("Posted on", 'swelltheme'); ?> <?php the_time(__("F j, Y", 'swelltheme')); ?></p>
 			</div>
 			<?php } ?>
 		
@@ -26,13 +26,13 @@
 			
 			<?php if (get_theme_mod('display_author_blog') == '1') { ?>
 				<div class="post-author">
-					<p><?php _e("by", 'swelllite'); ?> <?php esc_url ( the_author_posts_link() ); ?></p>
+					<p><?php _e("by", 'swelltheme'); ?> <?php esc_url ( the_author_posts_link() ); ?></p>
 				</div>
 			<?php } ?>
 			
 			<span class="divider-small"></span>
 		
-			<?php the_content(__("Read More", 'swelllite')); ?>
+			<?php the_content(__("Read More", 'swelltheme')); ?>
 			
 		<!-- END .article -->
 		</div>
@@ -42,10 +42,10 @@
 
 <?php endwhile; ?>
 
-	<?php if ( $wp_query->max_num_pages > 1 ) { ?>
+	<?php if ( $wp_query->max_num_pages > 1 && ! Jetpack::is_module_active( 'infinite-scroll' ) ) { ?>
 		<!-- BEGIN .pagination -->
 		<div class="pagination">
-			<?php echo swelllite_get_pagination_links(); ?>
+			<?php echo swell_get_pagination_links(); ?>
 		<!-- END .pagination -->
 		</div>
 	<?php } ?>
@@ -53,8 +53,8 @@
 <?php else : ?>
 
 	<div class="error-404">
-		<h1 class="headline"><?php _e("No Posts Found", 'swelllite'); ?></h1>
-		<p><?php _e("We're sorry, but no posts have been found. Create a post to be added to this section, and configure your theme options.", 'swelllite'); ?></p>
+		<h1 class="headline"><?php _e("No Posts Found", 'swelltheme'); ?></h1>
+		<p><?php _e("We're sorry, but no posts have been found. Create a post to be added to this section, and configure your theme options.", 'swelltheme'); ?></p>
 	</div>
 
 <?php endif; ?>
