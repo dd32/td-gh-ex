ffe016ab53db83a9fe8d3af484304991cd402659
Theme Directory <noreply@wordpress.org>
2015-02-10 18:55:08 +0000

    New version of fCorpo - 1.0.1

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 					<div id="copyright">
 						<p>
 						 <?php fkidd_show_copyright_text(); ?> <a href="<?php echo esc_url( 'http://tishonator.com/product/fcorpo' ); ?>" title="<?php esc_attr_e( 'fcorpo Theme', 'fkidd' ); ?>">
-							<?php _e('fCorpo Theme', 'fkidd'); ?></a> <?php esc_attr_e( 'powered by', 'fkidd' ); ?> <a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>" title="<?php esc_attr_e( 'WordPress', 'fkidd' ); ?>">
+							<?php _e('fCorpo Theme', 'fcorpo'); ?></a> <?php esc_attr_e( 'powered by', 'fkidd' ); ?> <a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>" title="<?php esc_attr_e( 'WordPress', 'fkidd' ); ?>">
 							<?php _e('WordPress', 'fkidd'); ?></a>
 						</p>
 					</div>
