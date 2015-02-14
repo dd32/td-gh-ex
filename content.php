d5ebec33e1b41c81407b13c282c621df55eb8d90
Theme Directory <noreply@wordpress.org>
2015-02-14 17:16:56 +0000

    New theme: bhost - 1.0.0

---
+++ content.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package bhost
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" class="single-post">
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php bhost_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bhost' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'bhost' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php bhost_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package bhost
+ */
+?>
+
+		</div><!-- #content -->
+	</div><!--.row-->
+
+	<div class="row">
+		<footer id="footer" class="site-footer twelve columns" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bhost' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bhost' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'bhost' ), 'bhost', '<a href="http://masum.esy.es/" rel="designer">Masum Billah</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #footer -->
+	</div>
+	</div><!-- #page -->
+</div><!--.end container-->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
