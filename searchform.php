5684c5c828b660f28bcae053ce4815fff3cc7391
Theme Directory <noreply@wordpress.org>
2015-02-14 10:22:10 +0000

    New version of Aaron - 1.1

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>/" role="search">
 	<label>
 		<span class='screen-reader-text'><?php _e( 'Search', 'aaron' ); ?></span>
-		<input type="text" name="s" placeholder="<?php _e( 'Search...', 'aaron' ); ?>" />
+		<input type="search" name="s" />
 	</label> 
-	<input type="submit" name="submit" value="<?php _e( 'Search', 'aaron' ); ?>" class="search-submit" />
+	<input type="submit" name="submit" value="<?php esc_attr_e( 'Search', 'aaron' ); ?>" class="search-submit" />
 </form>
