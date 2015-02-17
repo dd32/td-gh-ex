cbc3f83eaa64933e67f7dec7404f6f0fbe4c4de0
Theme Directory <noreply@wordpress.org>
2015-02-17 06:48:15 +0000

    New version of Gridalicious - 0.5

--- sidebar.php
+++ sidebar.php
@@ -65,20 +65,38 @@ do_action( 'gridalicious_before_secondary' );?>
 		if ( is_active_sidebar( 'primary-sidebar' ) ) {
         	dynamic_sidebar( 'primary-sidebar' ); 
    		}	
-		else { ?>
-			<section id="widget-default-text" class="widget widget_text">	
+		else { 
+			//Helper Text
+			if ( current_user_can( 'edit_theme_options' ) ) { ?>
+				<section id="widget-default-text" class="widget widget_text">	
+					<div class="widget-wrap">
+	                	<h4 class="widget-title"><?php _e( 'Primary Sidebar Widget Area', 'gridalicious' ); ?></h4>
+	           		
+	           			<div class="textwidget">
+	                   		<p><?php _e( 'This is the Primary Sidebar Widget Area if you are using a two column site layout option.', 'gridalicious' ); ?></p>
+	                   		<p><?php printf( __( 'By default it will load Search and Archives widgets as shown below. You can add widget to this area by visiting your <a href="%s">Widgets Panel</a> which will replace default widgets.', 'gridalicious' ), admin_url( 'widgets.php' ) ); ?></p>
+	                 	</div>
+	           		</div><!-- .widget-wrap -->
+	       		</section><!-- #widget-default-text -->
+			<?php
+			} ?>
+			<section class="widget widget_search" id="default-search">
 				<div class="widget-wrap">
-                	<h4 class="widget-title"><?php _e( 'Primary Sidebar Widget Area', 'gridalicious' ); ?></h4>
-           		
-           			<div class="textwidget">
-                   		<p><?php _e( 'This is the Primary Sidebar Widget Area if you are using a two column site layout option.', 'gridalicious' ); ?></p>
-                   		<p><?php printf( __( 'You can add content to this area by visiting your <a href="%s">Widgets Panel</a> and adding new widgets to this area.', 'gridalicious' ), admin_url( 'widgets.php' ) ); ?></p>
-                 	</div>
-           		</div><!-- .widget-wrap -->
-       		</section><!-- #widget-default-text -->
-		<?php
-	echo '</aside><!-- .sidebar sidebar-primary widget-area -->';
-	}
+					<?php get_search_form(); ?>
+				</div><!-- .widget-wrap -->
+			</section><!-- #default-search -->
+			<section class="widget widget_archive" id="default-archives">
+				<div class="widget-wrap">
+					<h4 class="widget-title"><?php _e( 'Archives', 'gridalicious' ); ?></h4>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</div><!-- .widget-wrap -->
+			</section><!-- #default-archives -->
+			<?php 
+		}
+		echo '</aside><!-- .sidebar sidebar-primary widget-area -->';
+
 
 	/** 
 	 * gridalicious_after_primary_sidebar hook
