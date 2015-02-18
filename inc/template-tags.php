4d549619b3080b830bb9315142ba3ad03fb66149
Theme Directory <noreply@wordpress.org>
2015-02-18 13:17:38 +0000

    New version of Hiero - 1.6

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -167,9 +167,6 @@ if ( ! function_exists( 'athemes_posted_on' ) ) :
  */
 function athemes_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) )
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
