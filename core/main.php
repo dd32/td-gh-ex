6f3f3fbaf40e6da06d0a94139428de0f388c9693
Theme Directory <noreply@wordpress.org>
2015-03-05 15:45:41 +0000

    New version of Alhena Lite - 1.0.9

--- core/main.php
+++ core/main.php
@@ -41,31 +41,24 @@ function alhenalite_add_menuclass( $ulclass ) {
 add_filter( 'wp_page_menu', 'alhenalite_add_menuclass' );
 
 /*-----------------------------------------------------------------------------------*/
-/* Tag Title */
+/* TAG TITLE */
 /*-----------------------------------------------------------------------------------*/  
- 
-function alhenalite_title( $title, $sep ) {
-	global $paged, $page;
 
-	if ( is_feed() )
-		return $title;
+if ( ! function_exists( '_wp_render_title_tag' ) ) {
 
-	// Add the site name.
-	$title .= get_bloginfo( 'name' );
+	function alhenalite_title( $title, $sep ) {
+		
+?>
 
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title = "$title $sep $site_description";
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentytwelve' ), max( $paged, $page ) );
+<?php
 
-	return $title;
+	}
+
+	add_filter( 'wp_head', 'alhenalite_title', 10, 2 );
+
 }
-
-add_filter( 'wp_title', 'alhenalite_title', 10, 2 );
 
 /*-----------------------------------------------------------------------------------*/
 /* Admin menu */
@@ -305,7 +298,8 @@ function alhenalite_setup() {
 	add_theme_support( 'post-formats', array( 'aside','gallery','quote','video','audio','link' ) );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
-	
+
+	add_theme_support( 'title-tag' );
 	
 	add_theme_support( 'custom-background', array(
 		'default-color' => 'f3f3f3',
