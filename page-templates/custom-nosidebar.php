e0decd4ebf88163d1d954610c16114556eda5277
Theme Directory <noreply@wordpress.org>
2015-03-05 22:43:39 +0000

    New version of Azul Silver - 0.1.0

--- page-templates/custom-nosidebar.php
+++ page-templates/custom-nosidebar.php
@@ -1,15 +1,17 @@
 <?php
 /*
- *  Template Name: Custom Sidebar
+ *  Template Name: No Sidebar
  */
 ?>
 <?php get_header(); ?>
     <?php if (have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
-            <?php get_template_part('content', 'page') ?>
+            <?php get_template_part('content', 'full-width'); ?>
     <?php endwhile; ?>
+		<div class = "post-navigation">
+			<?php azulsilver_paging_navigation(); ?>
+		</div>
     <?php else : ?>
             <?php get_template_part('content', 'none'); ?>
     <?php endif; ?>
-<?php get_sidebar('custom'); ?>
 <?php get_footer(); ?>
