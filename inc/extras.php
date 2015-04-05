d1faa8e93f672539890c8050701a97d135498e74
Theme Directory <noreply@wordpress.org>
2015-04-05 18:45:35 +0000

    New version of Accesspress Lite - 2.43

--- inc/extras.php
+++ inc/extras.php
@@ -596,3 +596,5 @@ add_action( 'wp_enqueue_scripts', 'accesspresslite_scripts' );
 	}
 
 	add_action('accesspresslite_call_to_action','accesspresslite_call_to_action_cb', 10);
+
+	add_filter('widget_text', 'do_shortcode');
