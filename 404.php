84a4a422ef67d249b2460d24f9dfd90d9c18cb25
Theme Directory <noreply@wordpress.org>
2015-06-19 11:35:52 +0000

    New version of MultiColors - 2.5

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@
 <div id="main-content">
 <div id="content">
 	<h4 class="page-title"><?php _e( 'Nothing Found', 'multicolors' ); ?></h4>
-		<p><?php _e('Sorry, no posts matched your criteria.', 'multicolors'); ?></p>
+	<p><?php _e('Sorry, no posts matched your criteria.', 'multicolors'); ?></p>
 	<?php get_search_form(); ?>
 </div>
 <?php get_sidebar(); ?>
--- single.php
+++ single.php
@@ -9,17 +9,16 @@
 <div id="main-content">
 <div id="content">
 
-	<?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h4 class="post-title"><?php the_title(); ?></h4>
 
 			<h5 class="postmetadata">
-			<?php printf( __( 'Posted on %s', 'multicolors' ), '<a href="' . esc_url( get_permalink() ) . '">' . esc_html( get_the_date() ). '</a>' ); ?> | 
-			<?php printf( __( 'By %s', 'multicolors' ), sprintf( '<a href="%1$s">%2$s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ), esc_html( get_the_author() ) ) ); ?>
-			<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : echo '|'; ?>
-			<?php comments_popup_link( __( 'Leave a response', 'multicolors' ), __( '1 response', 'multicolors' ), __( '% responses', 'multicolors' ) ); ?><?php endif; ?>
+				<?php printf( __( 'Posted on %s', 'multicolors' ), '<a href="' . esc_url( get_permalink() ) . '">' . esc_html( get_the_date() ). '</a>' ); ?> | 
+				<?php printf( __( 'By %s', 'multicolors' ), sprintf( '<a href="%1$s">%2$s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ), esc_html( get_the_author() ) ) ); ?>
+				<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : echo '|'; ?>
+				<?php comments_popup_link( __( 'Leave a response', 'multicolors' ), __( '1 response', 'multicolors' ), __( '% responses', 'multicolors' ) ); ?><?php endif; ?>
 			</h5>
 	
 			<?php the_content(); ?>
@@ -27,15 +26,14 @@
 			<div class="pagelink"><?php wp_link_pages(); ?></div>
 
 			<h5 class="postmetadata">
-			<?php printf( __( 'Posted in %s', 'multicolors' ), get_the_category_list( __( ', ', 'multicolors' ) ) ); ?>
-			<?php if(has_tag() ) : echo ' | '; ?><?php printf(__( 'Tags: %s', 'multicolors' ), get_the_tag_list('', __( ', ', 'multicolors' ) ) ); ?> <?php endif; ?>
+				<?php printf( __( 'Posted in %s', 'multicolors' ), get_the_category_list( __( ', ', 'multicolors' ) ) ); ?>
+				<?php if(has_tag() ) : echo ' | '; ?><?php printf(__( 'Tags: %s', 'multicolors' ), get_the_tag_list('', __( ', ', 'multicolors' ) ) ); ?> <?php endif; ?>
 			</h5>
 		</div>
 
 		<?php comments_template(); ?>
 
 	<?php endwhile; ?>
-	<?php endif; ?>
 
 	<?php edit_post_link( __( 'Edit', 'multicolors' ), '<div class="edit-link">', '</div>' ) ?>
 
