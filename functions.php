a13354516f2898e9fc0a73f8b39cf3fff94ce79d
Theme Directory <noreply@wordpress.org>
2015-09-11 05:22:04 +0000

    New version of GeneratePress - 1.3.13

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package GeneratePress
  */
 	
-define( 'GENERATE_VERSION', '1.3.12');
+define( 'GENERATE_VERSION', '1.3.13');
 define( 'GENERATE_URI', get_template_directory_uri() );
 define( 'GENERATE_DIR', get_template_directory() );
 
