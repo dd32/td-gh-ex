da6d5a1c9132f62d732f47ac210f2430e4b4d50b
Theme Directory <noreply@wordpress.org>
2015-12-25 13:22:18 +0000

    New version of Shipyard - 3.4

--- index.php
+++ index.php
@@ -9,33 +9,34 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-		<div class="sticky">
-			<h5><?php _e( 'Featured post', 'shipyard' ); ?></h5>
-		</div>
-	<?php endif; ?>
+		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+			<div class="sticky">
+				<h5><?php _e( 'Featured post', 'shipyard' ); ?></h5>
+			</div>
+		<?php endif; ?>
 
 		<h4 class="post-title">
 			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permalink to %s', 'shipyard'), the_title_attribute('echo=0')); ?>"> <?php the_title(); ?></a> 
 		</h4>
 
 		<div class="postmetadata">
-			<?php printf( __( 'Posted on %s', 'shipyard' ), '<a href="' . esc_url( get_permalink() ) . '">' . esc_html( get_the_date() ). '</a>' ); ?> | 
+			<?php printf( __( 'Posted on %s', 'shipyard' ), '<a href="'. esc_url( get_permalink() ) .'">' . esc_html( get_the_date() ). '</a>' ); ?> <?php echo '|'; ?> 
 			<?php printf( __( 'By %s', 'shipyard' ), sprintf( '<a href="%1$s">%2$s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ), esc_html( get_the_author() ) ) ); ?>
-			<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : echo '|'; ?>
-			<?php comments_popup_link( __( 'Leave a response', 'shipyard' ), __( '1 response', 'shipyard' ), __( '% responses', 'shipyard' ) ); ?><?php endif; ?>
+			<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
+				<?php echo '|'; ?> <?php comments_popup_link( __( 'Leave a response', 'shipyard' ), __( '1 response', 'shipyard' ), __( '% responses', 'shipyard' ) ); ?>
+			<?php endif; ?>
 		</div>
 
-	<?php if ( has_post_thumbnail() ) { 
-		the_post_thumbnail(); 
-	} ?>
+		<?php if ( has_post_thumbnail() ) { 
+			the_post_thumbnail(); 
+		} ?>
 
-	<?php the_excerpt(); ?>
+		<?php the_excerpt(); ?>
 
-	<div class="more">
-		<a class="readmore" href="<?php the_permalink() ?>" rel="bookmark"><?php _e( 'Read More &raquo;', 'shipyard' ); ?></a>
-	</div>
-
+		<div class="more">
+			<a class="readmore" href="<?php the_permalink() ?>" rel="bookmark"><?php _e( 'Read More &raquo;', 'shipyard' ); ?></a>
+		</div>
+	
 	<?php endwhile; ?>
 
 	<div class="post-nav">
