115f364dea50ec66c36d71268efb0693f8a900c6
Theme Directory <noreply@wordpress.org>
2016-01-07 14:15:54 +0000

    New version of MyKnowledgeBase - 2.8

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@
 		load_theme_textdomain('myknowledgebase', get_template_directory() . '/languages');  
 
 	// Register Menu
-		register_nav_menus( 
-		array( 'primary' => __( 'Primary Navigation', 'myknowledgebase' ), 
+		register_nav_menus( array( 
+			'primary' => __( 'Primary Navigation', 'myknowledgebase' ), 
 	 	) ); 
 
 	// Add document title
@@ -27,22 +27,22 @@
 
 	// Custom header	
 		$args = array(		
-		'width' => 600,
-		'height' => 400,
-		'default-image' => get_template_directory_uri() . '/images/boats.jpg',
-		'header-text' => false,
-		'uploads' => true,
+			'width' => 600,
+			'height' => 400,
+			'default-image' => get_template_directory_uri() . '/images/boats.jpg',
+			'header-text' => false,
+			'uploads' => true,
 		);	
 		add_theme_support( 'custom-header', $args );
 
 	// Default header
-	register_default_headers( array(
-		'boats' => array(
-			'url'           => get_template_directory_uri() . '/images/boats.jpg',
-			'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
-			'description'   => __( 'Default header', 'myknowledgebase' )
-		)
-	) );
+		register_default_headers( array(
+			'boats' => array(
+				'url'           => get_template_directory_uri() . '/images/boats.jpg',
+				'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
+				'description'   => __( 'Default header', 'myknowledgebase' )
+			)
+		) );
 
 	// Post thumbnails
 		add_theme_support( 'post-thumbnails' ); 
@@ -57,10 +57,11 @@
 		add_theme_support( 'automatic-feed-links' );
 
 	// Switches default core markup for search form, comment form, and comments to output valid HTML5
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'caption' ) );
 
 	// Background color
-		$args = array( 'default-color' => 'ffffff', 
+		$args = array( 
+			'default-color' => 'ffffff', 
 		); 
 		add_theme_support( 'custom-background', $args ); 
 
@@ -73,7 +74,7 @@
 		echo '<!--[if lt IE 9]>'. "\n"; 
 		echo '<script src="' . esc_url( get_template_directory_uri() . '/js/ie.js' ) . '"></script>'. "\n"; 
 		echo '<![endif]-->'. "\n"; 
-		}
+	}
 	add_action( 'wp_head', 'myknowledgebase_html5' ); 
 
 
@@ -172,11 +173,12 @@
 
 // Custom excerpt lenght (default length is 55 words)
 	function myknowledgebase_excerpt_length( $length ) { 
-		return 75; } 
+		return 75; 
+	} 
 	add_filter( 'excerpt_length', 'myknowledgebase_excerpt_length', 999 ); 
 
 
-// Theme Customizer
+// Theme Customizer (logo and posts per page)
 	function myknowledgebase_theme_customizer( $wp_customize ) { 
 		$wp_customize->add_section( 'myknowledgebase_logo_section' , array( 
 			'title' => __( 'Logo', 'myknowledgebase' ), 
