b21ad68e9bdd9c9e9e4bc3bd2f56fd71ae759ef1
Theme Directory <noreply@wordpress.org>
2016-01-07 14:16:48 +0000

    New version of Shipyard - 3.6

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@
 		load_theme_textdomain('shipyard', get_template_directory() . '/languages');  
 
 	// Register Menu
-		register_nav_menus( 
-		array( 'primary' => __( 'Primary Navigation', 'shipyard' ), 
+		register_nav_menus( array( 
+			'primary' => __( 'Primary Navigation', 'shipyard' ), 
 	 	) ); 
 
 	// Add document title
@@ -27,22 +27,22 @@
 
 	// Custom header	
 		$args = array(		
-		'width' => 1160,
-		'height' => 350,
-		'default-image' => get_template_directory_uri() . '/images/boats.jpg',
-		'header-text' => false,
-		'uploads' => true,
+			'width' => 1160,
+			'height' => 350,
+			'default-image' => get_template_directory_uri() . '/images/boats.jpg',
+			'header-text' => false,
+			'uploads' => true,
 		);	
 		add_theme_support( 'custom-header', $args );
 
 	// Default header
-	register_default_headers( array(
-		'boats' => array(
-			'url'           => get_template_directory_uri() . '/images/boats.jpg',
-			'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
-			'description'   => __( 'Default header', 'shipyard' )
-		)
-	) );
+		register_default_headers( array(
+			'boats' => array(
+				'url'           => get_template_directory_uri() . '/images/boats.jpg',
+				'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
+				'description'   => __( 'Default header', 'shipyard' )
+			)
+		) );
 
 	// Post thumbnails
 		add_theme_support( 'post-thumbnails' ); 
@@ -58,10 +58,11 @@
 
 
 	// Switches default core markup for search form, comment form, and comments to output valid HTML5
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'caption' ) );
 
 	// Background color
-		$args = array( 'default-color' => 'f8f8f8', 
+		$args = array( 
+			'default-color' => 'f8f8f8', 
 		); 
 		add_theme_support( 'custom-background', $args ); 
 
@@ -74,7 +75,7 @@
 		echo '<!--[if lt IE 9]>'. "\n"; 
 		echo '<script src="' . esc_url( get_template_directory_uri() . '/js/ie.js' ) . '"></script>'. "\n"; 
 		echo '<![endif]-->'. "\n"; 
-		} 
+	} 
 	add_action( 'wp_head', 'shipyard_html5' ); 
 
 
@@ -153,11 +154,12 @@
 
 // Custom excerpt lenght (default length is 55 words)
 	function shipyard_excerpt_length( $length ) { 
-		return 55; } 
+		return 55; 
+	} 
 	add_filter( 'excerpt_length', 'shipyard_excerpt_length', 999 ); 
 
 
-// Theme Customizer (option to add logo)
+// Theme Customizer (logo)
 	function shipyard_theme_customizer( $wp_customize ) { 
 		$wp_customize->add_section( 'shipyard_logo_section' , array( 
 			'title' => __( 'Logo', 'shipyard' ), 
