0516af8631f7f37ace3bf330328ebf225dfb4276
Theme Directory <noreply@wordpress.org>
2016-02-25 13:54:24 +0000

    New version of _tk - 1.1.10

---
+++ page-both-sidebar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Both Sidebar
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package 99fy
+ */
+
+get_header();
+?>
+	<div class="page-wrapper clear">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<?php get_sidebar('left'); ?>
+				</div>
+				<div class="col-md-6">
+					<?php
+						while ( have_posts() ) : the_post();
+							the_content();
+						endwhile; // End of the loop.
+					?>
+				</div>
+				<div class="col-md-3">
+					<?php get_sidebar('right'); ?>
+				</div>
+			</div>
+		</div><!-- #primary -->
+	</div><!-- #primary -->
+<?php
+get_footer();
---
+++ woocommerce/single-product/related.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Related Products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( $related_products ) : ?>
+
+	<section class="related products related-product-slider">
+
+		<div class="section-title text-center mb-65">
+            <h3><?php esc_html_e( 'Related Products', '99fy' ); ?></h3>
+            <p>There are many variations of pas of Lorem Ipsum.</p>
+        </div>
+
+		<div class="row owl-carousel">
+
+			<?php foreach ( $related_products as $related_product ) : ?>
+
+				<?php
+				 	$post_object = get_post( $related_product->get_id() );
+
+					setup_postdata( $GLOBALS['post'] =& $post_object );
+
+					wc_get_template_part( 'content', 'product' ); ?>
+
+			<?php endforeach; ?>
+
+		</div>
+
+	</section>
+
+<?php endif;
+
+wp_reset_postdata();
