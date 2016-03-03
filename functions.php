dce36448fec1e732c3c4085238463137903745dc
Theme Directory <noreply@wordpress.org>
2016-03-03 08:31:19 +0000

    New version of Catch Box - 4.3.6

--- functions.php
+++ functions.php
@@ -892,7 +892,7 @@ function catchbox_scripts_method() {
 	/**
 	 * Loads up Responsive Menu
 	 */
-	wp_enqueue_script('catchbox-sidr', get_template_directory_uri() . '/js/jquery.sidr.min.js', array( 'jquery' ), '2.1.0', false );
+	wp_enqueue_script( 'catchbox-sidr', get_template_directory_uri() . '/js/jquery.sidr.min.js', array( 'jquery' ), '2.1.1.1', false );
 
 	//Register JQuery circle all and JQuery set up as dependent on Jquery-cycle
 	wp_register_script( 'jquery-cycle', get_template_directory_uri() . '/js/jquery.cycle.all.min.js', array( 'jquery' ), '2.9999.5', true );
