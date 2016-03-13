144894227c8bf3761b85fab51179f7f55a083c33
Theme Directory <noreply@wordpress.org>
2016-03-13 22:47:53 +0000

    New version of BlueGray - 4.0

--- functions.php
+++ functions.php
@@ -80,13 +80,19 @@
 
 // Enqueues scripts and styles for front-end
 	function bluegray_scripts() {
-			wp_enqueue_style( 'bluegray-style', get_stylesheet_uri() );
-			wp_enqueue_script( 'bluegray-nav', get_template_directory_uri() . '/js/nav.js', array( 'jquery' ) );
-			wp_enqueue_style( 'bluegray-googlefonts', '//fonts.googleapis.com/css?family=Open+Sans' ); 
+		wp_enqueue_style( 'bluegray-style', get_stylesheet_uri() );
+		wp_enqueue_script( 'bluegray-nav', get_template_directory_uri() . '/js/nav.js', array( 'jquery' ) );
+		wp_enqueue_style( 'bluegray-googlefonts', '//fonts.googleapis.com/css?family=Open+Sans' ); 
 
-			if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-				wp_enqueue_script( 'comment-reply' );
-			}
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+		// mobile nav args
+		$bluegray_mobile_nav_args = array(
+			'navText' => __( 'Menu', 'bluegray' )
+		);
+		// localize script with data for mobile nav
+		wp_localize_script( 'bluegray-nav', 'objectL10n', $bluegray_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'bluegray_scripts' );
 
