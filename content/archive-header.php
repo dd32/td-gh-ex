45a010757ebad0c01a2a0cb4fd949fa5ce537c23
Theme Directory <noreply@wordpress.org>
2016-04-07 15:07:17 +0000

    New version of Tracks - 1.49

--- content/archive-header.php
+++ content/archive-header.php
@@ -1,10 +1,15 @@
 <?php
+
+if ( is_home() ) {
+	echo '<h1 class="screen-reader-text">' . get_bloginfo("name") . ' ' . __('Posts', 'tracks') . '</h1>';
+}
+
 if ( ! is_archive() ) {
 	return;
 }
 ?>
 
 <div class='archive-header'>
-	<h2><?php the_archive_title(); ?></h2>
+	<h1><?php the_archive_title(); ?></h1>
 	<?php the_archive_description(); ?>
 </div>
