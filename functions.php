32dfe9e9441a7cc047d0a7a578f5288930345e18
Theme Directory <noreply@wordpress.org>
2016-04-20 21:35:52 +0000

    New version of PrivateBusiness - 4.6

--- functions.php
+++ functions.php
@@ -24,17 +24,17 @@
 		add_theme_support( 'title-tag' );
 
 	// Add editor styles
-		add_editor_style( array( 'custom-editor-style.css' ) );
+		add_editor_style( 'custom-editor-style.css' );
 
 	// Custom header	
-		$args = array(		
+		$header_args = array(		
 			'width' => 1200,
 			'height' => 350,
 			'default-image' => get_template_directory_uri() . '/images/boats.jpg',
 			'header-text' => false,
 			'uploads' => true,
 		);	
-		add_theme_support( 'custom-header', $args );
+		add_theme_support( 'custom-header', $header_args );
 
 	// Default header
 		register_default_headers( array(
@@ -61,10 +61,10 @@
 		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'caption' ) );
 
 	// Background color
-		$args = array( 
+		$background_args = array( 
 			'default-color' => 'eeeeee', 
 		); 
-		add_theme_support( 'custom-background', $args ); 
+		add_theme_support( 'custom-background', $background_args ); 
 
 	}
 	add_action( 'after_setup_theme', 'privatebusiness_setup' ); 
