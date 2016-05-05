01854689c9a1c1df53860f1b17174b1db9d70ca5
Theme Directory <noreply@wordpress.org>
2016-05-05 21:02:50 +0000

    New version of BlueGray - 4.2

--- archive.php
+++ archive.php
@@ -6,30 +6,15 @@
 
 <?php get_header(); ?>
 <div id="content">
-
 	<?php if ( have_posts() ) : ?>
+
 		<?php
 			the_archive_title( '<h1 class="page-title">', '</h1>' );
 			the_archive_description( '<div class="archive-description">', '</div>' );
 		?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-			<h2 class="post-title">
-				<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permalink to %s', 'bluegray'), the_title_attribute('echo=0')); ?>"> <?php the_title(); ?></a> 
-			</h2>
-
-			<?php get_template_part( 'postmeta' ); ?>
-
-			<?php if ( has_post_thumbnail() ) { 
-				the_post_thumbnail(); 
-			} ?>
-
-			<?php the_excerpt(); ?>
-
-			<div class="more">
-				<a class="readmore" href="<?php the_permalink() ?>" rel="bookmark"><?php _e( 'Read More &raquo;', 'bluegray' ); ?></a>
-			</div>
-
+			<?php get_template_part( 'content-list' ); ?>
 		<?php endwhile; ?>
 
 		<div class="post-nav">
@@ -37,11 +22,11 @@
 			<?php previous_posts_link(); ?>
 		</div>
 
-		<?php else: ?>
-			<h1 class="page-title"><?php _e( 'Nothing Found', 'bluegray' ); ?></h1>
-			<p><?php _e('Sorry, no posts matched your criteria.', 'bluegray'); ?></p>
-	<?php endif; ?>
+	<?php else: ?>
+		<h1 class="page-title"><?php _e( 'Nothing Found', 'bluegray' ); ?></h1>
+		<p><?php _e('Sorry, no posts matched your criteria.', 'bluegray'); ?></p>
 
+	<?php endif; ?>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -5,66 +5,67 @@
 
 // Sets up theme defaults and registers various WordPress features that BlueGray supports
 	function bluegray_setup() { 
+		// Set max content width for img, video, and more
+			global $content_width; 
+			if ( ! isset( $content_width ) )
+			$content_width = 760;
 
-	// Set max content width for img, video, and more
-		global $content_width; 
-		if ( ! isset( $content_width ) )
-		$content_width = 760;
+		// Make theme available for translation
+			load_theme_textdomain('bluegray', get_template_directory() . '/languages');  
 
-	// Make theme available for translation
-		load_theme_textdomain('bluegray', get_template_directory() . '/languages');  
+		// Register Menu
+			register_nav_menus( array( 
+				'primary' => __( 'Primary Navigation', 'bluegray' ), 
+		 	) ); 
 
-	// Register Menu
-		register_nav_menus( array( 
-			'primary' => __( 'Primary Navigation', 'bluegray' ), 
-	 	) ); 
+		// Add document title
+			add_theme_support( 'title-tag' );
 
-	// Add document title
-		add_theme_support( 'title-tag' );
+		// Add editor styles
+			add_editor_style( 'custom-editor-style.css' );
 
-	// Add editor styles
-		add_editor_style( 'custom-editor-style.css' );
+		// Custom header	
+			$header_args = array(		
+				'width' => 1700,
+				'height' => 400,
+				'default-image' => get_template_directory_uri() . '/images/boats.jpg',
+				'header-text' => false,
+				'uploads' => true,
+			);	
+			add_theme_support( 'custom-header', $header_args );
 
-	// Custom header	
-		$header_args = array(		
-			'width' => 1700,
-			'height' => 400,
-			'default-image' => get_template_directory_uri() . '/images/boats.jpg',
-			'header-text' => false,
-			'uploads' => true,
-		);	
-		add_theme_support( 'custom-header', $header_args );
+		// Default header
+			register_default_headers( array(
+				'boats' => array(
+					'url' => get_template_directory_uri() . '/images/boats.jpg',
+					'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
+					'description' => __( 'Default header', 'bluegray' )
+				)
+			) );
 
-	// Default header
-		register_default_headers( array(
-			'boats' => array(
-				'url' => get_template_directory_uri() . '/images/boats.jpg',
-				'thumbnail_url' => get_template_directory_uri() . '/images/boats.jpg',
-				'description' => __( 'Default header', 'bluegray' )
-			)
-		) );
+		// Post thumbnails
+			add_theme_support( 'post-thumbnails' ); 
 
-	// Post thumbnails
-		add_theme_support( 'post-thumbnails' ); 
+		// Resize mode thumbnails
+			set_post_thumbnail_size( 350, 350 ); 
 
-	// Resize mode thumbnails
-		set_post_thumbnail_size( 350, 350 ); 
+		// Resize single page thumbnail
+			add_image_size( 'single', 350, 350 ); 
 
-	// Resize single page thumbnail
-		add_image_size( 'single', 350, 350 ); 
+		// This feature adds RSS feed links to html head 
+			add_theme_support( 'automatic-feed-links' );
 
-	// This feature adds RSS feed links to html head 
-		add_theme_support( 'automatic-feed-links' );
+		// Switch default core markup for search form, comment form, comments and caption to output valid html5
+			add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'caption' ) );
 
-	// Switch default core markup for search form, comment form, comments and caption to output valid html5
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'caption' ) );
+		// Background color
+			$background_args = array( 
+				'default-color' => 'eeeeee', 
+			); 
+			add_theme_support( 'custom-background', $background_args ); 
 
-	// Background color
-		$background_args = array( 
-			'default-color' => 'eeeeee', 
-		); 
-		add_theme_support( 'custom-background', $background_args ); 
-
+		// Post formats
+			add_theme_support( 'post-formats', array( 'aside', 'status', 'image', 'video', 'gallery', 'audio' ) );
 	}
 	add_action( 'after_setup_theme', 'bluegray_setup' ); 
 
@@ -87,6 +88,7 @@
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
+
 		// mobile nav args
 		$bluegray_mobile_nav_args = array(
 			'navText' => __( 'Menu', 'bluegray' )
@@ -99,76 +101,75 @@
 
 // Sidebars
 	function bluegray_widgets_init() {
-	register_sidebar( array(
-		'name' => __( 'Primary Sidebar', 'bluegray' ),
-		'id' => 'primary',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Primary Sidebar', 'bluegray' ),
+			'id' => 'primary',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Homepage Right', 'bluegray' ),
-		'id' => 'homepage-right',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Homepage Right', 'bluegray' ),
+			'id' => 'homepage-right',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Homepage Middle', 'bluegray' ),
-		'id' => 'homepage-middle',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Homepage Middle', 'bluegray' ),
+			'id' => 'homepage-middle',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Homepage Left', 'bluegray' ),
-		'id' => 'homepage-left',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Homepage Left', 'bluegray' ),
+			'id' => 'homepage-left',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Footer Right', 'bluegray' ),
-		'id' => 'footer-right',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Footer Right', 'bluegray' ),
+			'id' => 'footer-right',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Footer Middle', 'bluegray' ),
-		'id' => 'footer-middle',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
+		register_sidebar( array(
+			'name' => __( 'Footer Middle', 'bluegray' ),
+			'id' => 'footer-middle',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 
-	register_sidebar( array(
-		'name' => __( 'Footer Left', 'bluegray' ),
-		'id' => 'footer-left',
-		'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widgettitle">',
-		'after_title' => '</h3>',
-	) );
-
+		register_sidebar( array(
+			'name' => __( 'Footer Left', 'bluegray' ),
+			'id' => 'footer-left',
+			'description' => __( 'You can add one or multiple widgets here.', 'bluegray' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>',
+		) );
 	}
 	add_action( 'widgets_init', 'bluegray_widgets_init' );
 
