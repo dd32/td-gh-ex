b193b060136f8172cd00c1e174ad7631a5751a23
Theme Directory <noreply@wordpress.org>
2016-05-19 18:49:23 +0000

    New version of Bazaar Lite - 1.1.5

--- core/admin/customize/general.php
+++ core/admin/customize/general.php
@@ -4,16 +4,19 @@ if (!function_exists('bazaarlite_admin_init')) {
 
 	function bazaarlite_admin_init() {
 		
-		global $wp_version;
-
-		$file_dir = get_template_directory_uri()."/core/admin/assets/";
-	
-		wp_enqueue_style ( 'bazaarlite_style', $file_dir.'css/theme.css' ); 
-		wp_enqueue_script( 'bazaarlite_script', $file_dir.'js/theme.js',array('jquery'),'',TRUE ); 
+		global $wp_version, $pagenow;
 		
-		wp_enqueue_script( "jquery-ui-core");
-		wp_enqueue_script( "jquery-ui-tabs");
-	
+		if ( $pagenow == "post-new.php" || $pagenow == "post.php" ) :
+
+			$file_dir = get_template_directory_uri()."/core/admin/assets/";
+		
+			wp_enqueue_style ( 'bazaarlite_style', $file_dir.'css/theme.css' ); 
+			wp_enqueue_script( 'bazaarlite_script', $file_dir.'js/theme.js',array('jquery'),'',TRUE ); 
+			
+			wp_enqueue_script( "jquery-ui-core");
+			wp_enqueue_script( "jquery-ui-tabs");
+		
+		endif;
 	
 	}
 	
