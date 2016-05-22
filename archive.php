1eaa86b5f546d1cae06229a8b4ea08b9b0f211c8
Theme Directory <noreply@wordpress.org>
2016-05-22 20:54:20 +0000

    New version of LeftSide - 3.7

--- archive.php
+++ archive.php
@@ -23,8 +23,7 @@
 		</div>
 
 	<?php else: ?>
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'leftside' ); ?></h1>
-		<p><?php _e('Sorry, no posts matched your criteria.', 'leftside'); ?></p>
+		<?php get_template_part( 'content-none' ); ?>
 
 	<?php endif; ?>
 </div>
--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
 <?php get_header(); ?>
 <div id="content">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	
-		<?php get_template_part( 'content-list' ); ?>
+	<?php if ( have_posts() ) : ?>
 
-	<?php endwhile; ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content-list' ); ?>
+		<?php endwhile; ?>
 
 		<div class="post-nav">
 			<?php next_posts_link(); ?>
@@ -18,8 +18,7 @@
 		</div>
 
 	<?php else: ?>
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'leftside' ); ?></h1>
-		<p><?php _e('Sorry, no posts matched your criteria.', 'leftside'); ?></p>
+		<?php get_template_part( 'content-none' ); ?>
 
 	<?php endif; ?>
 </div>
