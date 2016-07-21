b6bc68a56df971af357a67412b8022a28d80a0e0
Theme Directory <noreply@wordpress.org>
2016-07-21 13:44:03 +0000

    New version of Beetle - 1.0.9

--- inc/theme-info.php
+++ inc/theme-info.php
@@ -34,36 +34,35 @@ function beetle_theme_info_page() {
 	// Get theme details.
 	$theme = wp_get_theme();
 	?>
-			
+
 	<div class="wrap theme-info-wrap">
 
 		<h1><?php printf( esc_html__( 'Welcome to %1$s %2$s', 'beetle' ), $theme->get( 'Name' ), $theme->get( 'Version' ) ); ?></h1>
 
 		<div class="theme-description"><?php echo $theme->get( 'Description' ); ?></div>
-		
-		<hr>        
+
+		<hr>
 		<div class="important-links clearfix">
 			<p><strong><?php esc_html_e( 'Theme Links', 'beetle' ); ?>:</strong>
 				<a href="<?php echo esc_url( __( 'https://themezee.com/themes/beetle/', 'beetle' ) . '?utm_source=theme-info&utm_medium=textlink&utm_campaign=beetle&utm_content=theme-page' ); ?>" target="_blank"><?php esc_html_e( 'Theme Page', 'beetle' ); ?></a>
-				<a href="<?php echo get_template_directory_uri(); ?>/changelog.txt" target="_blank"><?php esc_html_e( 'Changelog', 'beetle' ); ?></a>
 				<a href="<?php echo esc_url( 'http://preview.themezee.com/beetle/?utm_source=theme-info&utm_medium=textlink&utm_campaign=beetle&utm_content=demo' ); ?>" target="_blank"><?php esc_html_e( 'Theme Demo', 'beetle' ); ?></a>
 				<a href="<?php echo esc_url( __( 'https://themezee.com/docs/beetle-documentation/', 'beetle' ) . '?utm_source=theme-info&utm_medium=textlink&utm_campaign=beetle&utm_content=documentation' ); ?>" target="_blank"><?php esc_html_e( 'Theme Documentation', 'beetle' ); ?></a>
 				<a href="<?php echo esc_url( 'http://wordpress.org/support/view/theme-reviews/beetle?filter=5' ); ?>" target="_blank"><?php esc_html_e( 'Rate this theme', 'beetle' ); ?></a>
 			</p>
 		</div>
 		<hr>
-				
+
 		<div id="getting-started">
 
 			<h3><?php printf( esc_html__( 'Getting Started with %s', 'beetle' ), $theme->get( 'Name' ) ); ?></h3>
-			
+
 			<div class="columns-wrapper clearfix">
 
 				<div class="column column-half clearfix">
-				
+
 					<div class="section">
 						<h4><?php esc_html_e( 'Theme Documentation', 'beetle' ); ?></h4>
-						
+
 						<p class="about">
 							<?php esc_html_e( 'You need help to setup and configure this theme? We got you covered with an extensive theme documentation on our website.', 'beetle' ); ?>
 						</p>
@@ -73,10 +72,10 @@ function beetle_theme_info_page() {
 							</a>
 						</p>
 					</div>
-					
+
 					<div class="section">
 						<h4><?php esc_html_e( 'Theme Options', 'beetle' ); ?></h4>
-						
+
 						<p class="about">
 							<?php printf( esc_html__( '%s makes use of the Customizer for all theme settings. Click on "Customize Theme" to open the Customizer now.', 'beetle' ), $theme->get( 'Name' ) ); ?>
 						</p>
@@ -86,30 +85,30 @@ function beetle_theme_info_page() {
 					</div>
 
 				</div>
-				
+
 				<div class="column column-half clearfix">
-					
+
 					<img src="<?php echo get_template_directory_uri(); ?>/screenshot.jpg" />
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
-		
+
 		<hr>
-		
+
 		<div id="more-features">
 
 			<h3><?php esc_html_e( 'Get more features', 'beetle' ); ?></h3>
-			
+
 			<div class="columns-wrapper clearfix">
 
 				<div class="column column-half clearfix">
-				
+
 					<div class="section">
 						<h4><?php esc_html_e( 'Pro Version', 'beetle' ); ?></h4>
-						
+
 						<p class="about">
 							<?php printf( esc_html__( 'Purchase the Pro Version of %s to get additional features and advanced customization options.', 'beetle' ), 'Beetle' ); ?>
 						</p>
@@ -121,12 +120,12 @@ function beetle_theme_info_page() {
 					</div>
 
 				</div>
-				
+
 				<div class="column column-half clearfix">
-					
+
 					<div class="section">
 						<h4><?php esc_html_e( 'ThemeZee Plugins', 'beetle' ); ?></h4>
-						
+
 						<p class="about">
 							<?php esc_html_e( 'Extend the functionality of your WordPress website with our customized plugins.', 'beetle' ); ?>
 						</p>
@@ -139,25 +138,25 @@ function beetle_theme_info_page() {
 							</a>
 						</p>
 					</div>
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
-		
+
 		<hr>
-		
+
 		<div id="theme-author">
-			
+
 			<p><?php printf( esc_html__( '%1$s is proudly brought to you by %2$s. If you like this theme, %3$s :)', 'beetle' ),
 				$theme->get( 'Name' ),
 				'<a target="_blank" href="' . __( 'https://themezee.com/', 'beetle' ) . '?utm_source=theme-info&utm_medium=footer&utm_campaign=beetle" title="ThemeZee">ThemeZee</a>',
 			'<a target="_blank" href="http://wordpress.org/support/view/theme-reviews/beetle?filter=5" title="Beetle Review">' . esc_html__( 'rate it', 'beetle' ) . '</a>'); ?>
 			</p>
-		
+
 		</div>
-	
+
 	</div>
 
 	<?php
