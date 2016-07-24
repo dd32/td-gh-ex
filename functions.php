9864d0df952b380163a43c52406745c3d48a20da
Theme Directory <noreply@wordpress.org>
2016-07-24 16:59:05 +0000

    New version of Bhost - 1.3.2

--- functions.php
+++ functions.php
@@ -81,8 +81,8 @@ function bhost_setup() {
 endif; // bhost_setup
 add_action( 'after_setup_theme', 'bhost_setup' );
 
-define('BHOST_PRO_THEME_URL','//devitems.com/item/bhost-pro-responsive-wordpress-theme/','bhost');
-define('BHOST_LIVE_DEMO','//getmasum.net/bhost/','bhost');
+define('BHOST_PRO_THEME_URL','//codegrape.com/item/bhost-pro-responsive-wordpress-theme/8758','bhost');
+define('BHOST_LIVE_DEMO','//getmasum.net/preview/bhost/','bhost');
 
 //default menu
 function bhost_default_menu(){
