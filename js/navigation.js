9b00a1b6e67bd5f544c3f032649d405691ecc1b2
Theme Directory <noreply@wordpress.org>
2016-09-02 11:00:32 +0000

    New version of Esteem - 1.3.2

--- js/navigation.js
+++ js/navigation.js
@@ -7,12 +7,14 @@
 	var container, button, menu;
 
 	container = document.getElementById( 'site-navigation' );
-	if ( ! container )
+	if ( ! container ) {
 		return;
+	}
 
 	button = container.getElementsByTagName( 'h3' )[0];
-	if ( 'undefined' === typeof button )
+	if ( 'undefined' === typeof button ) {
 		return;
+	}
 
 	menu = container.getElementsByTagName( 'ul' )[0];
 
@@ -22,13 +24,23 @@
 		return;
 	}
 
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += 'nav-menu';
+	}
 
 	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'main-small-navigation' ) )
+		if ( -1 !== container.className.indexOf( 'main-small-navigation' ) ) {
 			container.className = container.className.replace( 'main-small-navigation', 'main-navigation' );
-		else
+		} else {
 			container.className = container.className.replace( 'main-navigation', 'main-small-navigation' );
+		}
 	};
+
+	    jQuery('#site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="icon-caret-down"></i> </span>');
+    jQuery('#site-navigation .sub-toggle').click(function() {
+        jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
+        jQuery(this).children('.icon-caret-right').first().toggleClass('icon-caret-down');
+        jQuery(this).toggleClass('active');
+    });
 } )();
+
