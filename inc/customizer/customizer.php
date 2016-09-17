63f8b68438dacbe1a4a4d2f0dae68f8daef531bb
Theme Directory <noreply@wordpress.org>
2016-09-17 09:31:18 +0000

    New version of Anderson Lite - 1.4.1

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -26,17 +26,17 @@ function anderson_customize_register_options( $wp_customize ) {
 		'capability'     => 'edit_theme_options',
 		'theme_supports' => '',
 		'title'          => esc_html__( 'Theme Options', 'anderson-lite' ),
-		'description'    => '',
+		'description'    => anderson_customize_theme_links(),
 	) );
-	
+
 	// Add postMessage support for site title and description.
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	
+
 	// Change default background section
 	$wp_customize->get_control( 'background_color'  )->section   = 'background_image';
 	$wp_customize->get_section( 'background_image'  )->title     = esc_html__( 'Background', 'anderson-lite' );
-	
+
 	// Add Display Site Title Setting
 	$wp_customize->add_setting( 'anderson_theme_options[site_title]', array(
         'default'           => true,
@@ -53,7 +53,7 @@ function anderson_customize_register_options( $wp_customize ) {
 		'priority' => 10
 		)
 	);
-	
+
 	// Add Header Tagline option
 	$wp_customize->add_setting( 'anderson_theme_options[header_tagline]', array(
         'default'           => false,
@@ -70,7 +70,7 @@ function anderson_customize_register_options( $wp_customize ) {
 		'priority' => 11
 		)
 	);
-	
+
 	// Add Header Image Link
 	$wp_customize->add_setting( 'anderson_theme_options[custom_header_link]', array(
         'default'           => '',
@@ -87,7 +87,7 @@ function anderson_customize_register_options( $wp_customize ) {
 		'priority' => 10
 		)
 	);
-	
+
 	// Add Custom Header Hide Checkbox
 	$wp_customize->add_setting( 'anderson_theme_options[custom_header_hide]', array(
         'default'           => false,
@@ -111,29 +111,8 @@ function anderson_customize_register_options( $wp_customize ) {
 add_action( 'customize_preview_init', 'anderson_customize_preview_js' );
 
 function anderson_customize_preview_js() {
-	
+
 	wp_enqueue_script( 'anderson-lite-customizer-preview', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151202', true );
-
-}
-
-// Embed JS file for Customizer Controls
-add_action( 'customize_controls_enqueue_scripts', 'anderson_customize_controls_js' );
-
-function anderson_customize_controls_js() {
-	
-	wp_enqueue_script( 'anderson-lite-customizer-controls', get_template_directory_uri() . '/js/customizer-controls.js', array(), '20151202', true );
-	
-	// Localize the script
-	wp_localize_script( 'anderson-lite-customizer-controls', 'anderson_theme_links', array(
-		'title'	=> esc_html__( 'Theme Links', 'anderson-lite' ),
-		'themeURL'	=> esc_url( __( 'https://themezee.com/themes/anderson/', 'anderson-lite' ) . '?utm_source=customizer&utm_medium=textlink&utm_campaign=anderson&utm_content=theme-page' ),
-		'themeLabel'	=> esc_html__( 'Theme Page', 'anderson-lite' ),
-		'docuURL'	=> esc_url( __( 'https://themezee.com/docs/anderson-documentation/', 'anderson-lite' ) . '?utm_source=customizer&utm_medium=textlink&utm_campaign=anderson&utm_content=documentation' ),
-		'docuLabel'	=>  esc_html__( 'Theme Documentation', 'anderson-lite' ),
-		'rateURL'	=> esc_url( 'http://wordpress.org/support/view/theme-reviews/anderson-lite?filter=5' ),
-		'rateLabel'	=> esc_html__( 'Rate this theme', 'anderson-lite' ),
-		)
-	);
 
 }
 
@@ -142,7 +121,52 @@ function anderson_customize_controls_js() {
 add_action( 'customize_controls_print_styles', 'anderson_customize_preview_css' );
 
 function anderson_customize_preview_css() {
-	
-	wp_enqueue_style( 'anderson-lite-customizer-css', get_template_directory_uri() . '/css/customizer.css', array(), '20151202' );
 
+	wp_enqueue_style( 'anderson-lite-customizer-css', get_template_directory_uri() . '/css/customizer.css', array(), '20160915' );
+
+}
+
+/**
+ * Returns Theme Links
+ */
+function anderson_customize_theme_links() {
+
+	ob_start();
+	?>
+
+		<div class="theme-links">
+
+			<span class="customize-control-title"><?php esc_html_e( 'Theme Links', 'anderson-lite' ); ?></span>
+
+			<p>
+				<a href="<?php echo esc_url( __( 'https://themezee.com/themes/anderson/', 'anderson-lite' ) ); ?>?utm_source=customizer&utm_medium=textlink&utm_campaign=anderson&utm_content=theme-page" target="_blank">
+					<?php esc_html_e( 'Theme Page', 'anderson-lite' ); ?>
+				</a>
+			</p>
+
+			<p>
+				<a href="http://preview.themezee.com/anderson/?utm_source=theme-info&utm_medium=textlink&utm_campaign=anderson&utm_content=demo" target="_blank">
+					<?php esc_html_e( 'Theme Demo', 'anderson-lite' ); ?>
+				</a>
+			</p>
+
+			<p>
+				<a href="<?php echo esc_url( __( 'https://themezee.com/docs/anderson-documentation/', 'anderson-lite' ) ); ?>?utm_source=customizer&utm_medium=textlink&utm_campaign=anderson&utm_content=documentation" target="_blank">
+					<?php esc_html_e( 'Theme Documentation', 'anderson-lite' ); ?>
+				</a>
+			</p>
+
+			<p>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/support/theme/anderson/reviews/?filter=5', 'anderson-lite' ) ); ?>" target="_blank">
+					<?php esc_html_e( 'Rate this theme', 'anderson-lite' ); ?>
+				</a>
+			</p>
+
+		</div>
+
+	<?php
+	$theme_links = ob_get_contents();
+	ob_end_clean();
+
+	return $theme_links;
 }
--- readme.txt
+++ readme.txt
@@ -26,6 +26,10 @@ For more information about Anderson Lite please go to https://themezee.com/theme
 
 == Changelog ==
 
+= 1.4.1 =
+* fixed mobile menus on RTL language sites
+* moved Theme Links to Theme Options panel dropdown
+
 = 1.4.0 =
 * added Right-to-Left language support
 
