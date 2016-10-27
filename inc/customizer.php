f9e181bd4f7bf87c4798eb03aa6b84787b6c2002
Theme Directory <noreply@wordpress.org>
2016-10-27 08:46:19 +0000

    New version of Blue Planet - 3.3

--- inc/customizer.php
+++ inc/customizer.php
@@ -18,10 +18,6 @@ function blue_planet_customize_register( $wp_customize ) {
 	$new_defaults = blue_planet_get_default_options();
 	$options = blue_planet_get_option_all();
 
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-
 	// Custom Controls.
 	require get_template_directory() . '/inc/customizer-includes/controls.php';
 
@@ -39,9 +35,54 @@ function blue_planet_customize_register( $wp_customize ) {
 add_action( 'customize_register', 'blue_planet_customize_register' );
 
 /**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ * Render the site title for the selective refresh partial.
+ *
+ * @since 3.3.0
+ *
+ * @return void
  */
-function blue_planet_customize_preview_js() {
-	wp_enqueue_script( 'blue_planet_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+function blue_planet_customize_partial_blogname() {
+	bloginfo( 'name' );
 }
-add_action( 'customize_preview_init', 'blue_planet_customize_preview_js' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @since 3.3.0
+ *
+ * @return void
+ */
+function blue_planet_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Customizer partials.
+ *
+ * @since 3.3.0
+ */
+function blue_planet_customizer_partials( WP_Customize_Manager $wp_customize ) {
+
+    // Abort if selective refresh is not available.
+    if ( ! isset( $wp_customize->selective_refresh ) ) {
+        return;
+    }
+
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+
+    // Partial blogname.
+    $wp_customize->selective_refresh->add_partial( 'blogname', array(
+		'selector'            => '.site-title a',
+		'container_inclusive' => false,
+		'render_callback'     => 'blue_planet_customize_partial_blogname',
+    ) );
+
+    // Partial blogdescription.
+    $wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+		'selector'            => '.site-description',
+		'container_inclusive' => false,
+		'render_callback'     => 'blue_planet_customize_partial_blogdescription',
+    ) );
+}
+add_action( 'customize_register', 'blue_planet_customizer_partials', 99 );
