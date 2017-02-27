e37b935babf82da69160540fad9578e53c1fcf31
Theme Directory <noreply@wordpress.org>
2017-02-27 03:53:38 +0000

    New version of Advance - 1.4.0

--- front-page.php
+++ front-page.php
@@ -156,35 +156,10 @@ get_header(); ?>
                                 
                                 
                                 
- <?php $value = get_theme_mod( 'advance_latest_post', true ); ?>
- <div class="<?php echo ( $value ) ? 'advance-latest-on' : 'advance-latest-off '; ?> ">
-		<div id="content  ">
-  		<div class="row">
-    		<div class="large-9 columns <?php if ( !is_active_sidebar( 'sidebar' ) ){ ?> nosid <?php }?>">	
-  					<?php if ( have_posts() ) : ?>
-			
-						<?php /* Start the Loop */ ?>
-							<?php while ( have_posts() ) : the_post(); ?>
-								<?php
-								/*
-							 	* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-					 		 	* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 			*/
-								get_template_part( 'content', get_post_format() );
-								?>
-
-						 <?php endwhile; ?>
-
-			 		<?php get_template_part('pagination'); ?>  
-
-				<?php else : ?>
-               
-			<?php get_template_part( 'content', 'none' ); ?>
-			
-		<?php endif; ?>
-		</div><!--POST END--> 
  
+ <?php if ( true == get_theme_mod( 'advance_latest_post', true ) ) : ?>
+ <?php get_template_part('layout/part','layout2'); ?>
+  <?php endif; ?>
  			<div class=" wow fadeIn large-3 small-12 columns"> 
    
    				<?php get_sidebar();?>
