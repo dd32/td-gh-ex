15b83070250911fb866dae82dcb808f12124212d
Theme Directory <noreply@wordpress.org>
2017-03-06 19:01:37 +0000

    New version of Looki Lite - 1.1.3

--- core/main.php
+++ core/main.php
@@ -96,6 +96,21 @@ if (!function_exists('lookilite_body_class')) {
 }
 
 /*-----------------------------------------------------------------------------------*/
+/*RESPONSIVE EMBED */
+/*-----------------------------------------------------------------------------------*/ 
+
+if (!function_exists('lookilite_embed_html')) {
+	
+	function lookilite_embed_html( $html ) {
+		return '<div class="embed-container">' . $html . '</div>';
+	}
+	 
+	add_filter( 'embed_oembed_html', 'lookilite_embed_html', 10, 3 );
+	add_filter( 'video_embed_html', 'lookilite_embed_html' );
+	
+}
+
+/*-----------------------------------------------------------------------------------*/
 /* REQUIRE FUNCTION */
 /*-----------------------------------------------------------------------------------*/ 
 
