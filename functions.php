8a8de581e37b38b50484c77647858b33f2ee5b5c
Theme Directory <noreply@wordpress.org>
2017-03-17 16:21:40 +0000

    New version of OneColumn - 5.5

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@
 			add_theme_support( 'title-tag' );
 
 		// Add editor styles
-			add_editor_style( 'custom-editor-style.css' );
+			add_editor_style( array( 'custom-editor-style.css', onecolumn_font_url() ) );
 
 		// Custom header	
 			$header_args = array(		
@@ -74,7 +74,7 @@
 	function onecolumn_scripts() {
 		wp_enqueue_style( 'onecolumn-style', get_stylesheet_uri() );
 		wp_enqueue_script( 'onecolumn-nav', get_template_directory_uri() . '/js/nav.js', array( 'jquery' ) );
-		wp_enqueue_style( 'onecolumn-googlefonts', '//fonts.googleapis.com/css?family=Open+Sans' ); 
+		wp_enqueue_style( 'onecolumn-googlefonts', onecolumn_font_url() ); 
 
 		// Add html5 support for IE 8 and older 
 		wp_enqueue_script( 'onecolumn_html5', get_template_directory_uri() . '/js/ie.js' );
@@ -92,6 +92,13 @@
 		wp_localize_script( 'onecolumn-nav', 'objectL10n', $onecolumn_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'onecolumn_scripts' );
+
+
+// Font family
+	function onecolumn_font_url() {
+		$font_url = '//fonts.googleapis.com/css?family=Open+Sans';
+		return esc_url_raw( $font_url );
+	}
 
 
 // Sidebars
