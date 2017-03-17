4764a9047cdc8ea742cd00837cbbe358293f71b4
Theme Directory <noreply@wordpress.org>
2017-03-17 16:21:31 +0000

    New version of MyKnowledgeBase - 5.0

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@
 			add_theme_support( 'title-tag' );
 
 		// Add editor styles
-			add_editor_style( 'custom-editor-style.css' );
+			add_editor_style( array( 'custom-editor-style.css', myknowledgebase_font_url() ) );
 
 		// Custom header	
 			$header_args = array(		
@@ -74,7 +74,7 @@
 	function myknowledgebase_scripts() {
 		wp_enqueue_style( 'myknowledgebase-style', get_stylesheet_uri() );
 		wp_enqueue_script( 'myknowledgebase-nav', get_template_directory_uri() . '/js/nav.js', array( 'jquery' ) );
-		wp_enqueue_style( 'myknowledgebase-googlefonts', '//fonts.googleapis.com/css?family=Open+Sans' ); 
+		wp_enqueue_style( 'myknowledgebase-googlefonts', myknowledgebase_font_url() ); 
 
 		// Add html5 support for IE 8 and older 
 		wp_enqueue_script( 'myknowledgebase_html5', get_template_directory_uri() . '/js/ie.js' );
@@ -92,6 +92,13 @@
 		wp_localize_script( 'myknowledgebase-nav', 'objectL10n', $myknowledgebase_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'myknowledgebase_scripts' );
+
+
+// Font family
+	function myknowledgebase_font_url() {
+		$font_url = '//fonts.googleapis.com/css?family=Open+Sans';
+		return esc_url_raw( $font_url );
+	}
 
 
 // Sidebars
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === MyKnowledgeBase ===
 Contributors: Guido07111975
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=donation%40guidovanderleest%2enl
-Version: 4.9
+Version: 5.0
 Requires at least: 4.2
 Tested up to: 4.7
 License: GNU General Public License v3 or later
