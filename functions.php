617b598063cd63d120b9642b16030790251a51c7
Theme Directory <noreply@wordpress.org>
2017-03-17 16:21:06 +0000

    New version of LeftSide - 4.7

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@
 			add_theme_support( 'title-tag' );
 
 		// Add editor styles
-			add_editor_style( 'custom-editor-style.css' );
+			add_editor_style( array( 'custom-editor-style.css', leftside_font_url() ) );
 
 		// Custom header	
 			$header_args = array(		
@@ -74,7 +74,7 @@
 	function leftside_scripts() {
 		wp_enqueue_style( 'leftside-style', get_stylesheet_uri() );
 		wp_enqueue_script( 'leftside-nav', get_template_directory_uri() . '/js/nav.js', array( 'jquery' ) );
-		wp_enqueue_style( 'leftside-googlefonts', '//fonts.googleapis.com/css?family=Open+Sans' ); 
+		wp_enqueue_style( 'leftside-googlefonts', leftside_font_url() ); 
 
 		// Add html5 support for IE 8 and older 
 		wp_enqueue_script( 'leftside_html5', get_template_directory_uri() . '/js/ie.js' );
@@ -92,6 +92,13 @@
 		wp_localize_script( 'leftside-nav', 'objectL10n', $leftside_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'leftside_scripts' );
+
+
+// Font family
+	function leftside_font_url() {
+		$font_url = '//fonts.googleapis.com/css?family=Open+Sans';
+		return esc_url_raw( $font_url );
+	}
 
 
 // Sidebars
