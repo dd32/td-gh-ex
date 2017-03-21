75674183e021aed67b0d2d3ab5fe709b04d262cb
Theme Directory <noreply@wordpress.org>
2017-03-21 13:47:54 +0000

    New version of Multishop - 1.4

--- theme-options/fasterthemes.php
+++ theme-options/fasterthemes.php
@@ -186,12 +186,9 @@ function fastertheme_framework_page(){
           <?php endfor; ?>
         </div>
 		   <div id="options-group-4" class="group faster-inner-tabs fasterthemes-pro-image">
-          	<div class="fasterthemes-pro-header">
-              <img src="<?php echo get_template_directory_uri(); ?>/theme-options/images/multishop_logopro_features.png" class="fasterthemes-pro-logo" />
-              <a href="http://fasterthemes.com/wordpress-themes/multishop" target="_blank">
-			  <img src="<?php echo get_template_directory_uri(); ?>/theme-options/images/buy-now.png" class="fasterthemes-pro-buynow" /></a>
+              <a href="https://fasterthemes.com/wordpress-themes/multishop" target="_blank">
+                <img src="<?php echo get_template_directory_uri(); ?>/theme-options/images/multishop_pro_features.png" />
               </div>
-          	<img src="<?php echo get_template_directory_uri(); ?>/theme-options/images/multishop_pro_features.png" />
           </div>
         <!--  F I N A L - - T H E M E - - O P T I O N S  --> 
       </div>
