70c85f0f6c1622b7661c0846268b1d9b570895ff
Theme Directory <noreply@wordpress.org>
2017-03-23 09:05:13 +0000

    New version of Digital - 1.5

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar">
-<?php get_template_part('/includes/popular'); ?>
+<?php if (of_get_option('digital_popular' ) =='1' ) {get_template_part('/includes/popular'); } ?>
 	<?php if (!dynamic_sidebar('digsidebar') ) : ?>				
 		<?php endif; ?>	</div>	<!-- end div #sidebar -->
 
