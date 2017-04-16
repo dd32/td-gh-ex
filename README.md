7853189e040d837d7369187053838e7f1bb0010b
Theme Directory <noreply@wordpress.org>
2017-04-16 15:05:47 +0000

    New theme: Adaptativo - 1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+Adaptativo
+==========
+Contributors: Webempresa
+Tags: Accessibility Ready, Custom Header, Background header, Custom Logo, Custom Menu, Editor Style, Featured Images, Flexible Header, Footer Widgets, One Column, Right Sidebar, RTL Language Support, Sticky Post, Theme Options, Threaded Comments, Translation Ready.
+Requires at least: 4.0
+Tested up to: 4.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Adaptativo is a theme designed especially for a personal blog, it brings a beauty home page with a custom background header, custom html widget and featured images in entries. 
+
+
+
---
+++ footer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Adaptativo
+ */
+
+ require_once get_template_directory().'/inc/helper.php';
+
+?>
+
+	</div><!-- #content -->
+
+
+	<!-- Content-bottom -->
+	<?php if ( is_my_home() &&  is_active_sidebar('home-content-bottom') ): ?>
+		<div class="container home-content-bottom">
+				<?php dynamic_sidebar( 'home-content-bottom' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( ! is_my_home() &&  is_active_sidebar('content-bottom') ): ?>
+		<div class="container content-bottom">
+				<?php dynamic_sidebar( 'content-bottom' ); ?>
+		</div>
+	<?php endif; ?>
+	<!-- Fin content-bottom -->
+
+</div><!-- wrap-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="<?php echo has_nav_menu('menu-2')?'col-6':''; ?> copyright">
+          <span><?php _e( 'Proudly powered by ', 'adaptativo' ) ?></span>	<a href="https://wordpress.org/">WordPress</a>
+    			<br/>©<span> <?php _e('Theme Adaptativo designed by', 'adaptativo'); ?> </span> <a href="https://www.webempresa.com/" rel="designer">Webempresa</a>
+
+        </div><!-- .site-info -->
+
+
+        <?php
+        if ( has_nav_menu('menu-2') ){
+          wp_nav_menu( array( 'theme_location' => 'menu-2', 'menu_id' => 'footer-menu', 'container_class' => 'col-6') );
+        }
+        ?>
+
+      </div>
+
+    </div>
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
