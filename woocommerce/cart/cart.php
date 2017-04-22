cd5765d13aaa126e463d9d9717119ce9b0b9ce8c
Theme Directory <noreply@wordpress.org>
2017-04-22 16:44:07 +0000

    New version of Alhena Lite - 2.2.0

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.0.0
+ * @version 3.0.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -86,7 +86,7 @@ do_action( 'woocommerce_before_cart' ); ?>
                                         ?>
                                     </td>
             
-                                    <td class="product-name" data-title="<?php _e( 'Product', 'alhena-lite' ); ?>">
+                                    <td class="product-name" data-title="<?php esc_attr_e( 'Product', 'alhena-lite' ); ?>">
                                         <?php
                                             if ( ! $product_permalink ) {
                                                 echo apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;';
@@ -104,13 +104,13 @@ do_action( 'woocommerce_before_cart' ); ?>
                                         ?>
                                     </td>
             
-                                    <td class="product-price" data-title="<?php _e( 'Price', 'alhena-lite' ); ?>">
+                                    <td class="product-price" data-title="<?php esc_attr_e( 'Price', 'alhena-lite' ); ?>">
                                         <?php
                                             echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
                                         ?>
                                     </td>
             
-                                    <td class="product-quantity" data-title="<?php _e( 'Quantity', 'alhena-lite' ); ?>">
+                                    <td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'alhena-lite' ); ?>">
                                         <?php
                                             if ( $_product->is_sold_individually() ) {
                                                 $product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
@@ -127,7 +127,7 @@ do_action( 'woocommerce_before_cart' ); ?>
                                         ?>
                                     </td>
             
-                                    <td class="product-subtotal" data-title="<?php _e( 'Total', 'alhena-lite' ); ?>">
+                                    <td class="product-subtotal" data-title="<?php esc_attr_e( 'Total', 'alhena-lite' ); ?>">
                                         <?php
                                             echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key );
                                         ?>
