11cd632f6b393c5b3e4258b845032968d0378f32
Theme Directory <noreply@wordpress.org>
2017-05-03 05:31:45 +0000

    New version of Avvocato - 1.0.9

--- header.php
+++ header.php
@@ -24,8 +24,8 @@
 						<div class="column-8-12 left">
 							<div class="gutter">
 								<ul class="top-bar-contact">
-								    <?php if(get_theme_mod('avvocato_header_phone')) { ?><li><i class="fa fa-phone"></i><a href="phone:<?php echo esc_attr(get_theme_mod('avvocato_header_phone')); ?>"><?php echo esc_attr(get_theme_mod('avvocato_header_phone')); ?></a></li><?php } ?>
-									<?php if(get_theme_mod('avvocato_header_email')) { ?><li><i class="fa fa-envelope"></i><a href="<?php echo esc_url( 'mailto:' . antispambot( get_theme_mod( 'avvocato_header_email' ) ) ); ?>"><?php echo antispambot(sanitize_email(get_theme_mod('avvocato_header_email'))); ?></a></li><?php } ?>
+								    <?php if(get_theme_mod('avvocato_header_phone')) { ?><li><i class="fa fa-phone"></i><a href="<?php echo esc_url( 'phone:'. esc_attr(get_theme_mod('avvocato_header_phone'))); ?>"><?php echo esc_attr(get_theme_mod('avvocato_header_phone')); ?></a></li><?php } ?>
+									<?php if(get_theme_mod('avvocato_header_email')) { ?><li><i class="fa fa-envelope"></i><a href="<?php echo esc_url( 'mailto:' . antispambot(sanitize_email(get_theme_mod('avvocato_header_email'))); ?>"><?php echo antispambot(sanitize_email(get_theme_mod('avvocato_header_email'))); ?></a></li><?php } ?>
 								</ul>
 							</div>
 						</div>
