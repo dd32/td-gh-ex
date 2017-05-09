7725d4010ae49244a5ae3e493ab5476c752649fe
Theme Directory <noreply@wordpress.org>
2017-05-09 16:12:17 +0000

    New version of QuickPress - 1.8.5

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ function quickpress_excerpt_more( $more ) {
 // credits
 function quickpress_credits() {
     echo '<p class="text-center">
-	<a href="http://www.quickonlinetips.com/archives/quickpress/" title="QuickPress Theme" rel="nofollow">QuickPress Theme</a>' 
+	<a href="http://www.quickonlinetips.com/archives/quickpress/" title="QuickPress Theme" rel="nofollow" target="_blank">QuickPress Theme</a>' 
  	. __(' powered by ', 'quickpress') . 
 	'<a href="http://wordpress.org">WordPress</a>
 	</p>';
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Theme URI: http://www.quickonlinetips.com/archives/quickpress/
 Description: A lightweight, 2-3 columns, widget ready, SEO optimized, fast loading, easy to customize wordpress theme.
 
 Author: QuickOnlineTips
-Author URI: http://www.quickonlinethemes.com/
+Author URI: http://www.quickonlinetips.com/
 Tags: white, blue, gray, silver, light, two-columns, three-columns, right-sidebar, fixed-width, threaded-comments, sticky-post
 
 License: GNU General Public License v2.0
