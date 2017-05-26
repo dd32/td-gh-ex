f12dfd3674670530a61806d22e1f7bd922c3abc6
Theme Directory <noreply@wordpress.org>
2017-05-26 12:18:04 +0000

    New version of Affluent - 1.0.5

--- template-parts/element-portfolio.php
+++ template-parts/element-portfolio.php
@@ -1,16 +1,15 @@
 <div class="portfolio-item">
-	<a class="portfolio-item-image" href="<?php the_permalink(); ?>">
-		<div class="portfolio-item-overlay dark primary-color-bg">
-			<h3 class="portfolio-item-title">
-				<?php the_title(); ?>
-			</h3>
-			<?php if(has_excerpt()): ?>
-			<div class="portfolio-item-description">
-				<?php the_excerpt(); ?>
-			</div>
-			<?php endif; ?>
-			<?php cpotheme_edit(); ?>
+	<a class="portfolio-item-image" href="<?php the_permalink(); ?>"></a>
+	<div class="portfolio-item-overlay dark primary-color-bg">
+		<h3 class="portfolio-item-title">
+			<?php the_title(); ?>
+		</h3>
+		<?php if(has_excerpt()): ?>
+		<div class="portfolio-item-description">
+			<?php the_excerpt(); ?>
 		</div>
-		<?php the_post_thumbnail('cpotheme-portfolio', array('title' => '')); ?>
-	</a>
+		<?php endif; ?>
+	</div>
+	<?php cpotheme_edit(); ?>
+	<?php the_post_thumbnail('cpotheme-portfolio', array('title' => '')); ?>
 </div>
