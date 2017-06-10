6af8a4373da07ca92dee07d766c0e204bb3b81a4
Theme Directory <noreply@wordpress.org>
2017-06-10 16:56:18 +0000

    New version of Alhena Lite - 2.2.4

--- attachment.php
+++ attachment.php
@@ -12,9 +12,7 @@
             
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-                <header class="title">
-                    <div class="line"><h1><a href="<?php echo get_permalink($post->ID); ?>"><?php the_title(); ?></a></h1></div>
-                </header>
+                <header class="title"><div class="line"><h1><a href="<?php echo get_permalink($post->ID); ?>"><?php the_title(); ?></a></h1></div></header>
 
                 <div class="entry-info">
                    
@@ -25,22 +23,19 @@
                 </div>
 
 				<p> 
-				
-				<?php if (wp_attachment_is_image($post->id)) {
-				$att_image = wp_get_attachment_image_src( $post->id, "blog");
-				?>
-					<a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>">
-					<img src="<?php echo $att_image[0];?>" width="<?php echo $att_image[1];?>" height="<?php echo $att_image[2];?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" />
-					</a>
-				<?php } else { ?>
-                
-                	<a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>">
-					<?php the_title(); ?>
-					</a>
 
+					<?php if (wp_attachment_is_image($post->id)) { ?>
+    
+                        <a data-rel="prettyPhoto" href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php echo the_title_attribute(); ?>">
+                            <?php echo wp_get_attachment_image($post->id, 'blog'); ?>
+                        </a>
+    
+                    <?php } else { ?>
+                            
+                        <a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+            
+                    <?php } ?>
 
-                <?php } ?>
-                
 				</p>
                 
                 </div>
