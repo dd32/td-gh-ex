bfd0d5adcfe5ad11feb46f65d1e9ea62cf510e9f
Theme Directory <noreply@wordpress.org>
2017-06-16 13:03:32 +0000

    New version of Shipyard - 5.5

--- header.php
+++ header.php
@@ -31,6 +31,10 @@
 
 		<?php if ( has_nav_menu( 'primary' ) ) : ?> 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'nav-head' ) ); ?>
+			<div class="mobile-nav-toggle"><?php _e( 'Menu', 'shipyard' ); ?><?php _e( ' +', 'shipyard' ); ?></div>
+			<div class="mobile-nav">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</div>
 		<?php endif; ?>
 
 		<?php if ( is_front_page() ) {?> 
