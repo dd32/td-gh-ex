c9856bb6048f38c81452e216a92dc97708d407d1
Theme Directory <noreply@wordpress.org>
2017-06-16 13:03:41 +0000

    New version of SimplyBlack - 5.5

--- functions.php
+++ functions.php
@@ -83,13 +83,6 @@
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
-
-		// mobile nav args
-		$simplyblack_mobile_nav_args = array(
-			'navText' => __( 'Menu', 'simplyblack' )
-		);
-		// localize script with data for mobile nav
-		wp_localize_script( 'simplyblack-nav', 'objectL10n', $simplyblack_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'simplyblack_scripts' );
 
