972842307df1e5e340f707f826fad2b36fd9af6e
Theme Directory <noreply@wordpress.org>
2017-06-16 13:01:53 +0000

    New version of BlueGray - 5.4

--- functions.php
+++ functions.php
@@ -83,13 +83,6 @@
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
-
-		// mobile nav args
-		$bluegray_mobile_nav_args = array(
-			'navText' => __( 'Menu', 'bluegray' )
-		);
-		// localize script with data for mobile nav
-		wp_localize_script( 'bluegray-nav', 'objectL10n', $bluegray_mobile_nav_args );
 	}
 	add_action( 'wp_enqueue_scripts', 'bluegray_scripts' );
 
