bdfad6d11693828fab18e31935f63edd19c02a93
Theme Directory <noreply@wordpress.org>
2017-07-05 06:22:53 +0000

    New version of Begonia Lite - 1.1.0

--- template-parts/toolbar.php
+++ template-parts/toolbar.php
@@ -3,7 +3,15 @@
  * The template for the lateral toolbar.
  * @package Begonia
  */ ?>
-			<nav id="social-navigation" class="toolbar-navigation" role="navigation">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<?php _e( '', 'begonia-lite' ); ?>
+					<span class="c-burger">
+						<b class="c-burger__slice c-burger__slice--top"></b>
+						<b class="c-burger__slice c-burger__slice--middle"></b>
+						<b class="c-burger__slice c-burger__slice--bottom"></b>
+					</span>
+				</button>
 				<h5 class="screen-reader-text"><?php _e( 'Main navigation', 'begonia-lite' ); ?></h5>
 				<?php
 				wp_nav_menu(
