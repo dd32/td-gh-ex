41f24b6322deb1c18973e8ff5cec6caedc8003cf
Theme Directory <noreply@wordpress.org>
2017-07-10 21:02:36 +0000

    New theme: AyaPortfolio - 1.0.0

---
+++ js/utilities.js
@@ -0,0 +1,80 @@
+(function($) {
+
+	"use strict";
+
+	$( document ).ready(function() {
+
+		$(window).scroll(function () {
+
+			if ($(this).scrollTop() > 100) {
+
+				$('.scrollup').fadeIn();
+
+			} else {
+
+				$('.scrollup').fadeOut();
+
+			}
+		});
+
+		$('.scrollup').click(function () {
+			
+			$("html, body").animate({
+				  scrollTop: 0
+			  }, 600);
+
+			return false;
+		});
+
+		ayaportfolio_mainMenuInit();
+
+		$('#navmain > div').on('click', function(e) {
+
+			e.stopPropagation();
+
+			// toggle main menu
+			if ( $(window).width() < 800 ) {
+
+				var parentOffset = $(this).parent().offset(); 
+				
+				var relY = e.pageY - parentOffset.top;
+			
+				if (relY < 36) {
+				
+					$('ul:first-child', this).toggle(400);
+				}
+			}
+		});
+
+		// re-init main menu on screen resize
+		$(window).resize(function () {
+	       
+	    	ayaportfolio_mainMenuClear();
+	    	ayaportfolio_mainMenuInit();
+		});
+	});
+
+	function ayaportfolio_mainMenuClear() {
+
+		if ( $(window).width() >= 800 ) {
+		
+			$('#navmain > div > ul > li:has("ul")').removeClass('level-one-sub-menu');
+			$('#navmain > div > ul li ul li:has("ul")').removeClass('level-two-sub-menu');										
+		}
+
+		if ( $('ul:first-child', $('#navmain > div') ).is( ":visible" ) ) {
+
+			$('ul:first-child', $('#navmain > div') ).css('display', '');
+		}
+	}
+
+	function ayaportfolio_mainMenuInit() {
+
+		if ( $(window).width() >= 800 ) {
+		
+			$('#navmain > div > ul > li:has("ul")').addClass('level-one-sub-menu');
+			$('#navmain > div > ul li ul li:has("ul")').addClass('level-two-sub-menu');										
+		}
+	}
+
+})(jQuery);
