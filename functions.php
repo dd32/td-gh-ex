228c459fe0db4328484395b10cdc4b187f27b76d
Theme Directory <noreply@wordpress.org>
2017-08-13 01:05:26 +0000

    New version of Avata - 1.2.4

--- functions.php
+++ functions.php
@@ -40,6 +40,11 @@ function avata_setup(){
 	
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
+	// Woocommerce Support
+	add_theme_support( 'woocommerce' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 
 add_action( 'after_setup_theme', 'avata_setup' );
