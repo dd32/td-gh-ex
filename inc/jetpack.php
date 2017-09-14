f8d63c9fda493bedffcf82412afd234e23c9c196
Theme Directory <noreply@wordpress.org>
2017-09-14 17:50:24 +0000

    New version of Blogghiamo - 1.6.1

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -12,6 +12,7 @@
  *
  * See: https://jetpack.com/support/infinite-scroll/
  * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
  */
 function blogghiamo_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
@@ -22,6 +23,18 @@ function blogghiamo_jetpack_setup() {
 	
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
+	
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => 'blogghiamo-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+	) );
 }
 add_action( 'after_setup_theme', 'blogghiamo_jetpack_setup' );
 
