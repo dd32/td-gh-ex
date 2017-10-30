4c39b5f6ebeab0c96b367fe386416837cf619f6e
Theme Directory <noreply@wordpress.org>
2017-10-30 21:53:25 +0000

    New theme: AyaClub - 1.0.0

---
+++ js/utilities.js
@@ -0,0 +1,149 @@
+(function($) {
+
+	"use strict";
+
+	$( document ).ready(function() {
+
+		$('#header-spacer').height( $('#header-main-fixed').height() );
+
+		if ($('#wpadminbar').length > 0) {
+		
+			$('#header-main-fixed').css('top', $('#wpadminbar').height() + 'px');
+			$('#wpadminbar').css('position', 'fixed');
+		}
+
+		$(window).scroll(function () {
+
+			if ($(this).scrollTop() > 100) {
+
+				$('.scrollup').fadeIn();
+
+			} else {
+
+				$('.scrollup').fadeOut();
+
+			}
+		});
+
+		$('.scrollup').click(function () {
+			
+			$("html, body").animate({
+				  scrollTop: 0
+			  }, 600);
+
+			return false;
+		});
+
+		ayaclub_mainMenuInit();
+
+		if (ayaclub_options && ayaclub_options.loading_effect) {
+			ayaclub_init_loading_effects();
+		}
+
+		$('#navmain > div').on('click', function(e) {
+
+			e.stopPropagation();
+
+			// toggle main menu
+			if ( $(window).width() < 800 ) {
+
+				var parentOffset = $(this).parent().offset(); 
+				
+				var relY = e.pageY - parentOffset.top;
+			
+				if (relY < 36) {
+				
+					$('ul:first-child', this).toggle(400);
+				}
+			}
+		});
+
+		// re-init main menu on screen resize
+		$(window).resize(function () {
+	       
+	    	ayaclub_mainMenuClear();
+	    	ayaclub_mainMenuInit();
+		});
+	});
+
+	function ayaclub_mainMenuClear() {
+
+		if ( $(window).width() >= 800 ) {
+		
+			$('#navmain > div > ul > li:has("ul")').removeClass('level-one-sub-menu');
+			$('#navmain > div > ul li ul li:has("ul")').removeClass('level-two-sub-menu');										
+		}
+
+		if ( $('ul:first-child', $('#navmain > div') ).is( ":visible" ) ) {
+
+			$('ul:first-child', $('#navmain > div') ).css('display', '');
+		}
+	}
+
+	function ayaclub_mainMenuInit() {
+
+		if ( $(window).width() >= 800 ) {
+		
+			$('#navmain > div > ul > li:has("ul")').addClass('level-one-sub-menu');
+			$('#navmain > div > ul li ul li:has("ul")').addClass('level-two-sub-menu');										
+		}
+	}
+
+	function ayaclub_init_loading_effects() {
+
+	    $('#header-logo').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated fadeInLeft',
+	            offset: 1
+	          });
+
+	    $('#navmain').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounceInRight',
+	            offset: 1
+	          });
+
+	    $('#page-header, article h1').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounceInUp',
+	            offset: 1
+	          });
+
+	    $('#main-content-wrapper h2, #main-content-wrapper h3')
+	            .addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounceInUp',
+	            offset: 1
+	          });
+
+	    $('img').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated zoomIn',
+	            offset: 1
+	          });
+
+	    $('#sidebar, .col3a, .col3b, .col3c').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounceInRight',
+	            offset: 1
+	          });
+
+	    $('.before-content, .after-content').addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounce',
+	            offset: 1
+	          });
+
+	    $('article p, article li')
+	        .addClass("hidden").viewportChecker({
+	            classToAdd: 'animated fadeInLeft',
+	            offset: 1
+	          });
+
+	    $('#footer-main h1, #footer-main h2, #footer-main h3')
+	        .addClass("hidden").viewportChecker({
+	            classToAdd: 'animated bounceInUp',
+	            offset: 1
+	          });
+
+	    $('#footer-main p, #footer-main li')
+	        .addClass("hidden").viewportChecker({
+	            classToAdd: 'animated fadeInLeft',
+	            offset: 1
+	          });
+	}
+
+})(jQuery);
