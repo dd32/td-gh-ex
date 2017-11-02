e660445aae6c49390c834d73a674c51734aef722
Theme Directory <noreply@wordpress.org>
2017-11-02 19:08:11 +0000

    New version of OneColumn - 6.0

--- functions.php
+++ functions.php
@@ -186,6 +186,65 @@
 			'section' => 'onecolumn_logo_section', 
 			'settings' => 'onecolumn_logo', 
 		) ) );
+		$wp_customize->add_section( 'onecolumn_blog_section' , array( 
+			'title' => __( 'Blog Page', 'onecolumn' ), 
+			'priority' => 31, 
+			'description' => __( 'Set a page title and content above your posts.', 'onecolumn' ),
+		) );
+		$wp_customize->add_setting( 'onecolumn_blog_title', array( 
+			'capability' => 'edit_theme_options', 
+			'sanitize_callback' => 'sanitize_text_field', 
+		) ); 
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'onecolumn_blog_title', array( 
+			'label' => __( 'Title', 'onecolumn' ), 
+			'section' => 'onecolumn_blog_section', 
+			'settings' => 'onecolumn_blog_title', 
+		) ) );
+		$wp_customize->add_setting( 'onecolumn_blog_content', array( 
+			'capability' => 'edit_theme_options', 
+			'sanitize_callback' => 'wp_kses_post', 
+		) ); 
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'onecolumn_blog_content', array( 
+			'label' => __( 'Content', 'onecolumn' ), 
+			'type' => 'textarea', 
+			'section' => 'onecolumn_blog_section', 
+			'settings' => 'onecolumn_blog_content', 
+		) ) );
+		$wp_customize->add_section( 'onecolumn_post_section' , array( 
+			'title' => __( 'Posts', 'onecolumn' ), 
+			'priority' => 32, 
+			'description' => __( 'Customize the way how posts are displayed.', 'onecolumn' ),
+		) );
+		$wp_customize->add_setting( 'onecolumn_content_type', array( 
+			'capability' => 'edit_theme_options', 
+			'sanitize_callback' => 'sanitize_text_field', 
+			'default' => 'yes', 
+		) ); 
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'onecolumn_content_type', array( 
+			'label' => __( 'Show a summary', 'onecolumn' ), 
+			'section' => 'onecolumn_post_section', 
+			'settings' => 'onecolumn_content_type', 
+			'type' => 'radio', 
+			'choices' => array( 
+				'yes' => __('Yes', 'onecolumn'), 
+				'no' => __('No', 'onecolumn'), 
+			), 
+		) ) );
+		$wp_customize->add_setting( 'onecolumn_read_more', array( 
+			'capability' => 'edit_theme_options', 
+			'sanitize_callback' => 'sanitize_text_field', 
+			'default' => 'yes', 
+		) ); 
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'onecolumn_read_more', array( 
+			'label' => __( 'Show Read More button', 'onecolumn' ), 
+			'section' => 'onecolumn_post_section', 
+			'settings' => 'onecolumn_read_more', 
+			'type' => 'radio', 
+			'choices' => array( 
+				'yes' => __('Yes', 'onecolumn'), 
+				'no' => __('No', 'onecolumn'), 
+			), 
+		) ) );
 	} 
 	add_action('customize_register', 'onecolumn_theme_customizer');
 
