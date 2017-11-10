2935f820122abf4809ab907721a5ee29a9722810
Theme Directory <noreply@wordpress.org>
2017-11-10 13:49:29 +0000

    New version of Astra - 1.0.30

--- inc/core/view-general.php
+++ inc/core/view-general.php
@@ -3,32 +3,45 @@
  * View General
  *
  * @package     Astra
- * @author      Brainstorm Force
- * @copyright   Copyright (c) 2015, Brainstorm Force
- * @link        http://www.brainstormforce.com
+ * @author      Astra
+ * @copyright   Copyright (c) 2017, Astra
+ * @link        http://wpastra.com/
  * @since       Astra 1.0
  */
 
+$astra_theme_name = apply_filters( 'astra_theme_name', __( 'Astra', 'astra' ) );
 ?>
 
-<dev class="wrap ast-clear">
+<div class="wrap ast-clear">
+	<h1 class="screen-reader-text"> <?php esc_html_e( 'Astra', 'astra' ); ?> </h1>
 	<div id="poststuff">
 		<div id="post-body" class="metabox-holder">
 			<div id="postbox-container-2" class="postbox-container astra-row">
 
 				<div id="normal-sortables-1" class="astra-col-33">
-					 <div class="postbox ">
-						<h2 class="ui-sortable-handle"><span><?php _e( 'Welcome to Astra', 'astra' ); ?></span></h2>
+					<div class="postbox ">
+						<h2 class="ui-sortable-handle">
+							<span>
+								<?php
+
+								/* translators: 1: Astra Theme name */
+								printf( esc_html__( 'Welcome to %1$s', 'astra' ), $astra_theme_name );
+								?>
+							</span>
+						</h2>
 						<div class="inside">
 							<p>
-								<?php _e( 'Astra is a very lightweight and beautiful theme made to work with Page Builders.', 'astra' ); ?>
+								<?php
+								/* translators: 1: Astra Theme name */
+								printf( esc_html__( '%1$s is a very lightweight and beautiful theme made to work with Page Builders.', 'astra' ), $astra_theme_name );
+								?>
 							</p>
 							<p>
-								<?php _e( 'Go ahead and start customizing your website.', 'astra' ); ?>
+								<?php esc_html_e( 'Go ahead and start customizing your website.', 'astra' ); ?>
 							</p>
-							<a class="submit button button-primary" href="<?php echo esc_url( admin_url( 'customize.php' ) ); ?>"><?php _e( 'Customize', 'astra' ); ?></a>
+							<a class="submit button button-primary" href="<?php echo esc_url( admin_url( 'customize.php' ) ); ?>"><?php esc_html_e( 'Customize', 'astra' ); ?></a>
 						</div>
-					 </div>
+					</div>
 				</div>
 
 			</div><!-- #postbox-container-2 -->
@@ -36,4 +49,4 @@
 		</div>
 	</div>
 
-</dev>
+</div>
--- sidebar.php
+++ sidebar.php
@@ -8,23 +8,23 @@
  * @since 1.0.0
  */
 
-$sidebar = apply_filters( 'ast_get_sidebar', 'sidebar-1' );
+$sidebar = apply_filters( 'astra_get_sidebar', 'sidebar-1' );
 
 ?>
 
-<div itemtype="http://schema.org/WPSideBar" itemscope="itemscope" id="secondary" <?php ast_secondary_class(); ?> role="complementary">
+<div itemtype="http://schema.org/WPSideBar" itemscope="itemscope" id="secondary" <?php astra_secondary_class(); ?> role="complementary">
 
 	<div class="sidebar-main">
 
-		<?php ast_sidebars_before(); ?>
-		
+		<?php astra_sidebars_before(); ?>
+
 		<?php if ( is_active_sidebar( $sidebar ) ) : ?>
 
 			<?php dynamic_sidebar( $sidebar ); ?>
 
 		<?php endif; ?>
-		
-		<?php ast_sidebars_after(); ?>
+
+		<?php astra_sidebars_after(); ?>
 
 	</div><!-- .sidebar-main -->
 </div><!-- #secondary -->
