7761796cff41df63e18539c69ab56b3f41c18291
Theme Directory <noreply@wordpress.org>
2017-12-03 17:11:31 +0000

    New version of Baskerville - 1.24

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="wrapper section medium-padding">
 
 	<?php
-	$paged = get_query_var( 'paged' ) ?: 1;
+	$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
 	$total_post_count = wp_count_posts();
 	$published_post_count = $total_post_count->publish;
 	$total_pages = ceil( $published_post_count / $posts_per_page );
