44a8eff431bcda4587a50eb7b49946ad2140be5a
Theme Directory <noreply@wordpress.org>
2017-12-20 23:21:59 +0000

    New version of AyaFreelance - 1.0.7

--- functions.php
+++ functions.php
@@ -95,6 +95,7 @@ if ( ! function_exists( 'ayafreelance_setup' ) ) :
 		// This theme uses wp_nav_menu() in header menu
 		register_nav_menus( array(
 			'primary'   => __( 'Primary Menu', 'ayafreelance' ),
+			'footer'    => __( 'Footer Menu', 'ayafreelance' ),
 		) );
 
 		$defaults = array(
