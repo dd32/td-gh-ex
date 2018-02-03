5ff4a90ba6305644907840e80b7d68bcdbb76439
Theme Directory <noreply@wordpress.org>
2018-02-03 16:48:05 +0000

    New version of Applicator - 2.2.5.64

--- inc/functions/post-content.php
+++
@@ -1,8 +0,0 @@
-<?php // Entry Content
-// To enable Child Themes to hook up conditionals in displaying single content without duplicating index.php
-
-if ( ! function_exists( 'applicator_post_content' ) ) {
-    function applicator_post_content() {
-        get_template_part( 'content', get_post_format() );
-    }
-}
