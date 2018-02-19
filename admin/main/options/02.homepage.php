fc9c98a8c6612d018920b904319e4eec11f85ff4
Theme Directory <noreply@wordpress.org>
2018-02-19 17:21:16 +0000

    New version of Renden - 1.3.21

--- admin/main/options/02.homepage.php
+++ admin/main/options/02.homepage.php
@@ -30,7 +30,7 @@ $slider_toggle           = NULL;
 			}
 		}
 
-		if ( empty( $thinkup_homepage_sliderswitch ) or $thinkup_homepage_sliderswitch == 'option1' ) {
+		if ( ( current_user_can( 'edit_theme_options' ) and empty( $thinkup_homepage_sliderswitch ) ) or $thinkup_homepage_sliderswitch == 'option1' ) {
 
 			echo '<div id="slider"><div id="slider-core">',
 			     '<div class="rslides-container"><div class="rslides-inner"><ul class="slides">';
@@ -217,7 +217,7 @@ global $thinkup_homepage_section4_link;
 	}
 
 	if ( is_front_page() or thinkup_check_ishome() ) {
-		if ( empty( $thinkup_homepage_sectionswitch ) or $thinkup_homepage_sectionswitch == '1' ) {
+		if ( ( current_user_can( 'edit_theme_options' ) and empty( $thinkup_homepage_sectionswitch ) ) or $thinkup_homepage_sectionswitch == '1' ) {
 
 		echo '<div id="section-home"><div id="section-home-inner">';
 
--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Declare latest theme version
-$GLOBALS['thinkup_theme_version'] = '1.3.20';
+$GLOBALS['thinkup_theme_version'] = '1.3.21';
 
 // Setup content width 
 function thinkup_content_width() {
