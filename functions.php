7b8898fb8c1fae3410c75c080c96b03c1f5e7d3f
Theme Directory <noreply@wordpress.org>
2018-03-12 18:10:28 +0000

    New version of Renden - 1.4.1

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Declare latest theme version
-$GLOBALS['thinkup_theme_version'] = '1.4.0';
+$GLOBALS['thinkup_theme_version'] = '1.4.1';
 
 // Setup content width 
 function thinkup_content_width() {
