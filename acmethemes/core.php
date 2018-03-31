1c2f81585a539bdd88901225348ef159f456325d
Theme Directory <noreply@wordpress.org>
2018-03-31 06:23:44 +0000

    New version of AcmeBlog - 1.4.2

--- acmethemes/core.php
+++ acmethemes/core.php
@@ -117,7 +117,7 @@ function acmeblog_scripts() {
 	}
 
     /*bxslider js*/
-    wp_enqueue_script('bxslider', get_template_directory_uri() . '/assets/library/bxslider/js/jquery.bxslider.min.js', array('jquery'), '4.2.5', 1);
+    wp_enqueue_script('bxslider', get_template_directory_uri() . '/assets/library/bxslider/js/jquery.bxslider.min.js', array('jquery'), '4.2.51', 1);
 
     /*custom-js*/
     wp_enqueue_script('acmeblog-custom', get_template_directory_uri() . '/assets/js/acmeblog-custom.js', array('jquery'), '1.0.1', 1);
