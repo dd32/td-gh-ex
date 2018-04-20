e7a10a7e71611c392a8a195740ebdb02f40fe680
Theme Directory <noreply@wordpress.org>
2018-04-20 20:20:57 +0000

    New version of Content - 1.1

--- functions.php
+++ functions.php
@@ -1,316 +1,40 @@
-<?php 
-if( ! isset( $content_width ) ) 
-    $content_width = 625;
-
-function content_setup() {
-    load_theme_textdomain( 'content', get_template_directory() . '/langs' );
-
-    add_editor_style();
-
-    add_theme_support('automatic-feed-links');
-
-    add_theme_support('custom-background', array('default-color' => 'fefefe',));
-
-    add_theme_support('title-tag');
-
-    add_theme_support('post-formats', 
-        array( 
-            'aside', 
-            'image', 
-            'link', 
-            'quote', 
-            'status'
-        )
-    );
-
-    register_nav_menu( 'menu', __( 'Main Menu', 'content' ) );
-
-    add_theme_support( 'post-thumbnails' );
-
-    set_post_thumbnail_size( 625, 9999 );
-}
-add_action( 'after_setup_theme', 'content_setup' );
-
-require(get_template_directory() . '/inc/custom-header.php');
-
-function content_scripts() {
-    wp_enqueue_style( 'content-style', get_stylesheet_uri() );
-
-    wp_enqueue_script( 'content-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
-
-    wp_enqueue_script( 'content-ie-html5', get_template_directory_uri() . '/js/html5.js', array(), '1.0', true );
-
-    wp_enqueue_script( 'content-it-css-media-queries', get_template_directory_uri() . '/js/css3-mediaqueries.js', array(), '1.0', true );
-
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'content_scripts' );
-
-function content_scripts_styles() {
-    global $wp_styles;
-    if ( 'off' !== _x( 'on', 'PT Sans  font: on or off', 'content' ) ) {
-        $subsets = 'latin,latin-ext';
-        $subset = _x( 'no-subset', 'PT Sans  font: add new subset (greek, cyrillic, vietnamese)', 'content' );
-        if ( 'cyrillic' == $subset ) 
-            $subsets .= ',cyrillic,cyrillic-ext';
-        elseif ( 'greek' == $subset ) 
-            $subsets .= ',greek,greek-ext';
-        elseif ( 'vietnamese' == $subset ) 
-            $subsets .= ',vietnamese';
-        $protocol = is_ssl() ? 'https' : 'http';
-        $query_args = array(
-            'family' => 'PT+Sans:400italic,700italic,400,700',
-            'subset' => $subsets,
-        );
-        wp_enqueue_style( 'content-fonts', add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" ), array(), null );
-    }
-    wp_enqueue_style( 'content-ie', get_template_directory_uri() . '/css/ie.css', array( 'content-style' ), '20130430' );
-    $wp_styles->add_data( 'content-ie', 'conditional', 'lt IE 9' );
-}
-add_action( 'wp_enqueue_scripts', 'content_scripts_styles' );
-
-function content_wp_title( $title, $sep ) {
-    global $paged, $page;
-    if( is_feed() ) 
-        return $title;
-    $title .= get_bloginfo( 'name' );
-    $site_description = get_bloginfo( 'description', 'display' );
-    if( $site_description && ( is_home() || is_front_page() ) ) 
-        $title = "$title $sep $site_description";
-    if( $paged >= 2 || $page >= 2 ) 
-        $title = "$title $sep " . sprintf( __( 'Page %s', 'content' ), max( $paged, $page ) );
-    return $title;
-}
-add_filter( 'wp_title', 'content_wp_title', 10, 2 );
-
-function content_page_menu_args( $args ) {
-    if( ! isset( $args['show_home'] ) ) 
-        $args['show_home'] = true;
-    return $args;
-}
-add_filter( 'wp_page_menu_args', 'content_page_menu_args' );
-
-function content_widgets_init() {
-    register_sidebar(
-        array(
-            'name' => __( 'Meta widget', 'content' ),
-            'id' => 'sidebar-1',
-            'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'Tag Cloud widget', 'content' ),
-            'id' => 'sidebar-2',
-            'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'Various widgets', 'content' ),
-            'id' => 'sidebar-3',
-            'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'Search Form Widget Area', 'content' ),
-            'id' => 'sidebar-4',
-            'description' => __( 'Search Form Widget Area', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'First Widget area in the footer', 'content' ),
-            'id' => 'sidebar-5',
-            'description' => __( 'First Widget area in the footer', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'Second Widget area in the footer', 'content' ),
-            'id' => 'sidebar-6',
-            'description' => __( 'Second Widget area in the footer', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name' => __( 'Third Widget area in the footer', 'content' ),
-            'id' => 'sidebar-7',
-            'description' => __( 'Third Widget area in the footer', 'content' ),
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-    );
-}
-add_action( 'widgets_init', 'content_widgets_init' );
-
-if( ! function_exists( 'content_content_nav' ) ) : 
-function content_content_nav($nav_id) {
-    global $wp_query;
-    $html_id = esc_attr($nav_id);
-    if( $wp_query->max_num_pages > 1 ) : ?>
-        <nav id="<?php echo $nav_id; ?>" class="navigation">
-            <h3 class="accessibility"><?php _e( 'Post navigation', 'content' ); ?></h3>
-            <ul>
-                <li class="prev-link alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'content' )); ?></li>
-                <li class="next-link alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'content' )); ?></li>
-            </ul>
-        </nav>
-    <?php endif;
-}
-endif;
-
-if( ! function_exists( 'content_comment' ) ) : 
-function content_comment( $comment, $args, $depth ) {
-    $GLOBALS['comment'] = $comment;
-    switch ($comment->comment_type) : 
-        case 'pingback' : 
-        case 'trackback' : ?>
-    <li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
-        <p><?php _e( 'Pingback:', 'content' ); comment_author_link(); edit_comment_link( __( 'Edit', 'content' ), '<span class="edit-link">', '</span>' ); ?></p>
-    <?php break;
-        default : 
-        global $post; ?>
-    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-        <article id="comment-<?php comment_ID(); ?>" class="comment">
-            <header class="comment-meta comment-author vcard">
-                <?php echo get_avatar( $comment, 44 );
-                    printf( '<cite class="fn">%1$s %2$s</cite>',
-                        get_comment_author_link(), (
-                        $comment->user_id === $post->post_author) ? '<span> ' . __( 'Post author', 'content' ) . '</span>' : ''
-                    );
-                    printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
-                        esc_url( get_comment_link( $comment->comment_ID ) ),
-                        get_comment_time( 'c' ),
-                        sprintf( __( '%1$s at %2$s', 'content' ), get_comment_date(), get_comment_time() )
-                    ); ?>
-            </header>
-            <?php if( '0' == $comment->comment_approved ) : ?>
-                <p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'content' ); ?></p>
-            <?php endif; ?>
-            <section class="comment-content comment">
-                <?php comment_text();
-                edit_comment_link( __( 'Edit', 'content' ), '<p class="edit-link">', '</p>' ); ?>
-            </section>
-            <div class="reply">
-                <?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'content' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-            </div>
-        </article>
-    <?php break;
-    endswitch;
-}
-endif;
-
-if( ! function_exists( 'content_entry_meta' ) ) : 
-function content_entry_meta() {
-    $categories_list = get_the_category_list( __( ', ', 'content' ) );
-    $tag_list = get_the_tag_list( '', __( ', ', 'content' ) );
-    $date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="post-date" datetime="%3$s">%4$s</time></a>',
-        esc_url( get_permalink() ),
-        esc_attr( get_the_time() ),
-        esc_attr( get_the_date( 'c' ) ),
-        esc_html( get_the_date() )
-    );
-    $author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-        esc_attr( sprintf( __( 'View all posts by %s', 'content' ), get_the_author() ) ),
-        get_the_author()
-    );
-    if( $tag_list ) {
-        $utility_text = __( '<p class="cats">Categories: %1$s</p><p class="tags">Tags: %2$s</p><p class="date">%3$s</p> <p class="by-author">Author %4$s</p>', 'content' );
-    } elseif( $categories_list ) {
-        $utility_text = __( '<p class="cats">Categories: %1$s</p><p class="tags">%3$s</p><p class="by-author">Author: %4$s</p>', 'content' );
-    } else {
-        $utility_text = __( '%3$s <p class="by-author">Author: %4$s</p>', 'content' );
-    }
-    printf(
-        $utility_text,
-        $categories_list,
-        $tag_list,
-        $date,
-        $author
-    );
-}
-endif;
-
-add_filter ( 'post_class' , 'content_alt_post_class' );
-global $current_class;
-$current_class = 'current';
-function content_alt_post_class( $alt_classes ) { 
-   global $current_class;
-   $alt_classes[] = $current_class;
-   $current_class = ( $current_class == 'current' ) ? 'clearing' : 'current';
-   return $alt_classes;
+<?php
+add_action('wp_enqueue_scripts', 'content_theme_css', 999);
+function content_theme_css() {
+    wp_enqueue_style('content-parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+	wp_enqueue_style('media-responsive-css', get_stylesheet_directory_uri()."/css/media-responsive.css" );
+    wp_enqueue_script('content-mp-masonry-js', get_stylesheet_directory_uri() . '/js/masonry/mp.mansory.js');
 }
 
-function content_body_class( $classes ) {
-    $background_color = get_background_color();
-    if( ! is_page_template( 'page-templates/full-width.php' ) ) {
-        foreach( $classes as $key => $value ) {
-            if( $value == 'full-width' ) unset( $classes[$key] );
-        }
-    }
-    if( is_page_template( 'page-templates/front-page.php' ) ) {
-        $classes[] = 'template-front-page';
-        if( has_post_thumbnail() )
-            $classes[] = 'has-post-thumbnail';
-    }
-    if( empty( $background_color ) )
-        $classes[] = 'custom-background-empty';
-    elseif( in_array( $background_color, array( 'fff', 'a73511' ) ) )
-        $classes[] = 'custom-background-white';
-    if( wp_style_is( 'content-fonts', 'queue' ) )
-        $classes[] = 'custom-font-enabled';
-    if( ! is_multi_author() )
-        $classes[] = 'single-author';
-    return $classes;
-}
-add_filter( 'body_class', 'content_body_class' );
+//Load text domain for translation-ready
+load_theme_textdomain('content', get_stylesheet_directory() . '/languages');
 
-function content_content_width() {
-    if( is_page_template( 'page-templates/full-width.php' ) || is_attachment() || ! is_active_sidebar( 'sidebar-1' ) ) {
-        global $content_width;
-        $content_width = 960;
-    }
-}
-add_action( 'template_redirect', 'content_content_width' );
+// footer custom script
+function content_footer_custom_script() {
+?>
+    <script>
+        jQuery(document).ready(function (jQuery) {
+            jQuery("#blog-masonry").mpmansory(
+                    {
+                        childrenClass: 'item', // default is a div
+                        columnClasses: 'padding', //add classes to items
+                        breakpoints: {
+                            lg: 4, //Change masonry column here like 2, 3, 4 column
+                            md: 6,
+                            sm: 6,
+                            xs: 12
+                        },
+                        distributeBy: {order: false, height: false, attr: 'data-order', attrOrder: 'asc'}, //default distribute by order, options => order: true/false, height: true/false, attr => 'data-order', attrOrder=> 'asc'/'desc'
+                        onload: function (items) {
+                            //make somthing with items
+                        }
+                    }
+            );
+        });
+    </script>
+    <?php
 
-function content_customize_register( $wp_customize ) {
-    $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
-    $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
 }
-add_action( 'customize_register', 'content_customize_register' );
-
-function content_customize_preview_js() {
-    wp_enqueue_script( 'content-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20120827', TRUE );
-}
-add_action( 'customize_preview_init', 'content_customize_preview_js' );
+add_action('wp_footer', 'content_footer_custom_script');
+?>
--- index.php
+++ index.php
@@ -1,32 +1,69 @@
-<?php get_header(); ?>
-    <section id="primary">
-        <div class="content">
-            <?php if( have_posts() ) : 
-                while ( have_posts() ) : the_post();
-                    get_template_part( 'content', get_post_format() );
-                endwhile;
-                content_content_nav( 'nav-below' );
-                else : ?>
-                    <article id="post-0" class="post no-results not-found">
-                        <?php if( current_user_can( 'edit_posts' ) ) : ?>
-                            <header class="post-header">
-                                <h1 class="post-title"><?php _e( 'No posts to display', 'content' ); ?></h1>
-                            </header>
-                            <div class="post-content">
-                                <p><?php printf( __( '<a href="%s" title="publish your first article">Publish your first article</a>.', 'content' ), admin_url( 'post-new.php' ) ); ?></p>
-                            </div>
-                        <?php else : ?>
-                            <header class="post-header">
-                                <h1 class="post-title"><?php _e( 'Nothing Found', 'content' ); ?></h1>
-                            </header>
-                            <div class="post-content">
-                                <p><?php _e( 'No contents are found, try to do another research using a different term.', 'content' ); ?></p>
-                                <?php get_search_form(); ?>
-                            </div>
-                        <?php endif; ?>
-                    </article>
-            <?php endif; ?>
-        </div>
-    </section>
-<?php get_sidebar();
-get_footer(); ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ *
+ * @package WordPress
+ * @subpackage Spicepress
+ */
+ 
+get_header();
+?>
+ <!-- Page Title Section -->
+		<section class="page-title-section">		
+			<div class="overlay">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+						   <?php 	$our_title = get_the_title( get_option('page_for_posts', true) );
+									echo '<div class="page-title wow bounceInLeft animated" ata-wow-delay="0.4s"><h1>'.$our_title.'</h1></div>';
+						   ?>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<?php
+								echo '<ul class="page-breadcrumb wow bounceInRight animated" ata-wow-delay="0.4s">';
+									$homeLink = home_url();
+									$our_title = get_the_title( get_option('page_for_posts', true) );
+								    echo '<li><a href="'.$homeLink.'">'.$our_title .'</a></li>';
+									echo '<li class="active"><a href="'.$our_title .'">'.get_bloginfo( 'name' ).'</a></li>';
+								 echo '</ul>'
+							?>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</section>
+		<div class="page-seperate"></div>
+		<!-- /Page Title Section -->
+ 
+ 
+		<!-- Blog & Sidebar Section -->
+		<section class="blog-section">
+			<div class="container">
+				<div class="row" id="blog-masonry">
+					<?php 
+					if ( have_posts() ) :
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
+						// Include the post format-specific template for the content. get_post_format
+						echo '<div class="item">';
+						get_template_part( 'content','');
+						echo '</div>';
+					endwhile;
+				endif;
+				?>
+				</div>
+				<?php
+				// Previous/next page navigation.
+					the_posts_pagination( array(
+						'prev_text'          => '<i class="fa fa-angle-double-left"></i>',
+						'next_text'          => '<i class="fa fa-angle-double-right"></i>'
+					) );
+				?>	
+			</div>
+		</section>
+<!-- /Blog & Sidebar Section -->
+<?php get_footer(); ?>
