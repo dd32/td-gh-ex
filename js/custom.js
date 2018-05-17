fdd61cc329814dd93c5d96826705162df4d731d9
Theme Directory <noreply@wordpress.org>
2018-05-17 10:55:35 +0000

    New theme: Beautify - 1.0.1

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+(function($){
+
+	$(function(){
+		$('.flexslider').flexslider(); 
+		$('.flex-carousel').flexslider({
+			animation: "slide",
+			animationLoop: true,
+			controlNav: false,
+			itemWidth: 250,
+			itemMargin: 5
+		});   
+	});
+
+})(jQuery);
