02a309dcf5a8d0bbc8a6eb9afd3ce5bbcd8d93a4
Theme Directory <noreply@wordpress.org>
2018-05-29 14:43:23 +0000

    New version of SuevaFree - 3.5.4

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -32,7 +32,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 ?>
 
-<div class="row">
+<div class="row checkout-wrapper">
 
 	<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
