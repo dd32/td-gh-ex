71ecadac319cb0c40ea4ddf7db31170d3cd359f6
Theme Directory <noreply@wordpress.org>
2018-07-13 06:55:43 +0000

    New version of Benzer - 1.0.5

--- templates/page-full-width.php
+++ templates/page-full-width.php
@@ -2,8 +2,8 @@
 /**
 Template Name: Page Fullwidth
 **/
-get_header(); 
-?>
+get_header();
+get_template_part('sections/specia','breadcrumb'); ?>
 
 <section class="page-wrapper">
 	<div class="container">
