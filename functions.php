b483f497a78cacccf8e268061d2f5ec886dbc0da
Theme Directory <noreply@wordpress.org>
2018-07-24 12:58:46 +0000

    New version of Engrave (Lite) - 1.9.5

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Declare latest theme version
-$GLOBALS['thinkup_theme_version'] = '1.9.4';
+$GLOBALS['thinkup_theme_version'] = '1.9.5';
 
 // Setup content width 
 function thinkup_content_width() {
