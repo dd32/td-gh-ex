6caae0d1593abdb2f9999c770d5f2bbdbe78b5e2
Theme Directory <noreply@wordpress.org>
2018-08-06 16:43:52 +0000

    Nuova versione di SuevaFree - 3.5.7

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -12,12 +12,12 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.4.4
  */
 
 defined( 'ABSPATH' ) || exit;
 
-if ( ! wc_coupons_enabled() || ! empty( WC()->cart->applied_coupons ) ) { // @codingStandardsIgnoreLine.
+if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 	return;
 }
 
@@ -31,7 +31,9 @@ if ( ! wc_coupons_enabled() || ! empty( WC()->cart->applied_coupons ) ) { // @co
        
     </div>
 
-	<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+    <form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+    
+        <p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'suevafree' ); ?></p>
     
         <p class="form-row form-row-first">
             <input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'suevafree' ); ?>" id="coupon_code" value="" />
