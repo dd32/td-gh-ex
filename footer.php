07948cecae03364fae4e32576705c2ba6cf25f1d
Theme Directory <noreply@wordpress.org>
2018-08-07 10:22:20 +0000

    New theme: Augusta - 1.0.4

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Augusta
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<?php augusta_footer_before(); ?>
+			<?php augusta_footer(); ?>
+			<?php augusta_footer_after(); ?>
+
+		</div><!-- #page -->
+
+		<?php augusta_body_bottom(); ?>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
