e9934a1673548cee63710168d63fa7e55d473c8d
Theme Directory <noreply@wordpress.org>
2018-08-14 21:04:10 +0000

    New version of SemPress - 1.5.10

--- footer.php
+++ footer.php
@@ -9,14 +9,28 @@
  */
 ?>
 
-  </div><!-- #main -->
+	</div><!-- #main -->
 
-  <footer id="colophon" role="contentinfo">
-    <div id="site-generator">
-      <?php do_action( 'sempress_credits' ); ?>
-      <?php printf( __( 'This site is powered by %1$s and styled with %2$s', 'sempress' ), '<a href="http://wordpress.org/" rel="generator">WordPress</a>', '<a href="http://notizblog.org/projects/sempress/">SemPress</a>'); ?>
-    </div>
-  </footer><!-- #colophon -->
+	<footer id="colophon" role="contentinfo">
+		<div id="site-publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+			<meta itemprop="name" content="<?php echo get_bloginfo( 'name', 'display' ); ?>" />
+			<meta itemprop="url" content="<?php echo home_url( '/' ); ?>" />
+			<?php
+			if ( has_custom_logo() ) {
+				$image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ) );
+			?>
+				<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+					<meta itemprop="url" content="<?php echo current( $image ); ?>" />
+					<meta itemprop="width" content="<?php echo next( $image ); ?>" />
+					<meta itemprop="height" content="<?php echo next( $image ); ?>" />
+				</div>
+			<?php } ?>
+		</div>
+		<div id="site-generator">
+			<?php do_action( 'sempress_credits' ); ?>
+			<?php printf( __( 'This site is powered by %1$s and styled with %2$s', 'sempress' ), '<a href="http://wordpress.org/" rel="generator">WordPress</a>', '<a href="http://notiz.blog/projects/sempress/">SemPress</a>' ); ?>
+		</div>
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
