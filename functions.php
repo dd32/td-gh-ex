70c9274ad61940946491a05e35a4bdd117e03788
Theme Directory <noreply@wordpress.org>
2018-09-02 15:32:54 +0000

    New version of Gump - 2.0.6

--- functions.php
+++ functions.php
@@ -239,72 +239,6 @@ function gump_register_required_plugins() {
 add_action( 'tgmpa_register', 'gump_register_required_plugins' );
 
 /**
- * Print the attached image with a link to the next attached image.
- *
- * @since gump 1.0
- */
-if ( ! function_exists( 'gump_the_attached_image' ) ) :
-function gump_the_attached_image() {
-	$post                = get_post();
-	/**
-	 *
-	 * @since gump 1.0
-	 *
-	 * @param array $dimensions {
-	 *     An array of height and width dimensions.
-	 *
-	 *     @type int $height Height of the image in pixels. Default 810.
-	 *     @type int $width  Width of the image in pixels. Default 810.
-	 * }
-	 */
-	$attachment_size     = apply_filters( 'gump_attachment_size', array( 810, 810 ) );
-	$next_attachment_url = wp_get_attachment_url();
-
-	/*
-	 * Grab the IDs of all the image attachments in a gallery so we can get the URL
-	 * of the next adjacent image in a gallery, or the first image (if we're
-	 * looking at the last image in a gallery), or, in a gallery of one, just the
-	 * link to that image file.
-	 */
-	$attachment_ids = get_posts( array(
-		'post_parent'    => $post->post_parent,
-		'fields'         => 'ids',
-		'numberposts'    => -1,
-		'post_status'    => 'inherit',
-		'post_type'      => 'attachment',
-		'post_mime_type' => 'image',
-		'order'          => 'ASC',
-		'orderby'        => 'menu_order ID',
-	) );
-
-	// If there is more than 1 attachment in a gallery...
-	if ( count( $attachment_ids ) > 1 ) {
-		foreach ( $attachment_ids as $attachment_id ) {
-			if ( $attachment_id == $post->ID ) {
-				$next_id = current( $attachment_ids );
-				break;
-			}
-		}
-
-		// get the URL of the next image attachment...
-		if ( $next_id ) {
-			$next_attachment_url = get_attachment_link( $next_id );
-		}
-
-		// or get the URL of the first image attachment.
-		else {
-			$next_attachment_url = get_attachment_link( array_shift( $attachment_ids ) );
-		}
-	}
-
-	printf( '<a href="%1$s" rel="attachment">%2$s</a>',
-		esc_url( $next_attachment_url ),
-		wp_get_attachment_image( $post->ID, $attachment_size )
-	);
-}
-endif;
-
-/**
  * Returns a "Read more" link for excerpts
  *
  * @since gump 1.0
@@ -313,17 +247,6 @@ function gump_excerpt_more( $more ) {
 	return '<a class="more-link" href="'. get_permalink( get_the_ID() ) . '">' . __( 'Read more ->', 'gump' ) . '</a>';
 }
 add_filter( 'excerpt_more', 'gump_excerpt_more' );
-
-
-/**
- * Remove Code from Header
- *
- * http://www.themelab.com/remove-code-wordpress-header/
- *
- * @since gump 1.0
- */
-remove_action('wp_head', 'wp_generator'); //Version of Wordpress
-remove_action('wp_head', 'adjacent_posts_rel_link'); //Next and Prev Posts
 
 /**
  * Add Google Fonts
