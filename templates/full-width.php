066e6fa0c5ded82efdbbd227991495e64c82faec
Theme Directory <noreply@wordpress.org>
2018-09-13 15:20:36 +0000

    New version of Apex - 1.28

--- templates/full-width.php
+++ templates/full-width.php
@@ -17,9 +17,9 @@ get_header(); ?>
 							<h1 class='post-title'><?php the_title(); ?></h1>
 						</div>
 						<div class="post-content">
-							<?php echo get_the_content(); ?>
+							<?php the_content(); ?>
 							<?php wp_link_pages( array(
-								'before' => '<p class="singular-pagination">' . __( 'Pages:', 'apex' ),
+								'before' => '<p class="singular-pagination">' . esc_html__( 'Pages:', 'apex' ),
 								'after'  => '</p>',
 							) ); ?>
 						</div>
