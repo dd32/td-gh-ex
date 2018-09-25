97aef25f914012d7dd63630951983f0e00026ac4
Theme Directory <noreply@wordpress.org>
2018-09-25 11:53:51 +0000

    New version of Ashe - 1.7.2

--- inc/about/about-ashe.php
+++ inc/about/about-ashe.php
@@ -387,6 +387,16 @@ function ashe_about_page_output() {
 					</tr>
 					<tr>
 						<td>
+							<h3>
+								<?php esc_html_e( 'Predefined Styles', 'ashe' ); ?>
+								<a href="<?php echo admin_url('themes.php?page=about-ashe#ashe-predefined-styles'); ?>" target="_blank"><span class="dashicons dashicons-external"></span></a>
+							</h3>							
+						</td>
+						<td class="compare-icon"><span class="dashicons-before dashicons-no"></span></td>
+						<td class="compare-icon"><span class="dashicons-before dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td>
 							<h3><?php esc_html_e( 'Unlimited Colors Options', 'ashe' ); ?></h3>
 						</td>
 						<td class="compare-icon"><span class="dashicons-before dashicons-no"></span></td>
@@ -646,12 +656,12 @@ function ashe_enqueue_about_page_scripts($hook) {
 	}
 
 	// enqueue CSS
-	wp_enqueue_style( 'ashe-about-page-css', get_theme_file_uri( '/inc/about/css/about-ashe-page.css' ), array(), '1.7.1' );
+	wp_enqueue_style( 'ashe-about-page-css', get_theme_file_uri( '/inc/about/css/about-ashe-page.css' ), array(), '1.7.2' );
 
 	// Demo Import
 	wp_enqueue_script( 'plugin-install' );
 	wp_enqueue_script( 'updates' );
-	wp_enqueue_script( 'ashe-about-page-css', get_theme_file_uri( '/inc/about/js/about-ashe-page.js' ), array(), '1.7.1' );
+	wp_enqueue_script( 'ashe-about-page-css', get_theme_file_uri( '/inc/about/js/about-ashe-page.js' ), array(), '1.7.2' );
 
 }
 add_action( 'admin_enqueue_scripts', 'ashe_enqueue_about_page_scripts' );
