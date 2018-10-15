a34eed6d89d04eedc5f4a8fb29e1970b866eafaa
Theme Directory <noreply@wordpress.org>
2018-10-15 16:19:51 +0000

    New version of Appointment Red - 1.1.8

--- functions.php
+++ functions.php
@@ -15,7 +15,29 @@ function appointment_red_theme_css() {
 	 */
 	function appointment_red_setup() {
    add_theme_support( 'title-tag' );
+   require( get_stylesheet_directory() . '/functions/customizer/customizer-copyright.php' );
+   load_theme_textdomain('appointment-red', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'appointment_red_setup' );
 
+
+function appointment_red_default_data(){
+	return array(
+	// general settings
+	'footer_copyright_text' => '<p>'.__( '<a href="https://wordpress.org">Proudly powered by WordPress</a> | Theme: <a href="https://webriti.com" rel="designer">Appointment Red</a> by Webriti', 'appointment-red' ).'</p>',
+	'footer_menu_bar_enabled' => '',
+	'footer_social_media_enabled' => '',
+	'footer_social_media_facebook_link' => '#',
+	'footer_facebook_media_enabled' => 1,
+	'footer_social_media_twitter_link' => '#',
+	'footer_twitter_media_enabled'=>1,
+	'footer_social_media_linkedin_link' => '#',
+	'footer_linkedin_media_enabled'=>1,
+	'footer_social_media_googleplus_link' => '#',
+	'footer_googleplus_media_enabled' => 1,
+	'footer_social_media_skype_link' => '#',
+	'footer_skype_media_enabled' => 1,
+	
+	);
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -7,6 +7,8 @@ A Business theme which is ideal for creating a corporate / business website. App
 For any ideas, support and feedback you can access the theme forum.
 
 == Version ==
+1.1.8
+1. Update string.
 1.1.7
 1. Update Author URI and Theme URI change http to https secure.
 1.1.6
