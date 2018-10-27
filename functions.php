72dbfaf5abd014fc65dc9321a573d146525ced05
Theme Directory <noreply@wordpress.org>
2018-10-27 13:59:46 +0000

    New version of discover - 1.7.31

--- functions.php
+++ functions.php
@@ -106,9 +106,9 @@ function discover_custom_header_setup() {
 		'width'                  => 220,
 		'height'                 => 75,
 		'flex-height'            => true,
-		'wp-head-callback'       => 'discover_header_style',
-		'admin-head-callback'    => 'discover_admin_header_style',
-		'admin-preview-callback' => 'discover_admin_header_image',
+		'wp-head-callback'       => '',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => '',
 	);
 
 	$args = apply_filters( 'discover_custom_header_args', $args );
@@ -116,97 +116,6 @@ function discover_custom_header_setup() {
 
 }
 add_action( 'after_setup_theme', 'discover_custom_header_setup' );
-
-if ( ! function_exists( 'discover_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see _s_custom_header_setup().
- */
-function discover_header_style() {
-
-	// If no custom options for text are set, let's bail
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value
-	if ( HEADER_TEXTCOLOR == get_header_textcolor() )
-		return;
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( 'blank' == get_header_textcolor() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute !important;
-			clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo get_header_textcolor(); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif; // _s_header_style
-
-if ( ! function_exists( 'discover_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * @see _s_custom_header_setup().
- */
-function discover_admin_header_style() {
-?>
-	<style type="text/css">
-	.appearance_page_custom-header #headimg {
-		border: none;
-	}
-	#headimg h1,
-	#desc {
-	}
-	#headimg h1 {
-	}
-	#headimg h1 a {
-	}
-	#desc {
-	}
-	#headimg img {
-	}
-	</style>
-<?php
-}
-endif; // _s_admin_header_style
-
-if ( ! function_exists( 'discover_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * @see discover_s_custom_header_setup().
- */
-function discover_admin_header_image() { ?>
-	<div id="headimg">
-		<?php
-		if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
-			$style = ' style="display:none;"';
-		else
-			$style = ' style="color:#' . get_header_textcolor() . ';"';
-		?>
-		<h1 class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div class="displaying-header-text" id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<img src="<?php echo esc_url( $header_image ); ?>" alt="" />
-		<?php endif; ?>
-	</div>
-<?php }
-endif; // discover_s_admin_header_image
 
 ?>
 <?php
