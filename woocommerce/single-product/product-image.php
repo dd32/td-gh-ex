5beeb604a5bedb083aae7ebafafffb39614381a6
Theme Directory <noreply@wordpress.org>
2018-11-01 15:54:34 +0000

    New version of Pinnacle - 1.6.7

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -4,7 +4,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.5.0
+ * @version     3.5.1
  */
 
 
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post, $woocommerce, $product, $pinnacle;
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 5 );
-$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+$post_thumbnail_id = $product->get_image_id();
 $full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, 'full' );
 $image_title      	= get_post_field( 'post_excerpt', $post_thumbnail_id );
 if(!empty($image_title)) {
@@ -20,10 +20,9 @@ if(!empty($image_title)) {
 } else {
 	$light_title  = get_the_title($post_thumbnail_id);
 }
-$placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
 $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
 	'woocommerce-product-gallery',
-	'woocommerce-product-gallery--' . $placeholder,
+	'woocommerce-product-gallery--' . ( $product->get_image_id() ? 'with-images' : 'without-images' ),
 	'woocommerce-product-gallery--columns-' . absint( $columns ),
 	'images',
 	'kad-light-gallery',
@@ -94,7 +93,7 @@ if(isset($pinnacle['product_simg_resize']) && $pinnacle['product_simg_resize'] =
 			$html .= '</div>';
 		}
 
-		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, get_post_thumbnail_id( $post->ID ) );
+		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id );
 		
 		if(! $galslider) {
 			echo '</div>';
