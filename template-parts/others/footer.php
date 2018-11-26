9c6dc2f92b5220244fcf981cff9b3e8262003e5a
Theme Directory <noreply@wordpress.org>
2018-11-26 06:06:34 +0000

    New version of Augusta - 1.0.7

--- template-parts/others/footer.php
+++ template-parts/others/footer.php
@@ -5,7 +5,7 @@
 				<div class="site-info">
 					<?php
 						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'augusta' ), 'augusta', '<a href="https://templatic.com/">Templatic</a>' );
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'augusta' ), 'augusta', '<a href="https://templatic.com/" rel="nofollow">Templatic</a>' );
 					?>
 				</div><!-- .site-info -->
 			</div>
