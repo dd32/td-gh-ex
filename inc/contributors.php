866bfca01368c649f2b587db959df59d3facf100
Theme Directory <noreply@wordpress.org>
2018-11-27 14:53:15 +0000

    New version of Origami - 1.7.6

--- inc/contributors.php
+++ inc/contributors.php
@@ -3,15 +3,22 @@ return json_decode( '{
 	"b7122312e4008f8f2c76911080bca01a": {
 		"name": "Andrew Misplon",
 		"email": "b7122312e4008f8f2c76911080bca01a",
-		"loc": 201,
-		"score": 15.713748112000154,
-		"percent": 81.70115921673028
+		"loc": 202,
+		"score": 8.267743191495589,
+		"percent": 80.9513348171728
 	},
 	"a885c7bd5442dd2acd8c3e42001332c6": {
 		"name": "Alex S",
 		"email": "a885c7bd5442dd2acd8c3e42001332c6",
-		"loc": 38,
-		"score": 3.519452815193527,
-		"percent": 18.298840783269718
+		"loc": 36,
+		"score": 1.1624936630129918,
+		"percent": 11.382237154415439
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 2,
+		"score": 0.7829896601228887,
+		"percent": 7.666428028411763
 	}
 }', true );
