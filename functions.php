69cd1dffaef5c28e6955d097787378f236916203
Theme Directory <noreply@wordpress.org>
2018-12-14 11:14:32 +0000

    New theme: Apelle uno - 1.0

---
+++ functions.php
@@ -0,0 +1,237 @@
+<?php 
+
+function add_theme_scripts() {
+ 
+
+  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bootstrap.min.css', array(), '4.1.3', 'all');
+ 
+}
+
+
+
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+function add_theme_scripts_footer() { 
+
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_script( 'popper-min', get_template_directory_uri() . '/popper.min.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'bootstrap-min', get_template_directory_uri() . '/bootstrap.min.js', array(), '1.0.0', true );
+	
+}
+add_action( 'wp_footer', 'add_theme_scripts_footer' );
+function register_menu_home() {
+  register_nav_menus(
+    array(
+      'header-home-menu' => __( 'Header Home Menu', 'apelleuno' ),
+      'central-home-menu' => __( 'Home Menu', 'apelleuno' ),
+	  'top-primary' => __( 'Primary Menu', 'apelleuno' ),
+     )
+   );
+ }
+ add_action( 'init', 'register_menu_home' );
+ 
+ function add_link_atts($atts,$item, $args ) {
+	 if( $args->theme_location == 'header-home-menu' ) {
+		 
+		  $atts['class'] = "nav-link";
+		 
+	 }
+	 if( $args->theme_location == 'central-home-menu' ) {
+		  $atts['class'] = "c-content-nav__link js-contentNavListItem";
+		 
+	 }
+	 if( $args->theme_location == 'top-primary' ) {
+		  $atts['class'] = "nav-link ";
+		 
+	 }
+	  return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_link_atts', 10, 3);
+
+// Add CSS class to children menu item on WordPress
+add_filter( 'wp_nav_menu_objects', 'add_menu_parent_class' );
+function add_menu_parent_class( $items ) {
+
+    foreach ( $items as $item ) {
+        if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+            $item->classes[] = 'dropdown-item';
+        }
+    }
+
+    return $items;
+}
+add_filter( 'nav_menu_link_attributes', 'add_class_to_items_link', 10, 3 );
+
+function add_class_to_items_link( $atts, $item, $args ) {
+  // check if the item has children
+  $hasChildren = (in_array('menu-item-has-children', $item->classes));
+  if ($hasChildren) {
+    // add the desired attributes:
+    $atts['class'] = 'nav-link dropdown-toggle';
+    $atts['data-toggle'] = 'dropdown';
+    $atts['data-target'] = '#';
+  }
+  return $atts;
+}
+class Apelle_Walker_Nav_Menu_primary extends Walker_Nav_Menu {
+	
+	function start_lvl(&$output, $depth) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<ul class=\"dropdown-menu \" >\n";
+  }
+   
+    public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+      
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li class="nav-item">';
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+ 	
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+      
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+      
+        $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . $title . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+       
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
+
+class Apelle_Walker_Nav_Menu extends Walker_Nav_Menu {
+	
+	function start_lvl(&$output, $depth) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<ul class=\"dropdown-menu bg-dark\" >\n";
+  }
+   
+    public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+      
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li class="nav-item">';
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+ 	
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+      
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+      
+        $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . $title . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+       
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
+
+function add_description_to_menu($item_output, $item, $depth, $args) {
+	 if( $args->theme_location == 'central-home-menu' ) {
+		if (strlen($item->description) > 0 ) {
+				  
+			$item_output = substr($item_output, 0, -strlen("</a>{$args->after}")) . sprintf('<span class="c-content-nav__subtext">%s</span >', esc_html($item->description)) . "</a>{$args->after}";
+		}
+	 }
+
+    return $item_output;
+}
+add_filter('walker_nav_menu_start_el', 'add_description_to_menu', 10, 4);
+
+
+add_action( 'after_setup_theme', 'wpse_theme_setup' );
+function wpse_theme_setup() {
+   
+    add_theme_support( 'title-tag' );
+}
+
+add_theme_support( 'custom-logo' );
+function apelleuno_logo_setup() {
+    $defaults = array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    );
+   add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'apelleuno_logo_setup' );
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+add_theme_support( 'automatic-feed-links' );
+add_action( 'widgets_init', 'apelleuno_widgets_init' );
+function apelleuno_widgets_init() {
+$args = array(
+	'name'          => __( 'Barra Laterale', 'apelleuno' ),
+	'id'            => 'apelleuno-sidebar-laterale',    
+	'description'   => __( 'i widgets in quest\'area verranno mostrati nella Barra Laterale in tutte le pagine.', 'apelleuno' ),
+        'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>' ); 
+register_sidebar( $args );
+}
+add_theme_support( 'post-thumbnails' ); 
+?>
---
+++ index.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="utf-8">
+<?php 
+	wp_enqueue_script("jquery");
+	wp_head(); ?>
+    <meta name="viewport" content="width=device-width">
+</head>
+
+<body <?php body_class(); ?>>
+
+
+<div class="page-wrapper page-wrapper page-wrapper">
+    <header id="main-header" class="l-header  l-header--concierge" role="banner" aria-hidden="false">
+		<div class="l-header__top-strip">
+			<div class="l-top-strip js-topStrip ">
+				<div class="l-top-strip__cta-list">	
+                <!-------- MENU TOP HOME --------------->
+         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#topHomeMenu" aria-controls="topHomeMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+                <?php 
+                    wp_nav_menu(
+                      array(
+                        'menu' => 'header-home-menu',
+                        'theme_location' => 'header-home-menu',
+                        'container' => 'div',
+                        'container_id' => 'topHomeMenu',
+                        'container_class' => 'collapse navbar-collapse',
+                        'menu_class'=>'nav navbar-nav',
+                        'link_before' => '',
+                        'link_after' => '',
+						'walker'=> new Apelle_Walker_Nav_Menu()
+                      )
+                    );
+                    
+                    
+                    ?>
+           
+</nav>  
+                <!-------- FINE MENU TOP HOME ---------->
+
+				</div>
+            </div>
+		</div>
+        
+        <div class="l-header__inner" style="margin-top:50px" >
+			<div class="l-header__top-banner">
+				<div class="l-top-banner" >
+					<div class="l-top-banner__inner">
+						
+                        <!-------- PRIMARY TOP MENU --------------->
+						<div class="l-top-banner__primary-nav">
+                        	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+						<?php 
+                            wp_nav_menu(
+                              array(
+                                'menu' => 'top-primary',
+                                'theme_location' => 'top-primary',
+                                'container' => 'div',
+                                'container_id' => 'topPrimaryMenu',
+                                'container_class' => 'collapse navbar-collapse',
+								'menu_class'=>'navbar-nav',
+                                'link_before' => '',
+                                'link_after' => '',
+								'walker'=> new Apelle_Walker_Nav_Menu_primary()
+                              )
+                            );
+                            
+                            
+                            ?>
+                            </nav>
+                        </div>
+                        <!-------- PRIMARY TOP MENU ---------->
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+	</header>
+	<?php 
+	if ( ! isset( $content_width ) ) $content_width = 900;
+	?>
+	<main class="container" id="main-content" role="main" style="max-width:<?php echo $content_width?>" data-view="responsive/AnimationHandler" aria-hidden="false">
+    <div class="l-top-banner__logo">
+
+                            <div class="c-logo " itemscope="" itemtype="http://schema.org/Organization">
+                                <a href="/" id="logo" itemprop="url">
+                                    <meta itemprop="name" content="Demo">
+                                    <?php 
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+			if ( has_custom_logo() ) {
+					echo '<img src="'. esc_url( $logo[0] ) .'" alt="Demo logo" class="c-logo__img" itemprop="logo">';
+			} else {
+					echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+			}
+			?>
+                                </a>
+                            </div>
+
+						</div>
+    <div class="row">
+        <div class="col-sm-12 <?php if ( is_active_sidebar( 'apelleuno-sidebar-laterale' ) ){ echo "col-md-8"; }?> ">
+        <?php	
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+			?><div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h1><?php 
+				the_title();
+			?></h1>
+            
+			<?php 
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail();
+			} 	
+				the_content();
+			?></div><?php
+			endwhile;
+			else :
+				_e( 'Sorry, no posts matched your criteria.', 'themeuno' );
+			endif;
+          ?>
+        </div>
+      <?php if ( is_active_sidebar( 'apelleuno-sidebar-laterale' ) ) : ?>
+       <div class=" col-sm-12 col-md-4">
+            <ul id="apelleuno-sidebar-laterale">
+                <?php dynamic_sidebar( 'apelleuno-sidebar-laterale' ); ?>
+            </ul>
+        </div>
+<?php endif; ?>
+      </div>
+    </main>
+    <div class="container">
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <!--<img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">-->
+            Apelle uno
+            <small class="d-block mb-3 text-muted">© 2018-2019</small>
+            <?php the_tags();?>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+			
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Cool stuff</a></li>
+              <li><a class="text-muted" href="#">Random feature</a></li>
+              <li><a class="text-muted" href="#">Team feature</a></li>
+              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another one</a></li>
+              <li><a class="text-muted" href="#">Last time</a></li>
+            </ul>
+            
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Resource</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Team</a></li>
+              <li><a class="text-muted" href="#">Locations</a></li>
+              <li><a class="text-muted" href="#">Privacy</a></li>
+              <li><a class="text-muted" href="#">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+        <div><?php the_posts_navigation() ?>
+            <?php wp_link_pages(); ?>
+        </div>
+        
+    </div>
+    
+</div>
+
+<style>
+#contenuto-centrale.l-takeover__content{     z-index: unset;}
+#contenuto-menu-sinistro.l-takeover__wrapper { position:absolute; z-index:99;}
+#mappa-di-sfondo.l-takeover__media:before{z-index:0;}
+#contenuto-menu-sinistro.l-takeover__wrapper {background-color: rgba(26,26,26,0.7);      }
+.modal-backdrop {
+    z-index: 10;}
+	
+
+</style>
+
+<!-- Large modal -->
+
+
+<div class="modal  fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+       	 	<h5 class="modal-title">Comemnti</h5>
+       	 	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          		<span aria-hidden="true">&times;</span>
+        	</button>
+        </div>
+        <div class="modal-body">
+        	<!--<form method="post">
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+            </form>-->
+        	<div class="comment list">
+	    <?php comments_template(); ?>
+    	<?php wp_list_comments( array( 'style' => 'div' ) ); ?>
+        <?php 
+		$comments_args = array(
+        // change the title of send button 
+        'label_submit'=>'Send',
+        // change the title of the reply section
+        'title_reply'=>'Write a Reply or Comment',
+        // remove "Text or HTML to be displayed after the set of comment fields"
+        'comment_notes_after' => '',
+        // redefine your own textarea (the comment body)
+        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'themeuno' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+);
+
+comment_form($comments_args);
+?>
+	</div>
+    <div class="comment list">
+    	<?php the_comments_navigation( ); ?>
+	</div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      
+    </div>
+  </div>
+</div>
+
+<!-- FINE Large modal -->
+<?php 
+
+    wp_footer();
+?>
+<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
+</body>
+</html>
