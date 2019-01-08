8d31250be10952fababdc7f230ef684dcb684d7d
Theme Directory <noreply@wordpress.org>
2019-01-08 19:31:59 +0000

    New version of Bazaar Lite - 1.5.6

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,3 +1,12 @@
+/*
+ * jQuery Bazaar Lite theme functions file
+ * https://www.themeinprogress.com
+ *
+ * Copyright 2019, ThemeinProgress
+ * Licensed under MIT license
+ * https://opensource.org/licenses/mit-license.php
+ */
+
 jQuery.noConflict()(function($){
 
 /* ===============================================
@@ -204,6 +213,7 @@ jQuery.noConflict()(function($){
 		theme:'pp_default',
 		show_title:false,
 		overlay_gallery: false,
+		deeplinking: false,
 		social_tools: false
 		
 	});
