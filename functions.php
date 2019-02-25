9bd334340d2076e6142bea0af7dcd6c607054146
Theme Directory <noreply@wordpress.org>
2019-02-25 15:56:17 +0000

    New version of Apex - 1.33

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 require_once( trailingslashit( get_template_directory() ) . 'theme-options.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/customizer.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/deprecated.php' );
+require_once( trailingslashit( get_template_directory() ) . 'inc/last-updated-meta-box.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/review.php' );
 require_once( trailingslashit( get_template_directory() ) . 'inc/scripts.php' );
 
@@ -781,3 +782,22 @@ function ct_apex_scroll_to_top_arrow() {
 	}
 }
 add_action( 'body_bottom', 'ct_apex_scroll_to_top_arrow');
+
+//----------------------------------------------------------------------------------
+// Output the "Last Updated" date on posts
+//----------------------------------------------------------------------------------
+function ct_apex_output_last_updated_date() {
+	
+	global $post;
+
+	if ( get_the_modified_date() != get_the_date() ) {
+		$updated_post = get_post_meta( $post->ID, 'ct_apex_last_updated', true );
+		$updated_customizer = get_theme_mod( 'last_updated' );
+		if ( 
+			( $updated_customizer == 'yes' && ($updated_post != 'no') )
+			|| $updated_post == 'yes' 
+			) {
+				echo '<p class="last-updated">'. esc_html__("Last updated on", "apex") . ' ' . get_the_modified_date() . ' </p>';
+			}
+	}
+}
