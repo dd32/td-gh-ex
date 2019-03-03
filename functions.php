1377725a2647076dbca57817777d86f7b9734c91
Theme Directory <noreply@wordpress.org>
2019-03-03 14:00:51 +0000

    New version of QuickPic - 1.5.7

--- functions.php
+++ functions.php
@@ -79,7 +79,18 @@ function quickpic_credits() {
 
 // load javascript
 	function quickpic_scripts() {
-	wp_enqueue_style( 'quickpic', get_stylesheet_uri() );
+
+		wp_enqueue_style( 'quickpic-normalize', 
+			get_template_directory_uri() . '/css/normalize.css', 
+			array(), 
+			'8.0.1'
+			);
+		
+			wp_enqueue_style( 'quickpic', 
+					 get_stylesheet_uri(),
+					 array(), 
+			         '1.5.7'
+					);
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ){
         wp_enqueue_script( 'comment-reply' );
