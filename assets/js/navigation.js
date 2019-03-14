9c76bd2a60ae0215e103e7018e2bcad90b0093b4
Theme Directory <noreply@wordpress.org>
2019-03-14 16:53:47 +0000

    New version of Storefront - 2.4.4

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,13 +5,13 @@
  *
  * Handles toggling the navigation menu for small screens.
  * Also adds a focus class to parent li's for accessibility.
- * Finally adds a class required to reveal the search in the handheld footer bar.
  */
 ( function() {
 
 	// Wait for DOM to be ready.
 	document.addEventListener( 'DOMContentLoaded', function() {
 		var container = document.getElementById( 'site-navigation' );
+
 		if ( ! container ) {
 			return;
 		}
@@ -22,7 +22,55 @@
 			return;
 		}
 
-		var menu = container.querySelector( 'ul' );
+		// Add dropdown toggle that displays child menu items.
+		var handheld = document.getElementsByClassName( 'handheld-navigation' );
+
+		if ( ! handheld ) {
+			return;
+		}
+
+		[].forEach.call( handheld[0].querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' ), function( anchor ) {
+
+			// Add dropdown toggle that displays child menu items
+			var btn = document.createElement( 'button' );
+			btn.setAttribute( 'aria-expanded', 'false' );
+			btn.setAttribute( 'aria-haspopup', 'true' );
+
+			btn.classList.add( 'dropdown-toggle' );
+
+			var btnSpan = document.createElement( 'span' );
+			btnSpan.classList.add( 'screen-reader-text' );
+			btnSpan.appendChild( document.createTextNode( storefrontScreenReaderText.expand ) );
+
+			btn.appendChild( btnSpan );
+
+			anchor.parentNode.insertBefore( btn, anchor.nextSibling );
+
+			// Set the active submenu dropdown toggle button initial state
+			if ( anchor.parentNode.classList.contains( 'current-menu-ancestor' ) ) {
+				btn.setAttribute( 'aria-expanded', 'true' );
+				btn.classList.add( 'toggled-on' );
+				btn.nextElementSibling.classList.add( 'toggled-on' );
+			}
+
+			// Add event listener
+			btn.addEventListener( 'click', function() {
+				btn.classList.toggle( 'toggled-on' );
+
+				// Remove text inside span
+				while ( btnSpan.firstChild ) {
+					btnSpan.removeChild( btnSpan.firstChild );
+				}
+
+				var expanded = btn.classList.contains( 'toggled-on' );
+
+				btn.setAttribute( 'aria-expanded', expanded );
+				btnSpan.appendChild( document.createTextNode( expanded ? storefrontScreenReaderText.collapse : storefrontScreenReaderText.expand ) );
+				btn.nextElementSibling.classList.toggle( 'toggled-on' );
+			} );
+		} );
+
+		var menu = handheld[0].querySelector( 'ul' );
 
 		// Hide menu toggle button if menu is empty and return early.
 		if ( ! menu ) {
@@ -31,92 +79,15 @@
 		}
 
 		button.setAttribute( 'aria-expanded', 'false' );
-		menu.setAttribute( 'aria-expanded', 'false' );
 		menu.classList.add( 'nav-menu' );
+		menu.style.visibility = 'hidden';
 
 		button.addEventListener( 'click', function() {
 			container.classList.toggle( 'toggled' );
 			var expanded = container.classList.contains( 'toggled' ) ? 'true' : 'false';
+			menu.style.visibility = 'true' === expanded ? 'visible' : 'hidden';
 			button.setAttribute( 'aria-expanded', expanded );
-			menu.setAttribute( 'aria-expanded', expanded );
 		} );
-
-		// Add dropdown toggle that displays child menu items.
-		var handheld = document.getElementsByClassName( 'handheld-navigation' );
-
-		if ( handheld.length > 0 ) {
-			[].forEach.call( handheld[0].querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' ), function( anchor ) {
-
-				// Add dropdown toggle that displays child menu items
-				var btn = document.createElement( 'button' );
-				btn.setAttribute( 'aria-expanded', 'false' );
-				btn.classList.add( 'dropdown-toggle' );
-
-				var btnSpan = document.createElement( 'span' );
-				btnSpan.classList.add( 'screen-reader-text' );
-				btnSpan.appendChild( document.createTextNode( storefrontScreenReaderText.expand ) );
-
-				btn.appendChild( btnSpan );
-
-				anchor.parentNode.insertBefore( btn, anchor.nextSibling );
-
-				// Set the active submenu dropdown toggle button initial state
-				if ( anchor.parentNode.classList.contains( 'current-menu-ancestor' ) ) {
-					btn.setAttribute( 'aria-expanded', 'true' );
-					btn.classList.add( 'toggled-on' );
-					btn.nextElementSibling.classList.add( 'toggled-on' );
-				}
-
-				// Add event listener
-				btn.addEventListener( 'click', function() {
-					btn.classList.toggle( 'toggled-on' );
-
-					// Remove text inside span
-					while ( btnSpan.firstChild ) {
-						btnSpan.removeChild( btnSpan.firstChild );
-					}
-
-					var expanded = btn.classList.contains( 'toggled-on' );
-
-					btn.setAttribute( 'aria-expanded', expanded );
-					btnSpan.appendChild( document.createTextNode( expanded ? storefrontScreenReaderText.collapse : storefrontScreenReaderText.expand ) );
-					btn.nextElementSibling.classList.toggle( 'toggled-on' );
-				} );
-			} );
-		}
-
-		// Add class to footer search when clicked.
-		[].forEach.call( document.querySelectorAll( '.storefront-handheld-footer-bar .search > a' ), function( anchor ) {
-			anchor.addEventListener( 'click', function( event ) {
-				anchor.parentElement.classList.toggle( 'active' );
-				event.preventDefault();
-			} );
-		} );
-
-		// Add focus class to body when an input field is focused.
-		// This is used to hide the Handheld Footer Bar when an input is focused.
-		var footer_bar = document.getElementsByClassName( 'storefront-handheld-footer-bar' );
-		var forms      = document.forms;
-		var isFocused  = function( focused ) {
-			return function() {
-				if ( !! focused ) {
-					document.body.classList.add( 'sf-input-focused' );
-				} else {
-					document.body.classList.remove( 'sf-input-focused' );
-				}
-			};
-		};
-
-		if ( footer_bar.length && forms.length ) {
-			for ( var i = 0; i < forms.length; i++ ) {
-				if ( footer_bar[0].contains( forms[ i ] ) ) {
-					continue;
-				}
-
-				forms[ i ].addEventListener( 'focus', isFocused( true ), true );
-				forms[ i ].addEventListener( 'blur', isFocused( false ), true );
-			}
-		}
 
 		// Add focus class to parents of sub-menu anchors.
 		[].forEach.call( document.querySelectorAll( '.site-header .menu-item > a, .site-header .page_item > a, .site-header-cart a' ), function( anchor ) {
