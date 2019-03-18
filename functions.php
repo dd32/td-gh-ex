17ba81b9d54f840438ae91136179944600a69a74
Theme Directory <noreply@wordpress.org>
2019-03-18 13:08:35 +0000

    New version of Ansia - 1.0.7

--- functions.php
+++ functions.php
@@ -200,7 +200,7 @@ function ansia_scripts() {
 		wp_enqueue_script( 'ansia-nanoScroll', get_template_directory_uri() . '/js/jquery.nanoscroller.min.js', array('jquery'), '0.8.7', true );
 	}
 	if ( ansia_options('_smooth_scroll', '1') == 1) {
-		wp_enqueue_script( 'ansia-smooth-scroll', get_template_directory_uri() . '/js/SmoothScroll.min.js', array('jquery'), '1.4.8', true );
+		wp_enqueue_script( 'ansia-smooth-scroll', get_template_directory_uri() . '/js/SmoothScroll.min.js', array('jquery'), '1.4.9', true );
 	}
 
 	wp_enqueue_script( 'ansia-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
