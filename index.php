b0acf8fe2b953afb90f0537a4d97f8ce763113b2
Theme Directory <noreply@wordpress.org>
2019-04-22 11:26:10 +0000

    New version of Apelle uno - 1.1.27

--- index.php
+++ index.php
@@ -74,8 +74,8 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
         <div class="modal-header">
-       	 	<h5 class="modal-title"><?php echo esc_html_e('Comment', 'apelle-uno' );?></h5>
-       	 	<button type="button" class="close" data-dismiss="modal" aria-label="<?php echo esc_attr_e( "Close" ,'apelle-uno' ); ?>">
+       	 	<h5 class="modal-title"><?php esc_html_e('Comment', 'apelle-uno' );?></h5>
+       	 	<button type="button" class="close" data-dismiss="modal" aria-label="<?php esc_attr_e( "Close" ,'apelle-uno' ); ?>">
           		<span aria-hidden="true">&times;</span>
         	</button>
         </div>
@@ -87,7 +87,7 @@
     
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal" ><?php echo esc_html_e('Close', 'apelle-uno' );?></button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" ><?php esc_html_e('Close', 'apelle-uno' );?></button>
         </div>
       
     </div>
