7393b61de9f32d3adceea19677e0f26f93f7feec
Theme Directory <noreply@wordpress.org>
2019-05-20 08:49:20 +0000

    New version of BC Business Consulting - 1.1.6

--- readme.txt
+++ readme.txt
@@ -2,8 +2,9 @@
 
 Contributors: athemeart
 Requires at least: 4.0
-Tested up to: 4.9.5
-Stable tag: 1.1.5
+Tested up to: 5.2.1
+Stable tag: 1.1.6
+Requires PHP: 7.0
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,28 @@
  * @package Business_Consulting
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+
 ?>
 
 <aside id="secondary" class="widget-area sidebar">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php 
+    if ( is_active_sidebar( 'sidebar-1' ) ) {
+        dynamic_sidebar( 'sidebar-1' );
+    }else{ ?>
+    <div id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</div>
+		<div id="archives" class="widget">
+			<h3 class="widget-title"><?php esc_html_e( 'Archives', 'shopstore' ); ?></h3>
+			<ul><?php wp_get_archives( array( 'type' => 'monthly' ) ); ?></ul>
+		</div>
+		<div id="meta" class="widget">
+			<h3 class="widget-title"><?php esc_html_e( 'Meta', 'shopstore' ); ?></h3>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</div>
+    <?php }?>
 </aside><!-- #secondary -->
