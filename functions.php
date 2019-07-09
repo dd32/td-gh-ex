dee0796d838ed67cac5d0578380f74ba867460df
Theme Directory <noreply@wordpress.org>
2019-07-09 04:47:23 +0000

    New version of AyaMedicine - 1.1.3

--- functions.php
+++ functions.php
@@ -21,6 +21,55 @@
  * {@link https://codex.wordpress.org/Plugin_API}
  */
 
+/**
+ * Set a constant that holds the theme's minimum supported PHP version.
+ */
+define( 'AYAMEDICINE_MIN_PHP_VERSION', '5.6' );
+
+/**
+ * Immediately after theme switch is fired we we want to check php version and
+ * revert to previously active theme if version is below our minimum.
+ */
+add_action( 'after_switch_theme', 'ayamedicine_test_for_min_php' );
+
+/**
+ * Switches back to the previous theme if the minimum PHP version is not met.
+ */
+function ayamedicine_test_for_min_php() {
+
+	// Compare versions.
+	if ( version_compare( PHP_VERSION, AYAMEDICINE_MIN_PHP_VERSION, '<' ) ) {
+		// Site doesn't meet themes min php requirements, add notice...
+		add_action( 'admin_notices', 'ayamedicine_min_php_not_met_notice' );
+		// ... and switch back to previous theme.
+		switch_theme( get_option( 'theme_switched' ) );
+		return false;
+
+	};
+}
+
+/**
+ * An error notice that can be displayed if the Minimum PHP version is not met.
+ */
+function ayamedicine_min_php_not_met_notice() {
+	?>
+	<div class="notice notice-error is_dismissable">
+		<p>
+			<?php esc_html_e( 'You need to update your PHP version to run this theme.', 'ayamedicine' ); ?> <br />
+			<?php
+			printf(
+				/* translators: 1 is the current PHP version string, 2 is the minmum supported php version string of the theme */
+				esc_html__( 'Actual version is: %1$s, required version is: %2$s.', 'ayamedicine' ),
+				PHP_VERSION,
+				AYAMEDICINE_MIN_PHP_VERSION
+			); // phpcs: XSS ok.
+			?>
+		</p>
+	</div>
+	<?php
+}
+
+
 if ( ! function_exists( 'ayamedicine_setup' ) ) :
 	/**
 	 * ayamedicine setup.
@@ -73,6 +122,11 @@ if ( ! function_exists( 'ayamedicine_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
+
+		// Add support for Block Styles.
+		add_theme_support( 'wp-block-styles' );
+
+		add_theme_support( 'editor-styles' );
 
 		/*
 		 * This theme styles the visual editor to resemble the theme style,
