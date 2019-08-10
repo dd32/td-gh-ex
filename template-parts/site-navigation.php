98bbe25ac460fd6018b1c3a8066e1ef205a9b584
Theme Directory <noreply@wordpress.org>
2019-08-10 08:56:46 +0000

    New theme: Anther - 1.0

---
+++ template-parts/site-navigation.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying site navigation
+ * @package Anther
+ */
+?>
+
+<nav id="site-navigation" class="main-navigation">
+	<div class="main-navigation-inside">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'anther' ); ?></a>
+		<div class="toggle-menu-wrapper">
+			<a href="#header-menu-responsive" title="<?php esc_attr_e( 'Menu', 'anther' ); ?>" class="toggle-menu-control">
+				<span class="toggle-menu-label"><?php esc_html_e( 'Menu', 'anther' ); ?></span>
+			</a>
+		</div>
+
+		<?php
+		// Header Menu
+		wp_nav_menu( apply_filters( 'anther_header_menu_args', array(
+			'container'       => 'div',
+			'container_class' => 'site-header-menu',
+			'theme_location'  => 'header-menu',
+			'menu_class'      => 'header-menu sf-menu',
+			'menu_id'         => 'menu-1',
+			'depth'           => 3,
+		) ) );
+		?>
+	</div><!-- .main-navigation-inside -->
+</nav><!-- .main-navigation -->
