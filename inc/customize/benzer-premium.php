a67e7269b931a789abe13ff72b0e35d92ce64db0
Theme Directory <noreply@wordpress.org>
2019-08-28 12:24:55 +0000

    New version of Benzer - 1.0.49

--- inc/customize/benzer-premium.php
+++ inc/customize/benzer-premium.php
@@ -38,6 +38,8 @@ function benzer_premium_setting( $wp_customize ) {
 					<li><a href="https://specia.ticksy.com/submit/#100013009" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/support.png"></a></li>
 					
 					<li><a href="https://wordpress.org/support/theme/benzer/reviews/#new-post" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/rating.png"></a></li>
+					
+					<li><a href="https://speciatheme.com/benzer-premium/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/upgrade-pro.jpg"></a></li>
 				</ul>
 			</div>
 		<?php
