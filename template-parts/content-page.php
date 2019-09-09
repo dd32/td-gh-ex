723a4e04229eb5b6df8b3429580f3144152cda4a
Theme Directory <noreply@wordpress.org>
2019-09-09 07:04:53 +0000

    New version of AyaPhotography - 1.1.7

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -34,7 +34,7 @@
 
 			<?php if ('open' == $post->comment_status) : ?>
 
-				<span class="comments-icon">
+				<span class="icon comments-icon">
 					<?php comments_popup_link(__( 'No Comments', 'ayaphotography' ), __( '1 Comment', 'ayaphotography' ), __( '% Comments', 'ayaphotography' ), '', __( 'Comments are closed.', 'ayaphotography' )); ?>
 				</span>
 
