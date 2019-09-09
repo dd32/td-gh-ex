cbb4c07207bda5fd73016b6deda7fad3fafcbeb5
Theme Directory <noreply@wordpress.org>
2019-09-09 06:28:17 +0000

    New version of AyaCoffeeShop - 1.1.5

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -34,7 +34,7 @@
 
 			<?php if ('open' == $post->comment_status) : ?>
 
-				<span class="comments-icon">
+				<span class="icon comments-icon">
 					<?php comments_popup_link(__( 'No Comments', 'ayacoffeeshop' ), __( '1 Comment', 'ayacoffeeshop' ), __( '% Comments', 'ayacoffeeshop' ), '', __( 'Comments are closed.', 'ayacoffeeshop' )); ?>
 				</span>
 
