e2ab006792bcdc0a1b86783f182a4e1e9a9231e8
Theme Directory <noreply@wordpress.org>
2019-09-09 07:06:17 +0000

    New version of AyaPsychology - 1.1.7

--- readme.txt
+++ readme.txt
@@ -46,6 +46,9 @@ Reference: https://codex.wordpress.org/Creating_a_Static_Front_Page
 
 == Changelog ==
 
+= 1.1.7 =
+* increase default text font size
+
 = 1.1.6 =
 * main menu: Add Keyboard navigation
 
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -34,7 +34,7 @@
 
 			<?php if ('open' == $post->comment_status) : ?>
 
-				<span class="comments-icon">
+				<span class="icon comments-icon">
 					<?php comments_popup_link(__( 'No Comments', 'ayapsychology' ), __( '1 Comment', 'ayapsychology' ), __( '% Comments', 'ayapsychology' ), '', __( 'Comments are closed.', 'ayapsychology' )); ?>
 				</span>
 
