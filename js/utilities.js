0315b80718fadc7fae4a8fdda50995c7f7603d86
Theme Directory <noreply@wordpress.org>
2019-11-11 10:52:01 +0000

    New version of AyaBooks - 1.1.9

--- js/utilities.js
+++ js/utilities.js
@@ -147,15 +147,14 @@
 			$('#navmain > div > ul li ul li:has("ul")').addClass('level-two-sub-menu');
 
     // add support of browsers which don't support focus-within
-    $('#navmain > div > ul > li a')
-      .focus(function() {
+    $('#navmain > div > ul > li > a:not(.login-form-icon):not(.search-form-icon), #navmain > div > ul > li > ul > li > a, #navmain > div > ul > li > ul > li > ul > li > a, .mega-menu-sub-menu')
+      .hover(function() {
         $(this).closest('li.level-one-sub-menu').addClass('menu-item-focused');
         $(this).closest('li.level-two-sub-menu').addClass('menu-item-focused');
 
         if (!$(this).parent().find('#cart-popup-content').length && $('#cart-popup-content').css('right') != '-99999px')
           $('#cart-popup-content').css('right', '-99999px');
-      })
-      .blur(function() {
+      }, function() {
         $(this).closest('li.level-one-sub-menu').removeClass('menu-item-focused');
         $(this).closest('li.level-two-sub-menu').removeClass('menu-item-focused');
     });										
