6206e1840d69fa7c1c055ea6e54129db5beae75e
Theme Directory <noreply@wordpress.org>
2019-11-11 16:42:25 +0000

    New version of ArileWP - 1.0.2

--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Author: ThemeArile
 Requires at least: WordPress 4.7
 Tested up to: WordPress 5.2.2
 Requires PHP: 5.6
-Stable tag: 1.0.1
-Version: 1.0.1
+Stable tag: 1.0.2
+Version: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets, post-formats, theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, e-commerce, portfolio
@@ -52,6 +52,9 @@ License: MIT (http://opensource.org/licenses/MIT)
 License: CC0 Public Domain
 
 == Changelog ==
+
+= Version 1.0.2
+* Update logo with text.
 
 = Version 1.0.1
 * Fixed menu issue.
--- template-parts/site-navbar.php
+++ template-parts/site-navbar.php
@@ -7,26 +7,27 @@ $arilewp_search_icon_disabled = get_theme_mod('arilewp_search_icon_disabled', tr
 	<!-- Theme Menubar -->
 	<nav class="navbar navbar-expand-lg not-sticky navbar-light <?php if($arilewp_menu_style == 'sticky'){echo 'header-sticky'; }?>">
 		<div class="<?php echo esc_attr($arilewp_menu_container_size); ?>">
-			<div class="row">
-				<div class="col-lg-3 align-self-center">	
-					<?php arilewp_header_logo(); ?>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-				</div>
-				<div class="col-lg-9">
+			<div class="row align-self-center">
+				
+					<div class="align-self-center">	
+						<?php arilewp_header_logo(); ?>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+					</div>
+					
 					<?php 
-								wp_nav_menu( array(
-									 'theme_location'  => 'primary',
-									 'container'       => 'div',
-									 'container_class' => 'collapse navbar-collapse',
-									 'container_id' => 'navbarNavDropdown',
-									 'menu_class'      => 'nav navbar-nav m-right-auto',
-									 'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
-									 'walker'          => new wp_bootstrap_navwalker()
-								) );
-						    ?>
-				</div>
+						wp_nav_menu( array(
+							 'theme_location'  => 'primary',
+							 'container'       => 'div',
+							 'container_class' => 'collapse navbar-collapse',
+							 'container_id' => 'navbarNavDropdown',
+							 'menu_class'      => 'nav navbar-nav m-right-auto',
+							 'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+							 'walker'          => new wp_bootstrap_navwalker()
+						) );
+					?>
+				
 			</div>
 		</div>
 	</nav>
