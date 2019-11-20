e973bbf19fdaaf9e16373b33850a4e126570dff2
Theme Directory <noreply@wordpress.org>
2019-11-20 08:48:26 +0000

    New version of Aerobics - 1.0.1

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ if ( ! function_exists( 'aerobics_fonts_url' ) ) :
 
         /*
          * Translators: If there are characters in your language that are not supported
-         * by Dosis, translate this to 'off'. Do not translate into your own language.
+         * by Merriweather Sans, translate this to 'off'. Do not translate into your own language.
          */
         if ( 'off' !== _x( 'on', 'Merriweather+Sans font: on or off', 'aerobics' ) ) {
             $font_families[] = 'Merriweather+Sans:300,300i,400,400i,700,700i,900,900i';
@@ -81,20 +81,20 @@ add_action( 'customize_register', 'aerobics_customize_register', 20 );
 add_action( 'wp_enqueue_scripts', 'aerobics_scripts', 20 );
 
 function aerobics_scripts() {
-    
+
     global $aerobics_version;
-    
+
     wp_enqueue_style( 'aerobics-google-font', aerobics_fonts_url(), array(), null );
-    
+
     wp_dequeue_style( 'swipewp-style' );
     wp_dequeue_style( 'swipewp-responsive-style' );
 
     wp_enqueue_style( 'lightslider-style', get_stylesheet_directory_uri() . '/assets/library/lightslider/css/lightslider.min.css', array(), '1.1.3' );
-    
+
 	wp_enqueue_style( 'swipewp-parent-style', get_template_directory_uri() . '/style.css', array(), esc_attr( $aerobics_version ) );
-    
+
     wp_enqueue_style( 'swipewp-parent-responsive', get_template_directory_uri() . '/assets/css/responsive.css', array(), esc_attr( $aerobics_version ) );
-    
+
     wp_enqueue_style( 'aerobics', get_stylesheet_uri(), array(), esc_attr( $aerobics_version ) );
 
     wp_enqueue_script( 'lightslider-scripts', get_stylesheet_directory_uri() . '/assets/library/lightslider/js/lightslider.min.js', array( 'jquery' ), '1.1.3' );
@@ -106,12 +106,12 @@ function aerobics_scripts() {
 
         $output_css .= ".navigation .nav-links a,.bttn,button,input[type='button'],input[type='reset'],input[type='submit'],.navigation .nav-links a:hover,.bttn:hover,button,input[type='button']:hover,input[type='reset']:hover,input[type='submit']:hover,.reply .comment-reply-link,.widget_search .search-submit, #masthead .search-form-wrapper .search-close, .side-menu-toggle-off,.woocommerce .price-cart:after,.woocommerce ul.products li.product .price-cart .button:hover, .woocommerce .widget_price_filter .ui-slider .ui-slider-range,.woocommerce .widget_price_filter .ui-slider .ui-slider-handle, .woocommerce .widget_price_filter .price_slider_wrapper .ui-widget-content, .woocommerce #respond input#submit,.woocommerce a.button,.woocommerce button.button,.woocommerce input.button,.woocommerce #respond input#submit.alt,.woocommerce a.button.alt,.woocommerce button.button.alt,.woocommerce input.button.alt,.added_to_cart.wc-forward, .woocommerce #respond input#submit:hover,.woocommerce a.button:hover,.woocommerce button.button:hover,.woocommerce input.button:hover,.woocommerce #respond input#submit.alt:hover,.woocommerce a.button.alt:hover,.woocommerce button.button.alt:hover,.woocommerce input.button.alt:hover, .woocommerce ul.products li.product .onsale, .woocommerce span.onsale, .woocommerce #respond input#submit.alt.disabled,.woocommerce #respond input#submit.alt.disabled:hover,.woocommerce #respond input#submit.alt:disabled,.woocommerce #respond input#submit.alt:disabled:hover,.woocommerce #respond input#submit.alt[disabled]:disabled,.woocommerce #respond input#submit.alt[disabled]:disabled:hover,.woocommerce a.button.alt.disabled,.woocommerce a.button.alt.disabled:hover,.woocommerce a.button.alt:disabled,.woocommerce a.button.alt:disabled:hover,.woocommerce a.button.alt[disabled]:disabled,.woocommerce a.button.alt[disabled]:disabled:hover,.woocommerce button.button.alt.disabled,.woocommerce button.button.alt.disabled:hover,.woocommerce button.button.alt:disabled,.woocommerce button.button.alt:disabled:hover,.woocommerce button.button.alt[disabled]:disabled,.woocommerce button.button.alt[disabled]:disabled:hover,.woocommerce input.button.alt.disabled,.woocommerce input.button.alt.disabled:hover,.woocommerce input.button.alt:disabled,.woocommerce input.button.alt:disabled:hover,.woocommerce input.button.alt[disabled]:disabled,.woocommerce input.button.alt[disabled]:disabled:hover, .woocommerce select .selected, .page .woocommerce-info, .woocommerce-noreviews, p.no-comments, .mt-scroll-to-top-wrapper, #section-cta .cta-button.cta-button1 a:hover{ background: ". esc_attr( $aerobics_theme_color ) ."}\n";
 
-        $output_css .= "#preloader-background, a, a:hover, a:focus, a:active, .comment-author .fn .url:hover,.top-header-wrapper .top-element.phone::before, .top-header-wrapper .top-element.email::before, .top-header-wrapper .top-element.address::before, .social-link a:hover, #masthead #primary-menu li a:hover, .banner-btn-wrap a:hover,#secondary .widget-title:before, #secondary .widget a:hover,.cat-links a:hover::before,.edit-link a:hover::before,.widget a:hover::before,.widget li:hover::before,.posted-on a:hover::before,.comments-link a:hover::before, .banner-btn-wrap a:hover, .post-more-btn:hover:after, .post-more-btn:hover,#section-team .post-position, #section-blog .post-title a:hover,#colophon .widget_archive a:hover, #colophon .widget_categories a:hover, #colophon .widget_recent_entries a:hover, #colophon .widget_meta a:hover, #colophon .widget_pages li a:hover, #colophon .widget_nav_menu li a:hover, #colophon .cat-links a:hover, #colophon .edit-link a:hover, #colophon .site-info a:hover, .trail-items .trail-item::before, .trail-items li a:hover,.navigation .nav-links a:hover,.entry-date.published:hover,.author a:hover, .entry-title a:hover,.search-results .search-result-detail .entry-meta a:hover, .woocommerce .woocommerce-message:before,.woocommerce ul.products li.product .price, .woocommerce div.product p.price, .woocommerce div.product span.price, .woocommerce .woocommerce-message:before,.woocommerce div.product p.price ins,.woocommerce div.product span.price ins,.woocommerce div.product p.price del,.woocommerce .woocommerce-info:before, .search-icon i:hover, .side-menu-toggle i:hover, #section-portfolio .post-more-btn:hover, #section-portfolio .post-more-btn:hover::after, #masthead .widget .mt-social-icons-wrapper a:hover, .widget_archive a:hover, .widget_categories a:hover, .widget_recent_entries a:hover, .widget_meta a:hover, .widget_recent_comments li:hover, .widget_rss li:hover, .widget_pages li a:hover, .widget_nav_menu li a:hover, .single .cat-links a:hover, .entry-footer a:hover, #section-service .sec-post-title a:hover, #section-testimonials .lSAction > a:hover{ color: ". esc_attr( $aerobics_theme_color ) ."}\n";
-        
+        $output_css .= "#preloader-background, a, a:hover, a:focus, a:active, .comment-author .fn .url:hover,.top-header-wrapper .top-element.phone::before, .top-header-wrapper .top-element.email::before, .top-header-wrapper .top-element.address::before, .social-link a:hover, #masthead #primary-menu li a:hover, .banner-btn-wrap a:hover,#secondary .widget-title:before, #secondary .widget a:hover,.cat-links a:hover::before,.edit-link a:hover::before,.widget a:hover::before,.widget li:hover::before,.posted-on a:hover::before,.comments-link a:hover::before, .banner-btn-wrap a:hover, .post-more-btn:hover:after, .post-more-btn:hover,#section-team .post-position, #section-blog .post-title a:hover,#colophon .widget_archive a:hover, #colophon .widget_categories a:hover, #colophon .widget_recent_entries a:hover, #colophon .widget_meta a:hover, #colophon .widget_pages li a:hover, #colophon .widget_nav_menu li a:hover, #colophon .cat-links a:hover, #colophon .edit-link a:hover, #colophon .site-info a:hover, .trail-items .trail-item::before, .trail-items li a:hover,.navigation .nav-links a:hover,.entry-date.published:hover,.author a:hover, .entry-title a:hover,.search-results .search-result-detail .entry-meta a:hover, .woocommerce .woocommerce-message:before,.woocommerce ul.products li.product .price, .woocommerce div.product p.price, .woocommerce div.product span.price, .woocommerce .woocommerce-message:before,.woocommerce div.product p.price ins,.woocommerce div.product span.price ins,.woocommerce div.product p.price del,.woocommerce .woocommerce-info:before, .search-icon i:hover, .side-menu-toggle i:hover, #section-portfolio .post-more-btn:hover, #section-portfolio .post-more-btn:hover::after, #masthead .widget .mt-social-icons-wrapper a:hover, .widget_archive a:hover, .widget_categories a:hover, .widget_recent_entries a:hover, .widget_meta a:hover, .widget_recent_comments li:hover, .widget_rss li:hover, .widget_pages li a:hover, .widget_nav_menu li a:hover, .single .cat-links a:hover, .entry-footer a:hover, #section-service .sec-post-title a:hover, #section-testimonials .lSAction > a:hover#masthead #primary-menu li:hover > a,#masthead #primary-menu li.current-menu-item > a,#masthead #primary-menu li.current_page_ancestor > a,#masthead #primary-menu li.current_page_item > a,#masthead #primary-menu li.current-menu-ancestor > a{ color: ". esc_attr( $aerobics_theme_color ) ."}\n";
+
         $output_css .= "#masthead.header-sticky, .widget_search .search-submit, .custom-header .custom-header-wrapper, .btn, button, input[type='button'], input[type='reset'], input[type='submit'], .comment-list .comment-body, .woocommerce .woocommerce-info, .woocommerce .woocommerce-message, .woocommerce form .form-row.woocommerce-validated .select2-container, .woocommerce form .form-row.woocommerce-validated input.input-text, .woocommerce form .form-row.woocommerce-validated select, #section-cta .cta-button.cta-button1 a:hover { border-color: ". esc_attr( $aerobics_theme_color ) ."}\n";
 
     $refine_output_css = swipewp_css_strip_whitespace( $output_css );
 
     wp_add_inline_style( 'aerobics', $refine_output_css );
 
-}   
+}
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Aerobics ===
+
+Contributors     :  Mystery Themes Team
+Requires at least: 4.5
+Tested up to     : 5.2
+Requires PHP     : 5.6
+Stable tag		 : 1.0.1
+License          : GNU General Public License v3 or later
+License URI      : http://www.gnu.org/licenses/gpl-3.0.html
+Tags:         blog, portfolio, e-commerce, left-sidebar, right-sidebar, custom-background, custom-logo, custom-menu, featured-images, theme-options, translation-ready
+
+Aerobics is a child theme of SwipeWp which is awesome free WordPress Business Theme comes up with plenty of powerful features.
+
+== Description ==
+
+Aerobics is a child theme of SwipeWp which is awesome free WordPress Business Theme comes up with plenty of powerful features that allow you to create a business website in any niche. It is integrated with WooCommerce and other popular plugins that offers full flexibility to use all its features. It has a promising design and exemplary features like Testimonials, Customization option, Optimized for SEO, Amazing support and documentation that makes no trouble while using the theme. Demo: http://demo.mysterythemes.com/swipe-demos/
+
+== Installation ==
+
+1. In your admin panel, go to Appearance > Themes and click the Add New button.
+2. Click Upload Theme and Choose File, then select the theme's .zip file. Click Install Now.
+3. Click Activate to use your new theme right away.
+
+
+== Screenshots ==
+    
+    https://pxhere.com/en/photo/1544165
+
+== Copyright ==
+
+SwipeWP is distributed under the terms of the GNU GPL V3.
+
+Aerobics WordPress Theme is child theme of SwipeWp, Copyright 2019 Mystery Themes
+Aerobics is distributed under the terms of the GNU GPL    
+
+== Changelog ==
+
+= 1.0.1 =
+    * Added version required information 
+    * Minor changes in Readme
+    * Added Tags in Readme
+    * Reduced unwanted spacing
+    * Primary menu hover design fixes done
+    
+= 1.0.0 =
+    * Initial release
+
+== Resources ==
+	* lightslider © 2016 Sachin N, MIT
