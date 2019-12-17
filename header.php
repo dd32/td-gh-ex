5a2e09bc331ba76b4478302370ebde42b6739022
Theme Directory <noreply@wordpress.org>
2019-12-17 08:33:14 +0000

    New version of Bands - 0.3

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1
 <div id="search">
 <?php get_search_form(); ?>
 </div>
-<button type="button" class="menu-toggle"><span class="menu-icon">&#9776;</span><span class="menu-text screen-reader-text"><?php esc_html_e( ' Menu', 'generic' ); ?></span></button>
+<button type="button" class="menu-toggle"><span class="menu-icon">&#9776;</span><span class="menu-text screen-reader-text"><?php esc_html_e( ' Menu', 'bands' ); ?></span></button>
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 </nav>
 </div>
