be0fd55c9cf73439cadf9905294d4346aaf5b544
Theme Directory <noreply@wordpress.org>
2020-02-01 11:40:00 +0000

    New version of LeftSide - 6.0

--- content-none.php
+++ content-none.php
@@ -4,12 +4,8 @@
  */
 ?>
 
+<h1 class="page-title"><?php _e( 'Nothing Found', 'leftside' ); ?></h1>
+<p><?php _e('Sorry, no posts matched your criteria.', 'leftside'); ?></p>
 <?php if ( is_search() || is_404() ) : ?>
-	<h1 class="page-title"><?php _e( 'Nothing Found', 'leftside' ); ?></h1>
-	<p><?php _e('Sorry, no posts matched your criteria.', 'leftside'); ?></p>
 	<?php get_search_form(); ?>
-<?php else : ?>
-	<h1 class="page-title"><?php _e( 'Nothing Found', 'leftside' ); ?></h1>
-	<p><?php _e('Sorry, no posts matched your criteria.', 'leftside'); ?></p>
 <?php endif; ?>
-
