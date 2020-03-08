21329b8889c63c5a6171263a7275416d8fb68940
Theme Directory <noreply@wordpress.org>
2020-03-08 16:02:33 +0000

    New version of Semper Fi Lite - 101

--- inc/store-front/html.php
+++ inc/store-front/html.php
@@ -15,7 +15,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_1' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_1' , __( 'HTML5 Standards that Define' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_1' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_1' , __( 'HTML5' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -43,7 +43,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_2' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_2' , __( 'CSS3 Latest Evolutions' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_2' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_2' , __( 'CSS3' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -71,7 +71,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_3' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_3' , __( 'JavaScript Free, Means Conflict Free' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_3' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_3' , __( 'No JavaScript' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -80,7 +80,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_3' , get_template_directory_uri() . '/inc/store-front/images/Clinton-Skydive-Sunset-Flocking-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_3' , __( "JavaScript (JS) is a lightweight, interpreted, programming language with first-class functions. However, JavaScript is never used in this theme to avoid all possible conflicts with Plugins, WordPress Updates, Custom Coding so that SemperFi just works." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_3' , __( "JavaScript (JS) is a lightweight, interpreted, programming language with first-class functions. When setting up a Wordpress a theme can run into JavaScript conflicts which would cause issues. Although a WordPress.org theme could avoid all possible conflicts with Plugins, WordPress Updates, and Custom Coding if JavaScript is never used." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -99,7 +99,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_4' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_4' , __( 'A One Off WordPress Customizer Options Generator' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_4' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_4' , __( 'WordPress Customizer' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -108,7 +108,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_4' , get_template_directory_uri() . '/inc/store-front/images/Head-Down-Freeflying-Casey-Heather-Paul-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_4' , __( "The Theme Customization API, added in WordPress 3.4, allows developers to customize and add controls to the 'Appearance' → 'Customize' admin screen. SemperFi has one of kind code to automatically generate WordPress Customizer Controls for the rapid deployment of custom customizer features. Meaning, SemperFi has the most powerful theme customizers ever thought up." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_4' , __( "The Theme Customization API, added in WordPress 3.4, allows developers to customize and add controls to the 'Appearance' → 'Customize' admin screen. A theme with automatically generated WordPress Customizer Controls would allow the rapid deployment of custom customizer features. Meaning, it would have one of the most powerful theme customizers ever thought up." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -127,7 +127,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_5' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_5' , __( 'Built with Grid, Flex, and Font Scaling' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_5' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_5' , __( 'Grid, Flex, and Font Scaling' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -136,7 +136,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_5' , get_template_directory_uri() . '/inc/store-front/images/Tandem-Skydive-Cody-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_5' , __( "The Flexible Box Module, usually referred to as flexbox, was designed as a one-dimensional layout model, and as a method that could offer space distribution between items in an interface and powerful alignment capabilities. The grid CSS property is a shorthand property that sets all of the explicit grid properties (grid-template-rows, grid-template-columns, and grid-template-areas), and all the implicit grid properties (grid-auto-rows, grid-auto-columns, and grid-auto-flow), in a single declaration. A web design that has true Font Scaling can display a perfect layout and perfectly legible fonts on every modern cell phone, tablet, computer, 8k TV monitor. The size of the screen does not matter." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_5' , __( "The Flexible Box Module, usually referred to as flexbox, was designed as a one-dimensional layout model, and as a method that could offer space distribution between items in an interface and powerful alignment capabilities. The grid CSS property is a shorthand property that sets all of the explicit grid properties (grid-template-rows, grid-template-columns, and grid-template-areas), and all the implicit grid properties (grid-auto-rows, grid-auto-columns, and grid-auto-flow), in a single declaration. A theme that has true Font Scaling can display a perfect layout and perfectly legible fonts on every modern cell phone, tablet, computer, 8k TV monitor. The size of the screen's resolution would not matter." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -164,7 +164,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_6' , get_template_directory_uri() . '/inc/store-front/images/Klash-Tandem-Hanycam-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_6' , __( "Historically, most web designers and their clients have approached the desktop side of any project first, while leaving the mobile part as a secondary goal that gets accomplished later. There are over 1.2 billion mobile web users worldwide. In the U.S., 25% of Web users are mobile-only. Semper Fi is designed as a mobile website theme first and desktop second to ensure the best compatibility no matter what the size of the screen the website is viewed on." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_6' , __( "Historically, most web designers and their clients have approached the desktop side of any project first, while leaving the mobile part as a secondary goal that gets accomplished later. There are over 1.2 billion mobile web users worldwide. In the U.S., 25% of Web users are mobile-only. A theme that is designed as a mobile website first and desktop second would ensure the best compatibility no matter what the size of the screen the website is viewed on." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -183,7 +183,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_7' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_7' , __( 'Woocommerce Shopping Cart Integration' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_7' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_7' , __( 'Woocommerce Shopping Cart' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -192,7 +192,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_7' , get_template_directory_uri() . '/inc/store-front/images/Head-Down-Steve-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_7' , __( "In April 2017, a total of 1,986,324 in websites use WooCommerce, or 23,724 of the world’s top million websites. That’s 0.5% of all websites or 2.8% of the top million websites. SemperFi comes with WooCommerce code built in to enable a clean and simple shopping cart experience customized to match the theme. Making it easy to set up a shop quickly and focus on the products and not the website." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_7' , __( "In April 2017, a total of 1,986,324 websites used WooCommerce, also 23,724 of the world’s top million websites used WooComerce. That’s 0.5% of all websites or 2.8% of the top million websites. A WordPress theme customized with WooCommerce code has a clean and simple shopping cart experience ready to go. Making it easy to set up an e-commerce shop quickly and focus on the products instead of the website design." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -211,7 +211,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_8' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_8' , __( 'Built in Google Structured MicroData' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_8' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_8' , __( 'Google Structured MicroData' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -220,7 +220,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_8' , get_template_directory_uri() . '/inc/store-front/images/Undy-Hundy-Jon-Klash-Clint-Hunter-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_8' , __( "Google Search works hard to understand the content of a page. Structured data is a standardized format for providing information about a page and classifying the page content; for example, on a recipe page, what are the ingredients, the cooking time and temperature, the calories, and so on. SemperFi is equipped with Structured Microdata to ensure the best search results. Structured data isn't something an SEO plugin can provide only a theme Author can code." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_8' , __( "Google Search works hard to understand the content of a website. Structured data is a standardized format for providing information to a search engine to help with classifying the page content; for example, on a recipe page, what are the ingredients, the cooking time and temperature, the calories, and so on. With Structured Microdata coded into a website, it helps ensures the best possible search results. Structured data isn't something an SEO plugin can provide, only a theme author can code microdata." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -239,7 +239,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_9' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_9' , __( 'A WordPress Theme by Schwarttzy' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_9' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_9' , __( 'Schwarttzy' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -248,7 +248,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_9' , get_template_directory_uri() . '/inc/store-front/images/Jenny-Grabing-Jennys-Butt-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_9' , __( "All of the themes on WordPress.org are created, developed, and maintained free of charge by the author. As the author of this theme, Schwarttzy, I offer a premium plugin that includes additional features to enhance your experience. This premium plugin for Semper Fi Lite helps supports my investment of the many hours I've put into this theme. For incentive, the premium plugin for Semper Fi Lite includes additional features to customize the theme with. These options are not available in the free version. Additionally, I offer to make minor custom changes to the Semper Fi Lite code for further customization. However, I will always support the free versions when issues of bugs and errors in the code are found." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_9' , __( "All of the themes on WordPress.org are created, developed, and maintained free of charge by the author. Theme developers are allowed to upsell their theme but displaying “obtrusive” upselling isn't allowed. Only one link on the top level in the customizer is allowed. This must be done using the customizer API (no injecting via JavaScript). Further unobtrusive links are allowed in a separate section." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
@@ -267,7 +267,7 @@
 
                 <h5>
 
-                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_10' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_10' , __( 'Get Semper Fi Plus!' , 'semper-fi-lite' ) ) ); ?></span></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'store_front_url_10' , get_admin_url() . 'customize.php' ) ); ?>"><span><?php echo esc_html( get_theme_mod( 'store_front_title_10' , __( 'Semper Fi Plus!' , 'semper-fi-lite' ) ) ); ?></span></a>
 
                 </h5>
 
@@ -276,7 +276,7 @@
                     <img width="500" height="500" src="<?php echo esc_url( get_theme_mod( 'store_front_img_10' , get_template_directory_uri() . '/inc/store-front/images/Moab-Schwarttzy-H3-Hummer-Top-of-the-World-Over-Hang-Off-Road-4x4-500x500.jpg' ) ); ?>" />            
                 </a>
 
-                <p><?php echo esc_html( get_theme_mod( 'store_front_text_10' , __( "Get the Premium Plugin to go from Semper Fi Lite, to Semper Fi Plus! Just visit the author's website and help support the theme. The Plugin Semper Fi plus adds in additional features to those that choose to support the theme using the themes custom action hooks. When one supporter receives customization for the theme, everyone gets the customization with a simple update." , 'semper-fi-lite' ) ) ); ?></p>
+                <p><?php echo esc_html( get_theme_mod( 'store_front_text_10' , __( "No options or panels/sections can be locked behind a paywall. All settings in a theme must work. Although a WordPress theme author could develop a plugin that integrates with the free theme using built-in hooks to add in additional options." , 'semper-fi-lite' ) ) ); ?></p>
 
                 <div class="price">
 
--- readme.txt
+++ readme.txt
@@ -8,8 +8,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Semper Fi Lite Wordpress Theme written for my personal use and freely shared with everyone else.
 
+
 == Description ==
-Semper Fi Lite is a handwritten WordPress theme for blogs, businesses, and can also be used as a powerful ecommerce (Woocommerce) solution. Written without JavaScript, just pure HTML5 and CSS3, Semper Fi Lite is free of any possible scripting conflicts with every WordPress Pulgin out there. Fully responsive, with a built in mobile friendly touch screen menu, adaptive font sizing, Woo Commerce integration, page specific custom backgrounds, and 4k+ resolution ready. All of Semper Fi Lite's options are organized and controlled with WordPress's powerful Customizer for real time customization. For support with questions, help, or issues use http://schwarttzy.com/contact-me/ , or head over to https://wordpress.org/support/theme/semper-fi-lite/
+Semper Fi Lite is a handwritten WordPress theme for blogs, businesses, and can also be used as a powerful e-commerce (Woocommerce) solution. Written without JavaScript, just pure HTML5 and CSS3, Semper Fi Lite is free of any possible scripting conflicts with every WordPress Plugin out there. Fully responsive, with a built-in mobile friendly touch screen menu, adaptive font sizing, Woo Commerce integration, page-specific custom backgrounds, and 4k+ resolution ready. All of Semper Fi Lite's options are organized and controlled with WordPress's powerful Customizer for real-time customization. For support with questions, help, or issues use http://schwarttzy.com/contact-me/ or head over to https://wordpress.org/support/theme/semper-fi-lite/
+
 
 == Frequently Asked Questions ==
 
@@ -17,12 +19,29 @@ Semper Fi Lite is a handwritten WordPress theme for blogs, businesses, and can a
 
 Head to http://schwarttzy.com/contact-me/ send Schwarttzy an email directly, or head to https://wordpress.org/support/theme/semper-fi-lite/ to ask on the forums.
 
+
 == Changelog ==
+
+= 101 =
+* Removed theme promotions on semper-fi-lite\inc\store-front\html.php and semper-fi-lite\inc\store-front\customizer.php
+* Moved semper-fi-lite\inc\open-graph-protocol\ from the theme over to the premium plugin because a third party api key is plugin territory
+* fixed wrong use of unique prefix in semper-fi-lite\inc\woo-commerce\
+* fixed the wrong uses of esc_attr, esc_textarea, and missing esc_url
+* custom customizer sanitizer for range and select
+* adding in the missing text domain for translation in semper-fi-lite\inc\footer-widgets\html.php
+* fixed coding error in semper-fi-lite\inc\404\html.php and issues with escaping functions with esc_html() between HTML tags using only esc_attr() 
+* fixed hardcode the date formats, although few hidden hard coded dates remain to ensure the Microdata work properly, these coded dates are not seen but the viewer, only the search engines see these hard coded dates
+* fixed missing translations on semper-fi-lite\inc\comments\html.php and semper-fi-lite\inc\woo-commerce\customizer.php
+* updated Read Me to use WordPress.org match recommend 
 
 = 100 =
 * Theme released after two plus years with new code
 
+
 == Upgrade Notice ==
 
+= 101 =
+* Improvements to the theme based on WordPress.org Review Recommendations.
+
 = 100 =
-* Completely redesigned theme under the hood with many fixes, new features
+* Completely redesigned theme under the hood with many fixes, new features.
