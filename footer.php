b7708f2934c6698d710d6b6ee889e957b265c46b
Theme Directory <noreply@wordpress.org>
2020-06-27 12:03:16 +0000

    New version of Iconic One - 2.1.5

--- footer.php
+++ footer.php
@@ -9,8 +9,8 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-		<div class="footercopy"><?php echo get_theme_mod( 'textarea_copy', 'custom footer text left' ); ?></div>
-		<div class="footercredit"><?php echo get_theme_mod( 'custom_text_right', 'custom footer text right' ); ?></div>
+		<div class="footercopy"><?php echo wp_kses_post(get_theme_mod( 'textarea_copy', 'custom footer text left' )); ?></div>
+		<div class="footercredit"><?php echo wp_kses_post(get_theme_mod( 'custom_text_right', 'custom footer text right' )); ?></div>
 		<div class="clear"></div>
 		</div><!-- .site-info -->
 		</footer><!-- #colophon -->
