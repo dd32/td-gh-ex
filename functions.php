8821aaf91fdba996d251099b7c09a55b84a9497d
Theme Directory <noreply@wordpress.org>
2020-06-28 16:57:46 +0000

    New version of Minamaze - 1.7.5

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Declare latest theme version
-$GLOBALS['thinkup_theme_version'] = '1.7.4';
+$GLOBALS['thinkup_theme_version'] = '1.7.5';
 
 // Setup content width
 function thinkup_content_width() {
