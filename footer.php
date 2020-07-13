42fa429f4818e57f4e81c16aac8a204e57bc886c
Theme Directory <noreply@wordpress.org>
2020-07-13 06:23:39 +0000

    New version of Hardpressed - 1.2

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 
         <?php echo __('&copy; ', 'hardpressed') . esc_attr( get_bloginfo( 'name', 'display' ) );  ?>
         <?php if ( is_front_page() && ! is_paged() ) : ?>
-        <?php echo __(' - Built with','hardpressed'); ?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hardpressed' ) ); ?>" target="_blank"><?php printf( esc_html( '%s', 'hardpressed' ), 'WordPress' ); ?></a><span><?php esc_html_e(' and ','hardpressed'); ?></span><a href="<?php echo esc_url( __( 'https://wpdevshed.com/hardpressed-theme/', 'hardpressed' ) ); ?>" target="_blank"><?php printf( esc_html( '%s', 'hardpressed' ), 'Hardpressed' ); ?></a>
+        <?php echo __(' - Built with','hardpressed'); ?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hardpressed' ) ); ?>" rel="nofollow" target="_blank"><?php printf( esc_html( '%s', 'hardpressed' ), 'WordPress' ); ?></a><span><?php esc_html_e(' and ','hardpressed'); ?></span><a href="<?php echo esc_url( __( 'https://wpdevshed.com/hardpressed-theme/', 'hardpressed' ) ); ?>" rel="nofollow" target="_blank"><?php printf( esc_html( '%s', 'hardpressed' ), 'Hardpressed' ); ?></a>
         <?php endif; ?>
         <?php hardpressed_footer_nav(); ?>
         
--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" action="<?php echo esc_url(home_url( '/' )); ?>">
     <div>
     	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'hardpressed' ); ?></label>
         <input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Search...', 'hardpressed' ); ?>" />
