e2a9cb03df5e456148d49d7fe2e140cecff606f8
Theme Directory <noreply@wordpress.org>
2020-08-06 06:08:01 +0000

    New version of ArticlePress - 1.2.9

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 				<div class="row">
 					<div class="col-md-7">
 						<p class="site-copyright">
-							&copy; <?php esc_html( the_date( 'Y' )); ?> <?php esc_html( bloginfo( 'title' ) ) ;?> <?php esc_html_e('Designed by', 'articlepress') ?> <a href="<?php esc_url( __( 'https://themeasia.net', 'articlepress' ) ); ?>"><?php esc_html_e( 'ThemeAsia','articlepress' ) ?></a>
+							&copy; <?php esc_html( the_date( 'Y' )); ?> <?php esc_html( bloginfo( 'title' ) ) ;?> <?php esc_html_e('Designed by', 'articlepress') ?> <a href="<?php echo esc_url( 'https://themeasia.net' ); ?>"> <?php esc_html_e( 'ThemeAsia','articlepress' ) ?></a>
 						</p>
 					</div>
 
@@ -30,17 +30,17 @@
 							<ul>
 								<!-- Facebook -->
 								<?php if ( !empty( get_theme_mod('footer_socail_icon_facebook') ) ): ?>
-								<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_facebook') ); ?>"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_facebook') ); ?>" rel="nofollow"><i class="fab fa-facebook-f"></i></a></li>
 								<?php endif ?>
 
 								<!-- Twitter -->
 								<?php if ( get_theme_mod('footer_socail_icon_twitter') ): ?>
-									<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_twitter') ); ?>"><i class="fab fa-twitter"></i></a></li>
+									<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_twitter') ); ?>" rel="nofollow"><i class="fab fa-twitter"></i></a></li>
 								<?php endif ?>
 								
 								<!-- Pinterest -->
 								<?php if ( get_theme_mod('footer_socail_icon_pinterest') ): ?>
-									<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_pinterest') ); ?>"><i class="fab fa-pinterest-p"></i></a></li>
+									<li><a href="<?php echo esc_url( get_theme_mod('footer_socail_icon_pinterest') ); ?>" rel="nofollow"><i class="fab fa-pinterest-p"></i></a></li>
 								<?php endif ?>
 							</ul>
 						</div>
