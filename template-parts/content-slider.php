341eddac83c482f60cd07790de93f5303eeb9aea
Theme Directory <noreply@wordpress.org>
2020-10-09 18:18:16 +0000

    New version of Beetle - 1.6.5

--- template-parts/content-slider.php
+++ template-parts/content-slider.php
@@ -9,7 +9,7 @@
 
 <li id="slide-<?php the_ID(); ?>" class="zeeslide clearfix">
 
-	<?php beetle_slider_image( 'beetle-header-image', array( 'class' => 'slide-image' ) ); ?>
+	<?php beetle_slider_image( 'beetle-header-image', array( 'class' => 'slide-image', 'loading' => false ) ); ?>
 
 	<div class="slide-post clearfix">
 
