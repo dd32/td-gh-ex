f9570f6f5fc9d203f1e1afa2c191229311a48781
Theme Directory <noreply@wordpress.org>
2020-10-20 07:20:53 +0000

    New theme: AeonAccess - 1.0.1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/**
+ * File aeonaccess.
+ * @package   AeonAccess
+ * @author    AeonWP <info@aeonwp.com>
+ * @copyright Copyright (c) 2020, AeonWP
+ * @link      https://aeonwp.com/aeonaccess
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html
+*/
+(function($) {
+	"use strict";
+		//Check to see if the window is top if not then display button
+		jQuery(window).scroll(function($){
+			if (jQuery(this).scrollTop() > 100) {
+				jQuery('.go-to-top').addClass('gotop');
+				jQuery('.go-to-top').fadeIn();
+			} else {
+				jQuery('.go-to-top').fadeOut();
+			}
+		});
+		
+		//Click event to scroll to top
+		jQuery('.go-to-top').click(function($){
+			jQuery('html, body').animate({scrollTop : 0},800);
+			return false;
+		});
+	}
+)(jQuery);
