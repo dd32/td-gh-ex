7bfa77a0b4a9b177afb7ae16bfbfe262cdbc1413
Theme Directory <noreply@wordpress.org>
2020-10-27 12:05:42 +0000

    New version of Avril - 1.0.10

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -43,5 +43,9 @@ add_action( 'wp_enqueue_scripts', 'avril_scripts' );
 //Admin Enqueue for Admin
 function avril_admin_enqueue_scripts(){
 	wp_enqueue_style('avril-admin-style', get_template_directory_uri() . '/assets/css/admin.css');
+	wp_enqueue_script( 'avril-admin-script', get_template_directory_uri() . '/assets/js/avril-admin-script.js', array( 'jquery' ), '', true );
+    wp_localize_script( 'avril-admin-script', 'avril_ajax_object',
+        array( 'ajax_url' => admin_url( 'admin-ajax.php' ) )
+    );
 }
 add_action( 'admin_enqueue_scripts', 'avril_admin_enqueue_scripts' );
