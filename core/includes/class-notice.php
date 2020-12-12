5874f51ba6154c739ff9d4231f36c67e4554bf9e
Theme Directory <noreply@wordpress.org>
2020-12-12 20:26:24 +0000

    New version of Diarjo Lite - 1.2.5

--- core/includes/class-notice.php
+++ core/includes/class-notice.php
@@ -14,21 +14,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 if( !class_exists( 'diarjo_lite_admin_notice' ) ) {
 
 	class diarjo_lite_admin_notice {
-	
+
 		/**
 		 * Constructor
 		 */
-		 
+
 		public function __construct( $fields = array() ) {
 
-			if ( 
+			if (
 				!get_option( 'diarjo-lite-dismissed-notice') &&
 				version_compare( PHP_VERSION, DIARJO_LITE_MIN_PHP_VERSION, '>=' )
 			) {
 
 				add_action( 'admin_notices', array(&$this, 'admin_notice') );
 				add_action( 'admin_head', array( $this, 'dismiss' ) );
-			
+
 			}
 
 		}
@@ -36,57 +36,59 @@ if( !class_exists( 'diarjo_lite_admin_notice' ) ) {
 		/**
 		 * Dismiss notice.
 		 */
-		
+
 		public function dismiss() {
 
 			if ( isset( $_GET['diarjo-lite-dismiss'] ) && check_admin_referer( 'diarjo-lite-dismiss-action' ) ) {
-		
+
 				update_option( 'diarjo-lite-dismissed-notice', intval($_GET['diarjo-lite-dismiss']) );
 				remove_action( 'admin_notices', array(&$this, 'admin_notice') );
-				
-			} 
-		
+
+			}
+
 		}
 
 		/**
 		 * Admin notice.
 		 */
-		 
-		public function admin_notice() {
-			
-		?>
-			
-            <div class="notice notice-warning is-dismissible">
-            
-            	<p>
-            
-            		<strong>
 
+		public function admin_notice() {
+
+		?>
+
+            <div class="notice notice-warning is-dismissible">
+
+            	<p>
+
+            		<strong>
+                  <h2><?php esc_html_e('WordPress Bundle.','diarjo-lite');?></h2>
 						<?php
-                        
-                            esc_html_e( 'Download for free the premium version of Diarjo to enable an extensive option panel, 600+ Google Fonts, unlimited sidebars, portfolio and much more. ', 'diarjo-lite' );
-							
-							printf( 
-								'<a href="%1$s" class="dismiss-notice">' . esc_html__( 'Dismiss this notice', 'diarjo-lite' ) . '</a>', 
+
+            esc_html_e( 'The premium version of Diarjo with other 17 premium WordPress themes and 5 premium WordPress plugins, starting at only €1. ', 'diarjo-lite' );
+
+
+
+							printf(
+								'<a href="%1$s" class="dismiss-notice">' . esc_html__( 'Dismiss this notice', 'diarjo-lite' ) . '</a>',
 								esc_url( wp_nonce_url( add_query_arg( 'diarjo-lite-dismiss', '1' ), 'diarjo-lite-dismiss-action'))
 							);
-                            
+
                         ?>
-                    
+
                     </strong>
-                    
+
             	</p>
-                    
+
             	<p>
-            		
-					<a target="_blank" href="<?php echo esc_url( 'https://www.themeinprogress.com/diarjo-free-creative-minimal-wordpress-theme/?ref=2&campaign=diarjonotice' ); ?>" class="button button-primary"><?php _e( 'Download for free Diarjo Premium', 'diarjo-lite' ); ?></a>
-                
+
+                <a target="_blank" href="<?php echo esc_url( 'https://www.themeinprogress.com/wordpress-deal-bundle/?ref=2&campaign=diarjo-notice' ); ?>" class="button button-primary"><?php esc_html_e( 'Get the bundle', 'diarjo-lite' ); ?></a>
+
             	</p>
 
             </div>
-		
+
 		<?php
-		
+
 		}
 
 	}
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: alexvtn
 Tags: custom-background, custom-colors, custom-logo, custom-menu, featured-images, footer-widgets, post-formats, right-sidebar, sticky-post, theme-options, threaded-comments, translation-ready, one-column, two-columns, blog,  news, photography
 Requires at least: 4.0
-Tested up to: 5.3
-Stable tag: 1.2.4
+Tested up to: 5.6
+Stable tag: 1.2.5
 Requires PHP: 5.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -20,6 +20,11 @@ Diarjo Lite WordPress Theme, Copyright 2019 ThemeinProgress.
 Diarjo Lite is distributed under the terms of the GPLv2 or later
 
 == Changelog ==
+
+= 1.2.5 =
+Release date - December, 12 - 2019
+
+* Tested with WordPress 5.6
 
 = 1.2.4 =
 Release date - November, 26 - 2019
@@ -128,7 +133,7 @@ Google Web Fonts (Delius Swash Caps, Fjalla One, Roboto Slab) By Google - http:/
 Bootstrap By Twitter, Inc - http://getbootstrap.com
 * bootstrap.css v3.3.7 - Licensed under MIT license ( Applies to bootstrap.css file in /diarjo-lite/assets/css/ )
 
-prettyPhoto By Stephane Caron - http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/ 
+prettyPhoto By Stephane Caron - http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/
 * prettyPhoto.css v3.1.6 - Licensed under GPLv2 or Creative Commons 2.5 license ( Applies to prettyPhoto.css file in /diarjo-lite/assets/css/ )
 
 jQuery Easing By George McGinley Smith - http://gsgd.co.uk/sandbox/jquery/easing/
