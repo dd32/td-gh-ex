2af9588c4751465107f5707ea42b6ac970a8af9b
Theme Directory <noreply@wordpress.org>
2020-12-13 13:04:52 +0000

    New version of Advance Portfolio - 0.4.9

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ function advance_portfolio_setup() {
 	add_theme_support('post-thumbnails');
 	add_theme_support('woocommerce');
 	add_theme_support( 'align-wide' );
+	add_theme_support( 'wp-block-styles' );
 	add_theme_support('title-tag');
 	add_theme_support('custom-logo', array(
 			'height'      => 250,
@@ -162,6 +163,8 @@ function advance_portfolio_font_url() {
 
 function advance_portfolio_scripts() {
 	wp_enqueue_style('advance-portfolio-font', advance_portfolio_font_url(), array());
+	// blocks-css
+	wp_enqueue_style( 'block-style', get_theme_file_uri('/css/blocks.css') );
 	wp_enqueue_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css');
 	wp_enqueue_style('advance-portfolio-basic-style', get_stylesheet_uri());
 	wp_enqueue_style('advance-portfolio-customcss', get_template_directory_uri().'/css/custom.css');
