afb2db792822b57c8645075046919b8b80b53ee5
Theme Directory <noreply@wordpress.org>
2020-12-23 10:56:58 +0000

    New version of Affiliate Marketingly - 1.3

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php 
-	 add_action( 'wp_enqueue_scripts', 'affiliate_marketingly_enqueue_styles' );
-	 function affiliate_marketingly_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
+add_action( 'wp_enqueue_scripts', 'affiliate_marketingly_enqueue_styles' );
+function affiliate_marketingly_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+} 
 
 require get_stylesheet_directory() . '/inc/custom-header.php';
 
@@ -13,68 +13,68 @@ add_action( 'wp_enqueue_scripts', 'affiliate_marketingly_google_fonts' );
 
 
 function affiliate_marketingly_customize_register( $wp_customize ) {
-$wp_customize->add_section( 'footer_settings', array(
+	$wp_customize->add_section( 'footer_settings', array(
 		'title'      => __('Footer Settings','affiliate-marketingly'),
 		'priority'   => 1,
 		'capability' => 'edit_theme_options',
-		) );
+	) );
 
 	$wp_customize->add_setting( 'footer_headline_color', array(
 		'default'           => '#fff',
 		'sanitize_callback' => 'sanitize_hex_color',
 		'transport'         => 'postMessage',
-		) );
+	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_headline_color', array(
 		'label'       => __( 'Headline Color', 'affiliate-marketingly' ),
 		'section'     => 'footer_settings',
 		'priority'   => 1,
 		'settings'    => 'footer_headline_color',
-		) ) );
+	) ) );
 	$wp_customize->add_setting( 'footer_text_color', array(
 		'default'           => '#656565',
 		'sanitize_callback' => 'sanitize_hex_color',
 		'transport'         => 'postMessage',
-		) );
+	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_text_color', array(
 		'label'       => __( 'Text Color', 'affiliate-marketingly' ),
 		'section'     => 'footer_settings',
 		'priority'   => 1,
 		'settings'    => 'footer_text_color',
-		) ) );
+	) ) );
 	$wp_customize->add_setting( 'footer_link_color', array(
 		'default'           => '#fff',
 		'sanitize_callback' => 'sanitize_hex_color',
 		'transport'         => 'postMessage',
-		) );
+	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_link_color', array(
 		'label'       => __( 'Link Color', 'affiliate-marketingly' ),
 		'section'     => 'footer_settings',
 		'priority'   => 1,
 		'settings'    => 'footer_link_color',
-		) ) );
+	) ) );
 	$wp_customize->add_setting( 'footer_border_color', array(
 		'default'           => '#fff',
 		'sanitize_callback' => 'sanitize_hex_color',
 		'transport'         => 'postMessage',
-		) );
+	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_border_color', array(
 		'label'       => __( 'Border Color', 'affiliate-marketingly' ),
 		'section'     => 'footer_settings',
 		'priority'   => 1,
 		'settings'    => 'footer_border_color',
-		) ) );
+	) ) );
 
 	$wp_customize->add_setting( 'footer_background_color', array(
 		'default'           => '#000',
 		'sanitize_callback' => 'sanitize_hex_color',
 		'transport'         => 'postMessage',
-		) );
+	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_background_color', array(
 		'label'       => __( 'Background Color', 'affiliate-marketingly' ),
 		'section'     => 'footer_settings',
 		'priority'   => 1,
 		'settings'    => 'footer_background_color',
-		) ) );
+	) ) );
 
 
 }
@@ -137,11 +137,113 @@ if(! function_exists('affiliate_marketingly_customizer_css_final_output' ) ):
 			.comments-area, .single article.fbox, .page article.fbox { background: <?php echo esc_attr(get_theme_mod( 'postpage_bg_color')); ?>; }
 
 			<?php if ( get_theme_mod( 'sidebar_hide' ) == '1' ) : ?>
-			aside#secondary {display:none;}
-			.featured-content {width:100%;margin-right:0;max-width:100%;}
-		<?php endif; ?>
+				aside#secondary {display:none;}
+				.featured-content {width:100%;margin-right:0;max-width:100%;}
+			<?php endif; ?>
 
-	</style>
+		</style>
 	<?php }
 	add_action( 'wp_head', 'affiliate_marketingly_customizer_css_final_output' );
-	endif;
+endif;
+
+
+
+/**
+ * This file represents an example of the code that themes would use to register
+ * the required plugins.
+ *
+ * It is expected that theme authors would copy and paste this code into their
+ * functions.php file, and amend to suit.
+ *
+ * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ *
+ * @package    TGM-Plugin-Activation
+ * @subpackage Example
+ * @version    2.6.1 for child theme Affiliate Marketingly for publication on WordPress.org
+ * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
+ * @copyright  Copyright (c) 2011, Thomas Griffin
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+ * @link       https://github.com/TGMPA/TGM-Plugin-Activation
+ */
+
+/**
+ * Include the TGM_Plugin_Activation class.
+ *
+ * Depending on your implementation, you may want to change the include call:
+ *
+ * Parent Theme:
+ * require_once get_template_directory() . '/tgm/class-tgm-plugin-activation.php';
+ *
+ * Child Theme:
+ * require_once get_stylesheet_directory() . '/tgm/class-tgm-plugin-activation.php';
+ *
+ * Plugin:
+ * require_once dirname( __FILE__ ) . '/tgm/class-tgm-plugin-activation.php';
+ */
+require_once get_stylesheet_directory() . '/tgm/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'affiliate_marketingly_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register five plugins:
+ * - one included with the TGMPA library
+ * - two from an external source, one from an arbitrary source, one from a GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+ *
+ * The variables passed to the `tgmpa()` function should be:
+ * - an array of plugin arrays;
+ * - optionally a configuration array.
+ * If you are not changing anything in the configuration array, you can remove the array and remove the
+ * variable from the function call: `tgmpa( $plugins );`.
+ * In that case, the TGMPA default settings will be used.
+ *
+ * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+ */
+function affiliate_marketingly_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		array(
+			'name'      => 'Coupon Reveal Buttons',
+			'slug'      => 'coupon-reveal-button',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Social Share & Follow Buttons',
+			'slug'      => 'superb-social-share-and-follow-buttons',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Superb Tables (SEO Optimized with Schema Markup)',
+			'slug'      => 'superb-tables',
+			'required'  => false,
+		),
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'affiliate-marketingly',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
