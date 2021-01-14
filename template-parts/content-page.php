7f443255f6c7ae8ede18ee4be356f90adf24a998
Theme Directory <noreply@wordpress.org>
2021-01-14 19:45:30 +0000

    New version of Aperture Portfolio - 1.0.8

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php 		if(is_front_page() || is_home()){
+			//no page title
+			}else {
+				the_title( '<h1 class="entry-title">', '</h1>' );	
+		} ?>
 	</header><!-- .entry-header -->
 
 	<?php aperture_portfolio_post_thumbnail(); ?>
