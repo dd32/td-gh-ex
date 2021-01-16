77e80aa7a028688277e05261a361c6c6dc713da5
Theme Directory <noreply@wordpress.org>
2021-01-16 17:59:32 +0000

    New version of Vantage - 1.15.5

--- inc/contributors.php
+++ inc/contributors.php
@@ -3,78 +3,78 @@ return json_decode( '{
 	"b7122312e4008f8f2c76911080bca01a": {
 		"name": "Andrew Misplon",
 		"email": "b7122312e4008f8f2c76911080bca01a",
-		"loc": 7107,
-		"score": 256.69944041355785,
-		"percent": 91.96369303220368
+		"loc": 7082,
+		"score": 230.60525222338285,
+		"percent": 60.45089271254731
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 1314,
+		"score": 137.8006978696793,
+		"percent": 36.12309399859137
 	},
 	"85ce7a450a7ee4895f3bd823505e2e12": {
 		"name": "adiraomj",
 		"email": "85ce7a450a7ee4895f3bd823505e2e12",
 		"loc": 1638,
-		"score": 8.677600128287384,
-		"percent": 3.108788056445969
-	},
-	"5cff8d4385a469d1baab761889ad3161": {
-		"name": "Alex S",
-		"email": "5cff8d4385a469d1baab761889ad3161",
-		"loc": 185,
-		"score": 7.963833833948096,
-		"percent": 2.853078171439593
+		"score": 7.815161703783826,
+		"percent": 2.048667569934649
 	},
 	"43c0e7220c23bd6636cf5469be6e9612": {
 		"name": "Braam Genis",
 		"email": "43c0e7220c23bd6636cf5469be6e9612",
-		"loc": 162,
-		"score": 2.5809285184108424,
-		"percent": 0.9246288874755917
+		"loc": 161,
+		"score": 2.3068251741831927,
+		"percent": 0.6047114702143439
 	},
 	"36639e6e074b731b093bde5a77305179": {
 		"name": "Braam Genis",
 		"email": "36639e6e074b731b093bde5a77305179",
 		"loc": 596,
-		"score": 1.0601127455387667,
-		"percent": 0.37979001026721626
+		"score": 0.9547515886006551,
+		"percent": 0.25027871348621467
 	},
 	"be6a5d2323cfe9ec8246b7a677dbc65b": {
 		"name": "Aaron Evans",
 		"email": "be6a5d2323cfe9ec8246b7a677dbc65b",
 		"loc": 23,
-		"score": 0.7081285228233563,
-		"percent": 0.25369012879560404
+		"score": 0.6377499388249884,
+		"percent": 0.16717985716993936
 	},
 	"9e7a3b2d24c2b15c53209ba8e7b4e724": {
 		"name": "Kevin Batdorf",
 		"email": "9e7a3b2d24c2b15c53209ba8e7b4e724",
 		"loc": 22,
-		"score": 0.6355890981071386,
-		"percent": 0.2277025638184949
+		"score": 0.5724199596492963,
+		"percent": 0.15005424739311976
 	},
 	"d1bea7e3f82e561c135bf5a3d4f9f896": {
 		"name": "SiteOrigin Support",
 		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
-		"loc": 13,
-		"score": 0.413877751143865,
-		"percent": 0.1482735077167816
+		"loc": 14,
+		"score": 0.42964856496508647,
+		"percent": 0.1126281342440768
 	},
 	"d203744279876c5eda043108e2611648": {
 		"name": "Alex S",
 		"email": "d203744279876c5eda043108e2611648",
 		"loc": 19,
-		"score": 0.2581400276917608,
-		"percent": 0.0924797897016211
+		"score": 0.23248433420445172,
+		"percent": 0.06094347552295806
 	},
 	"a885c7bd5442dd2acd8c3e42001332c6": {
 		"name": "Alex S",
 		"email": "a885c7bd5442dd2acd8c3e42001332c6",
 		"loc": 20,
-		"score": 0.11074774323897077,
-		"percent": 0.03967586157114232
+		"score": 0.09974088669016389,
+		"percent": 0.02614608983199067
 	},
 	"07294cc1dd7658895e44c559dfffd76f": {
 		"name": "gpriday",
 		"email": "07294cc1dd7658895e44c559dfffd76f",
 		"loc": 20,
-		"score": 0.022888739238878682,
-		"percent": 0.00819999056432388
+		"score": 0.0206139017813145,
+		"percent": 0.005403731064035479
 	}
 }', true );
