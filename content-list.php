d50ad3d766455dcbe6bb619435550c8297ca2527
Theme Directory <noreply@wordpress.org>
2021-01-30 13:01:22 +0000

    New version of OneColumn - 6.9

--- content-list.php
+++ content-list.php
@@ -28,7 +28,7 @@
 
 	<?php if ( get_theme_mod( 'onecolumn_read_more' ) != "no" ) { ?>
 		<div class="more">
-			<a class="readmore" href="<?php the_permalink() ?>" rel="bookmark"><?php _e( 'Read More &raquo;', 'onecolumn' ); ?></a>
+			<a class="readmore" href="<?php the_permalink() ?>" rel="bookmark"><?php _e( 'Read More &raquo;', 'onecolumn' ); ?><span class="screen-reader-text"> <?php the_title(); ?></span></a>
 		</div>
 	<?php } ?>
 </article>
